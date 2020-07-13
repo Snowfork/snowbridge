17d0441e02318f0a3b12feb80ba7a2760ada5f6e
Vincent Geddes <vincent.geddes@hey.com>
2020-07-13 15:16:49 +0200

    Add crate for signature verifier

---
+++ parachain/ethereum/eth-signature/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+pub use primitive_types::H160;
+
+type Address = H160;
+type RawMessage = Vec<u8>;
+type Signature = Vec<u8>;
+
+fn verify(message: RawMessage, signature: Vec<u8>) -> bool {
+    false
+}
