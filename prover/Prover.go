e74cf1691e16fccfe8c1cb42ff6d106d1a590e30
denalimarsh <denalimarsh@gmail.com>
2020-07-13 22:49:11 +0200

    remove ThresholdVerifier

--- prover/Prover.go
+++ prover/Prover.go
@@ -9,7 +9,6 @@ import (
 // Prover can verify transactions via a SPV Proof or Threshold Vote
 type Prover interface {
 	ProofVerifier
-	ThresholdVerifier
 
 	// VerifyTransaction passes a transaction to ProofVerifier of ThresholdVerifier
 	VerifyTransaction(verificationData VerificationData) (bool, error)
@@ -40,41 +39,4 @@ type SpvProof struct {
 	parentNodes []string
 	txIndex     int           // Transaction's position in a block
 	txReceipt   types.Receipt // Raw transaction receipt
-}
-
-// ThresholdVerifier verifies transactions via a Threshold Vote
-type ThresholdVerifier interface {
-	// IsValidSignature validates that a given signature is the given validator for an individual transaction
-	IsValidSignature(tx types.Transaction, signature string, validator common.Address) (bool, error)
-
-	// VerifyVote validates that enough validators have signed a transaction to meet the threshold requirements
-	VerifyVote(tx types.Transaction, votes []Vote) bool
-
-	// UpdateValset updates the current validators and their powers
-	UpdateValset(valset map[common.Address]int) error
-
-	// SetThreshold sets the percentage threshold of total signed power required for a successful vote
-	SetThreshold(threshold int) error
-
-	// GetValidators gets the current list of validators
-	GetValidators() []common.Address
-
-	// IsValidator a boolean indicating if the given address is validator
-	IsValidator(validator common.Address) bool
-
-	// GetPower gets a validator's current power
-	GetPower(validator common.Address) int
-}
-
-// ThresholdVote supports proof-of-stake based validation
-type ThresholdVote struct {
-	verificationData VerificationData
-	votes            []Vote // A collection of votes
-}
-
-// Vote struct contains a validator address and their signature on a data payload
-type Vote struct {
-	address   common.Address         // Validator's address used create the signature
-	payload   map[string]interface{} // The data payload this vote is being made upon
-	signature string                 // Signature of validator on the message
 }
