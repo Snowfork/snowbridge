b68ed23e93ea9cf2c48b8dfd938fbc2d198b11c1
Vincent Geddes <vincent.geddes@hey.com>
2020-07-13 21:49:32 +0200

    Add crate for ethereum signature verifier (#8)
    * Add crate for signature verifier
    
    * Change argument type to Signature
    
    * No-op signature verifier should return true
    
    * Make items public

---
+++ parachain/ethereum/eth-signature/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+pub use primitive_types::H160;
+
+pub type Address = H160;
+pub type RawMessage = Vec<u8>;
+pub type Signature = Vec<u8>;
+
+pub fn verify(message: RawMessage, signature: Signature) -> bool {
+    true
+}
