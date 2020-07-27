a9dbe1fd0941d3f0309a38b423369081c5567d64
Vincent Geddes <vincent@snowfork.com>
2020-07-27 22:32:55 +0200

    Miscellaneous truffle contract fixes

--- ethereum/contracts/Bank.sol
+++ ethereum/contracts/Bank.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.2;
 
-import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
-import "../node_modules/@openzeppelin/contracts/token/erc20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Bank {
     using SafeMath for uint256;
