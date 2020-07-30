999c92680cb3e5e6bd6b8a45609757bf0c2ea93a
denalimarsh <denalimarsh@gmail.com>
2020-07-30 02:57:44 +0200

    prover prefix msg

--- prover/Prover.go
+++ prover/Prover.go
@@ -2,8 +2,10 @@ package prover
 
 import (
 	"crypto/ecdsa"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	solsha3 "github.com/miguelmota/go-solidity-sha3"
 )
 
 // Prover generates proofs
@@ -28,7 +30,8 @@ func NewProof(hash, signature []byte) Proof {
 
 // GenerateProof creates a new proof by signing a data hash with a private key
 func GenerateProof(data []byte, pk *ecdsa.PrivateKey) (Proof, error) {
-	hash := crypto.Keccak256Hash(data)
+	hash := crypto.Keccak256Hash(solsha3.String("\x19Ethereum Signed Message:\n32"), data)
+	fmt.Println(hash)
 	signature, err := crypto.Sign(hash.Bytes(), pk)
 	if err != nil {
 		return Proof{}, err
