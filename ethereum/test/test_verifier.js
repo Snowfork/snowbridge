cf0ec2ff37d7d6f1bffe4ac5fbc075a3851f8806
denalimarsh <denalimarsh@gmail.com>
2020-08-03 20:37:27 +0200

    add recover/verify tests

--- ethereum/test/test_verifier.js
+++ ethereum/test/test_verifier.js
@@ -9,6 +9,7 @@ require("chai")
 
 contract("Verifier", function (accounts) {
   const operator = accounts[1];
+  const userTwo = accounts[2];
 
   describe("Verifier contract deployment", function () {
     beforeEach(async function () {
@@ -28,12 +29,20 @@ contract("Verifier", function (accounts) {
       this.verifier = await Verifier.new(operator);
     });
 
-    it("should correctly validate signatures", async function () {
+    it("should recover a signer's address from a signed hash and signature", async function () {
+      const message =  web3.utils.soliditySha3("test123xyz~~")
+      const signature = await web3.eth.sign(message, userTwo);
+
+      const prefixedMsg = await this.verifier.ethMessageHash(message)
+      const signer = await this.verifier.recover(prefixedMsg, signature);
+      signer.should.be.equal(userTwo);
+    });
+
+    it("should validate messages signed by the operator", async function () {
       const message =  web3.utils.soliditySha3("test123xyz~~")
       const signature = await web3.eth.sign(message, operator);
 
-      // Recover a boolean indicating if the signature originates from the operator
-      const isOperator = await this.verifier.recover(message, signature);
+      const isOperator = await this.verifier.verify(message, signature);
       isOperator.should.be.equal(true);
     });
   });
