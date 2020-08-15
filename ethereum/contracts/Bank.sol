e12cacbba8bc1cc6e14f9c96d258333457684611
denalimarsh <denalimarsh@gmail.com>
2020-08-15 12:27:10 +0200

    Basic data submission and token unlocking

--- ethereum/contracts/Bank.sol
+++ ethereum/contracts/Bank.sol
@@ -15,6 +15,7 @@ contract Bank {
     enum AppEventTags { SendETH, SendERC20 }
 
     event AppEvent(uint _tag, bytes _data);
+    event Unlock(address _recipient, address _token, uint256 _amount);
 
     constructor() public {
         nonce = 0;
@@ -23,7 +24,8 @@ contract Bank {
     function sendETH(
         bytes32 _recipient
     )
-        public payable
+        public
+        payable
     {
         require(msg.value > 0, "Value of transaction must be positive");
 
@@ -69,5 +71,53 @@ contract Bank {
         returns(bytes memory)
     {
         return abi.encode(_sender, _recipient, _tokenAddr, _amount, _nonce);
+    }
+
+    function submit(bytes calldata data)
+        external
+    {
+        // TODO: decode bytes into (message, signature)
+        // require that signature is from operator
+
+        // TODO: decode message bytes into (tag, recipient, amount) or (tag, recipient, token, amount)
+         uint256 tag = 0;                        // placeholder
+         address payable recipient = address(0); // placeholder
+         address token = address(0);             // placeholder
+         uint256 amount = 1;                     // placeholder
+
+        if(tag == uint(AppEventTags.SendETH)) {
+            unlockETH(recipient, amount);
+        } else if(tag == uint(AppEventTags.SendERC20)) {
+            unlockERC20(recipient, token, amount);
+        }
+    }
+
+    function unlockETH(
+        address payable _recipient,
+        uint256 _amount
+    )
+        internal
+    {
+        require(_amount > 0, "Must unlock a positive amount");
+        require(totalETH > _amount, "ETH token balances insufficient to fulfill the unlock request");
+
+        totalETH = totalETH.sub(_amount);
+        _recipient.transfer(_amount);
+        emit Unlock(_recipient, address(0), _amount);
+    }
+
+    function unlockERC20(
+        address _recipient,
+        address _token,
+        uint256 _amount
+    )
+        internal
+    {
+        require(_amount > 0, "Must unlock a positive amount");
+        require(totalTokens[_token] > _amount, "ERC20 token balances insufficient to fulfill the unlock request");
+
+        totalTokens[_token] = totalTokens[_token].sub(_amount);
+        require(IERC20(_token).transfer(_recipient, _amount), "ERC20 token transfer failed");
+        emit Unlock(_recipient, _token, _amount);
     }
 }
