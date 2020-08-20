796d40cd4210daaea14e13627bfde62661ee62a7
Denali Marsh <denalimarsh@gmail.com>
2020-08-20 14:36:39 +0200

    Relayer data submission to contracts and unlocking of ETH/ERC20 tokens (#55)
    * Basic data submission and token unlocking
    
    * relayer submit data to ethereum
    
    * signature verification in relayer
    
    * revisions

--- bridgerelayer/chains/ethereum/router.go
+++ bridgerelayer/chains/ethereum/router.go
@@ -2,32 +2,47 @@ package ethereum
 
 import (
 	"bytes"
+	"context"
+	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	ctypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/sha3"
 
 	keybase "github.com/snowfork/polkadot-ethereum/bridgerelayer/keybase/ethereum"
-	"github.com/snowfork/polkadot-ethereum/bridgerelayer/types"
 	"github.com/snowfork/polkadot-ethereum/bridgerelayer/substrate"
+	"github.com/snowfork/polkadot-ethereum/bridgerelayer/types"
 	"github.com/snowfork/polkadot-ethereum/prover"
 )
 
 // Router packages raw event data as Packets and relays them to the bridge
 type Router struct {
-	keybase *keybase.Keypair
-	sc      *substrate.Client
+	keybase  *keybase.Keypair
+	sc       *substrate.Client
+	ec       *ethclient.Client
+	verifier common.Address
 }
 
 // NewRouter initializes a new instance of Router
-func NewRouter(keybase *keybase.Keypair) (*Router, error) {
-	sc, err := substrate.NewClient()
+func NewRouter(websocketURL string, keybase *keybase.Keypair, verifier common.Address) (*Router, error) {
+	substrateClient, err := substrate.NewClient()
 	if err != nil {
 		return nil, err
 	}
+
+	ethereumClient, err := ethclient.Dial(websocketURL)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Router{
-		keybase: keybase,
-		sc: sc,
+		keybase:  keybase,
+		sc:       substrateClient,
+		ec:       ethereumClient,
+		verifier: verifier,
 	}, nil
 }
 
@@ -67,7 +82,7 @@ func (er Router) buildPacket(id common.Address, eLog ctypes.Log) (types.PacketV2
 	}
 
 	packet := types.PacketV2{
-		Data: buff.Bytes(),
+		Data:      buff.Bytes(),
 		Signature: proof.Signature,
 	}
 	return packet, nil
@@ -78,4 +93,65 @@ func (er Router) sendPacket(appID [32]byte, packet types.PacketV2) error {
 	log.Info("Sending packet:\n", packet)
 	er.sc.SubmitPacket(appID, packet)
 	return nil
+}
+
+// Submit sends a SCALE-encoded message to an application deployed on the Ethereum network
+func (er Router) Submit(appID []byte, data []byte) error {
+	// TODO: break down data into (message, signature)
+	message := data[0:50]    // placeholder
+	signature := data[50:80] // placeholder
+
+	// isOperator is a boolean indicating if the message was signed by the operator
+	isOperator, err := er.verifySignature(message, signature)
+	if err != nil {
+		return err
+	}
+	// Check that the message signer's address matches the operator's address stored on contract
+	if !isOperator {
+		return fmt.Errorf("invalid operator signature %s for message %s", signature, message)
+	}
+
+	nonce, err := er.ec.PendingNonceAt(context.Background(), er.keybase.CommonAddress())
+	if err != nil {
+		return err
+	}
+
+	value := big.NewInt(0)      // in wei (0 eth)
+	gasLimit := uint64(2000000) // in units
+	gasPrice, err := er.ec.SuggestGasPrice(context.Background())
+	if err != nil {
+		return err
+	}
+
+	// Calculate the method ID of our function using crypto.sha3
+	submitFnSignature := []byte("submit(bytes)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(submitFnSignature)
+	methodID := hash.Sum(nil)[:4]
+
+	var txData []byte
+	txData = append(txData, methodID...)
+	txData = append(txData, data...) // TODO: consider padding bytes with common.LeftPadBytes(data.Bytes(), 32)
+
+	appAddress := common.BytesToAddress(appID)
+	tx := ctypes.NewTransaction(nonce, appAddress, value, gasLimit, gasPrice, data)
+	signedTx, err := ctypes.SignTx(tx, ctypes.HomesteadSigner{}, er.keybase.PrivateKey())
+	if err != nil {
+		return err
+	}
+
+	err = er.ec.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return err
+	}
+
+	log.Info("tx sent: ", signedTx.Hash().Hex())
+	return nil
+}
+
+// TODO: implement function
+func (er Router) verifySignature(message []byte, signature []byte) (bool, error) {
+	// 1. call verify(message, signature) on contract address er.verifier
+	// 2. get result
+	return true, nil
 }
