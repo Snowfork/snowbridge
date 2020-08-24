65e997b241b157c55dd373a3dbe954bf9b5eb3ee
denalimarsh <denalimarsh@gmail.com>
2020-08-24 23:56:01 +0200

    reject compact uint > 32 bytes, uint256 decoding

--- ethereum/contracts/Scale.sol
+++ ethereum/contracts/Scale.sol
@@ -5,7 +5,7 @@ contract Scale {
 
     // Read a byte at a specific index and return it as type uint8
     function readByteAtIndex(bytes memory data, uint8 index)
-        public
+        internal
         pure
         returns (uint8)
     {
@@ -14,7 +14,7 @@ contract Scale {
 
     // Reverse uint8[] array in place
     function reverse(uint8[] memory arr)
-        public
+        internal
         pure
         returns(uint8[] memory)
     {
@@ -29,7 +29,7 @@ contract Scale {
 
     // Convert an uint8[] array to uint256
     function uint8ArrayToUint256(uint8[] memory arr)
-        public
+        internal
         pure
         returns (uint256)
     {
@@ -38,6 +38,44 @@ contract Scale {
             number = number+uint(arr[i])*(2**(8*(arr.length-(i+1))));
         }
         return number;
+    }
+
+      // Convert bytes (big endian) to little endian format
+    function bytesToLittleEndian(bytes memory arr)
+        internal
+        pure
+        returns (uint256)
+    {
+        uint256 number;
+        for(uint i = 0; i < arr.length; i++){
+            number = number+uint(uint8(arr[i]))*(2**(8*(arr.length-(i+1))));
+        }
+        return number;
+    }
+
+     // Reverse an array of bytes in place
+    function reverseBytes(bytes memory arr)
+        internal
+        pure
+        returns(bytes memory)
+    {
+        for (uint i = 0; i < arr.length/2; i++) {
+            bytes1 current = arr[i];
+            uint256 otherIndex = arr.length - i - 1;
+            arr[i] = arr[otherIndex];
+            arr[otherIndex] = current;
+        }
+        return arr;
+    }
+
+    function decodeUint256(bytes memory data)
+        public
+        pure
+        returns (uint256)
+    {
+        bytes memory reversed = reverseBytes(data);
+        uint256 lEndian = bytesToLittleEndian(reversed);
+        return lEndian;
     }
 
     // Decodes a SCALE encoded compact unsigned integer
@@ -49,15 +87,15 @@ contract Scale {
         uint8 b = readByteAtIndex(data, 0);           // read the first byte
         uint8 mode = b & 3;                           // bitwise operation
 
-        if(mode == 0) {
-            return b >> 2;                           // right shift to remove mode bits
-        } else if(mode == 1) {
+        if(mode == 0) {                               // [0, 63]
+            return b >> 2;                            // right shift to remove mode bits
+        } else if(mode == 1) {                        // [64, 16383]
             uint8 bb = readByteAtIndex(data, 1);      // read the second byte
             uint64 r = bb;                            // convert to uint64
             r <<= 6;                                  // multiply by * 2^6
             r += b >> 2;                              // right shift to remove mode bits
             return r;
-        } else if(mode == 2) {
+        } else if(mode == 2) {                        // [16384, 1073741823]
             uint8 b2 = readByteAtIndex(data, 1);      // read the next 3 bytes
             uint8 b3 = readByteAtIndex(data, 2);
             uint8 b4 = readByteAtIndex(data, 3);
@@ -68,15 +106,15 @@ contract Scale {
 
             x3 >>= 2;                                 // remove the last 2 mode bits
             return uint256(x3);
-        } else if(mode == 3) {
+        } else if(mode == 3) {                        // [1073741824, 4503599627370496]
             uint8 l = b >> 2;                         // remove mode bits
-            require(l <= 63,                          // Max upper bound of 536 is (67 - 4)
-                    "Not supported: l>63 encountered when decoding a compact-encoded uint");
+            require(l > 32, "Not supported: number cannot be greater than 32 bytes");
             uint8[] memory buf = new uint8[](l+4);
             for(uint8 i = 1; i < l+4+1; i++) {        // read bytes from data[1:l+4]
                 uint8 bNext = readByteAtIndex(data, i);
                 buf[i-1] = bNext;
             }
+
             uint8[] memory reverseBuf = reverse(buf); // reverse byte array
             return uint8ArrayToUint256(reverseBuf);   // convert reversed bytes to uint256
         } else {
--- ethereum/test/test_scale.js
+++ ethereum/test/test_scale.js
@@ -24,7 +24,7 @@ contract("Scale", function () {
 
     describe("decoding compact uints", async function () {
 
-      it("case 0: [0, 63]", async function () {
+      it("should decode case 0: [0, 63]", async function () {
         const tests = [
           {encoded: toHexBytes("00"), decoded: 0},
           {encoded: toHexBytes("fc"), decoded: 63},
@@ -36,7 +36,7 @@ contract("Scale", function () {
         }
       });
 
-      it("case 1: [64, 16383]", async function () {
+      it("should decode case 1: [64, 16383]", async function () {
         const tests = [
           {encoded: toHexBytes("01 01"), decoded: 64},
           {encoded: toHexBytes("fd ff"), decoded: 16383},
@@ -48,7 +48,7 @@ contract("Scale", function () {
         }
       });
 
-      it("case 2: [16384, 1073741823]", async function () {
+      it("should decode case 2: [16384, 1073741823]", async function () {
         const tests = [
           {encoded: toHexBytes("02 00 01 00"), decoded: 16384},
           {encoded: toHexBytes("fe ff ff ff"), decoded: 1073741823},
@@ -60,88 +60,106 @@ contract("Scale", function () {
         }
       });
 
-      it("case 3: [1073741823, 4503599627370496]", async function () {
+      it("should reject case 3: [1073741824, 4503599627370496]", async function () {
         const tests = [
           {encoded: toHexBytes("03 00 00 00 40"), decoded: 1073741824},
-          // {encoded: toHexBytes("03 ff ff ff ff"), decoded:  1<<32 - 1}, // TODO: slightly off
-          // {encoded: toHexBytes("07 00 00 00 00 01"), decoded: 1 << 32},
-          // {encoded: toHexBytes("0b 00 00 00 00 00 01"), decoded: 1 << 40},
-          // {encoded: toHexBytes("0f ff ff ff ff ff ff ff"), decoded: 1 << 48},
-          // {encoded: toHexBytes("0f 00 00 00 00 00 00 01"), decoded: 1<<56 - 1},
-          // {encoded: toHexBytes("13 00 00 00 00 00 00 00 01"), decoded:  1 << 56},
+          {encoded: toHexBytes("07 00 00 00 00 01"), decoded: 1 << 32},
+          {encoded: toHexBytes("0f ff ff ff ff ff ff ff"), decoded: 1 << 48},
+          {encoded: toHexBytes("13 00 00 00 00 00 00 00 01"), decoded:  1 << 56},
         ];
 
         for(test of tests) {
-          const output = Number(await this.scale.decodeUintCompact.call(test.encoded));
-          output.should.be.bignumber.equal(test.decoded);
+          await this.scale.decodeUintCompact.call(test.encoded).should.not.be.fulfilled;
         }
       });
     });
   });
 
-    describe("Gas costs", async function () {
+  describe("decoding uint256s", async function () {
 
-      beforeEach(async function () {
-        this.scale = await Scale.new();
-      });
-
-      it("compact uints: [0, 63]", async function () {
-        const tests = [
-          {encoded: toHexBytes("00"), decoded: 0},
-          {encoded: toHexBytes("fc"), decoded: 63},
-        ];
-
-        let totalGas = 0;
-        for(test of tests) {
-          const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
-          totalGas += Number(gasCost);
-        }
-        totalGas = totalGas / tests.length;
-        console.log('\tDecoding [0, 63] average gas: ' + totalGas);
-      });
-
-      it("compact uints: [64, 16383]", async function () {
-        const tests = [
-          {encoded: toHexBytes("01 01"), decoded: 64},
-          {encoded: toHexBytes("fd ff"), decoded: 16383},
-        ];
-
-        let totalGas = 0;
-        for(test of tests) {
-          const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
-          totalGas += Number(gasCost);
-        }
-        totalGas = totalGas / tests.length;
-        console.log('\tDecoding [64, 16383] average gas: ' + totalGas);
-      });
-
-      it("compact uints: [16384, 1073741823]", async function () {
-        const tests = [
-          {encoded: toHexBytes("02 00 01 00"), decoded: 16384},
-          {encoded: toHexBytes("fe ff ff ff"), decoded: 1073741823},
-        ];
-
-        let totalGas = 0;
-        for(test of tests) {
-          const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
-          totalGas += Number(gasCost);
-        }
-        totalGas = totalGas / tests.length;
-        console.log('\tDecoding [16384, 1073741823] average gas: ' + totalGas);
-      });
-
-      it("compact uints: [1073741823, 4503599627370496]", async function () {
-        const tests = [
-          {encoded: toHexBytes("03 00 00 00 40"), decoded: 1073741824},
-        ];
-
-        let totalGas = 0;
-        for(test of tests) {
-          const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
-          totalGas += Number(gasCost);
-        }
-        totalGas = totalGas / tests.length;
-        console.log('\tDecoding [1073741823, 4503599627370496] average gas: ' + totalGas);
-      });
+    beforeEach(async function () {
+      this.scale = await Scale.new();
     });
+
+    it("should decode uint256", async function () {
+      const tests = [
+        {encoded: toHexBytes("1d 00 00"), decoded: 29},
+        {encoded: "0x1d000000000000000000000000000000", decoded: 29},
+        {encoded: "0x3412", decoded: 4660},
+        {encoded: "0x201f1e1d1c1b1a1817161514131211100f0e0d0c0b0a09080706050403020100", decoded: 1780731860627700044960722568376592200742329637303199754547880948779589408},
+      ];
+
+      for(test of tests) {
+        const output = Number(await this.scale.decodeUint256.call(test.encoded));
+        output.should.be.bignumber.equal(test.decoded);
+      }
+    });
+  });
+
+  describe("Gas costs", async function () {
+
+    beforeEach(async function () {
+      this.scale = await Scale.new();
+    });
+
+    it("compact uints: [0, 63]", async function () {
+      const tests = [
+        {encoded: toHexBytes("00"), decoded: 0},
+        {encoded: toHexBytes("fc"), decoded: 63},
+      ];
+
+      let totalGas = 0;
+      for(test of tests) {
+        const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
+        totalGas += Number(gasCost);
+      }
+      totalGas = totalGas / tests.length;
+      console.log('\tDecoding [0, 63] average gas: ' + totalGas);
+    });
+
+    it("compact uints: [64, 16383]", async function () {
+      const tests = [
+        {encoded: toHexBytes("01 01"), decoded: 64},
+        {encoded: toHexBytes("fd ff"), decoded: 16383},
+      ];
+
+      let totalGas = 0;
+      for(test of tests) {
+        const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
+        totalGas += Number(gasCost);
+      }
+      totalGas = totalGas / tests.length;
+      console.log('\tDecoding [64, 16383] average gas: ' + totalGas);
+    });
+
+    it("compact uints: [16384, 1073741823]", async function () {
+      const tests = [
+        {encoded: toHexBytes("02 00 01 00"), decoded: 16384},
+        {encoded: toHexBytes("fe ff ff ff"), decoded: 1073741823},
+      ];
+
+      let totalGas = 0;
+      for(test of tests) {
+        const gasCost = await this.scale.decodeUintCompact.estimateGas(test.encoded);
+        totalGas += Number(gasCost);
+      }
+      totalGas = totalGas / tests.length;
+      console.log('\tDecoding [16384, 1073741823] average gas: ' + totalGas);
+    });
+
+    it("uint256", async function () {
+      const tests = [
+        {encoded: "0x1d000000000000000000000000000000", decoded: 29},
+        {encoded: "0x201f1e1d1c1b1a1817161514131211100f0e0d0c0b0a09080706050403020100", decoded: 1780731860627700044960722568376592200742329637303199754547880948779589408},
+      ];
+
+      let totalGas = 0;
+      for(test of tests) {
+        const gasCost = await this.scale.decodeUint256.estimateGas(test.encoded);
+        totalGas += Number(gasCost);
+      }
+      totalGas = totalGas / tests.length;
+      console.log('\tDecoding uint256 average gas: ' + totalGas);
+    });
+  });
 });
