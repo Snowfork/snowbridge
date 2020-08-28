4341c3283db7054be77d8ebe71ab7605717245a7
denalimarsh <denalimarsh@gmail.com>
2020-08-28 16:12:58 +0200

    get tx by hash

---
+++ ethereum/scripts/getTx.js
@@ -0,0 +1,15 @@
+module.exports = async () => {
+    const Web3 = require("web3");
+
+    const txHash = process.argv[4].toString();
+    if (!txHash) {
+        console.log("Please provide an transaction hash")
+        return
+    }
+
+    let provider = new Web3.providers.HttpProvider("http://localhost:7545");
+    const web3 = new Web3(provider);
+
+    var receipt = await web3.eth.getTransactionReceipt(txHash);
+    console.log(receipt);
+};
