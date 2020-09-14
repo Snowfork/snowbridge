0bf3adece367c08ce19bc50007eb975eb7de2e71
denalimarsh <denalimarsh@gmail.com>
2020-09-14 13:51:04 +0200

    replace event ID, block number with event hashes

--- ethereum/contracts/Bridge.sol
+++ ethereum/contracts/Bridge.sol
@@ -10,13 +10,10 @@ contract Bridge {
     // 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned for accounts without code
     bytes32 NON_CONTRACT_ACCOUNT_HASH = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
 
-    uint64 public mostRecentBlock;
-    mapping(uint64 => uint64) public mostRecentBlockEvent;
+    mapping(bytes32 => bool) public eventHashes;
     mapping(address => bool) public applications;
 
-
     constructor(address[] memory apps) public {
-        mostRecentBlock = 0;
         for(uint256 i = 0; i < apps.length; i++) {
             if(verifyApp(apps[i])) {
                 applications[apps[i]] = true;
@@ -27,28 +24,20 @@ contract Bridge {
     /**
      * @dev routes the message to the specified application ID after verifying the operator's signature
      * @param _message address _message expected type: Message { AppID [32]byte, Payload []byte }
-     * @param _verification bytes _verification expected type: Verification { BlockNumber int64, EventID int64 }
+     * @param _hash bytes _hash is a unique hash of the event (app_id, payload, blockNum, eventId)
      */
-    function handle(bytes memory _message, bytes memory _verification)
+    function handle(bytes memory _message, bytes32 _hash)
         public
     {
-        uint64 blockNumber = uint64(_verification.slice(0, 8).decodeUint256());
-        require(blockNumber >= mostRecentBlock, "Blocks must be processed chronologically");
-
-        uint64 eventID = uint64(_verification.slice(8, 16).decodeUint256());
-        require(eventID > mostRecentBlockEvent[blockNumber], "Events must be processed chronologically");
-
         address appID = _message.sliceAddress(32);
         require(applications[appID], "App ID not found. Only registered application are supported");
+
+        require(!eventHashes[_hash], "Event has already been processed");
+        eventHashes[_hash] = true;
 
         bytes memory payload = _message.slice(32, _message.length-1);
         Application app = Application(appID);
         app.handle(payload);
-
-        mostRecentBlockEvent[blockNumber] = eventID;
-        if(blockNumber > mostRecentBlock) {
-            mostRecentBlock = blockNumber;
-        }
     }
 
     /**
