36d74f3b7ef84d7a5386c1f80577148593226573
Denali Marsh <denalimarsh@gmail.com>
2020-09-19 22:00:56 +0200

    ropsten deployment: config, scripts (#91)

--- ethereum/scripts/getEthBalance.js
+++ ethereum/scripts/getEthBalance.js
@@ -1,5 +1,11 @@
+// Example usage:
+// truffle exec getEthBalance.js [user-address]
+// truffle exec getEthBalance.js [user-address] --network ropsten
+
 module.exports = async () => {
+    require("dotenv").config();
     const Web3 = require("web3");
+    const HDWalletProvider = require("@truffle/hdwallet-provider");
 
     try {
         const account = process.argv[4].toString();
@@ -8,7 +14,17 @@ module.exports = async () => {
             return
         }
 
-        let provider = new Web3.providers.HttpProvider("http://localhost:9545");
+        let provider;
+        const NETWORK_ROPSTEN = process.argv[5] === "--network" && process.argv[6] === "ropsten";
+        if (NETWORK_ROPSTEN) {
+            provider = new HDWalletProvider(
+                process.env.MNEMONIC,
+                "https://ropsten.infura.io/v3/".concat(process.env.INFURA_PROJECT_ID)
+            );
+        } else {
+            provider = new Web3.providers.HttpProvider(process.env.LOCAL_PROVIDER);
+        }
+
         const web3 = new Web3(provider);
 
         const balanceWei = await web3.eth.getBalance(account)
