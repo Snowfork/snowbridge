7903a2a60b93cfc2149c2c0a5bd875feba3e6a38
Denali Marsh <denalimarsh@gmail.com>
2020-10-13 16:48:01 +0200

    Fix ETH unlock edge case (#125)
    * catch balances edge case
    
    * greater than or equal to

--- ethereum/contracts/ETHApp.sol
+++ ethereum/contracts/ETHApp.sol
@@ -61,7 +61,7 @@ contract ETHApp is Application {
         internal
     {
         require(_amount > 0, "Must unlock a positive amount");
-        require(totalETH > _amount, "ETH token balances insufficient to fulfill the unlock request");
+        require(totalETH >= _amount, "ETH token balances insufficient to fulfill the unlock request");
 
         totalETH = totalETH.sub(_amount);
         _recipient.transfer(_amount);
