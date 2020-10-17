f8b9c059fcb91d32dabcd3628c313218145f5da0
Rizmari Versfeld <rizziepit@gmail.com>
2020-10-17 15:32:40 -0700

    Add Eth block header definition to bridge

--- parachain/primitives/ethereum/src/lib.rs
+++ parachain/primitives/ethereum/src/lib.rs
@@ -1,10 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use sp_runtime::RuntimeDebug;
+use sp_std::prelude::*;
+use codec::{Encode, Decode};
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+#[cfg(feature = "std")]
+use serde_big_array::big_array;
+
 pub mod log;
 
+pub use ethereum_types::{Address, H160, H256, U256};
 pub use log::Log;
-
-pub use ethereum_types::{H160, U256};
 
 #[derive(Debug)]
 pub enum DecodeError {
@@ -25,5 +33,78 @@ impl From<rlp::DecoderError> for DecodeError {
 impl From<ethabi::Error> for DecodeError {
 	fn from(err: ethabi::Error) -> Self {
 		DecodeError::InvalidABI(err)
+	}
+}
+
+/// Complete block header id.
+#[derive(Clone, Copy, Default, Encode, Decode, PartialEq, RuntimeDebug)]
+pub struct HeaderId {
+	/// Header number.
+	pub number: u64,
+	/// Header hash.
+	pub hash: H256,
+}
+
+/// An Ethereum block header.
+/// TODO: add nonce for PoW
+#[derive(Clone, Default, Encode, Decode, PartialEq, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct Header {
+	/// Parent block hash.
+	pub parent_hash: H256,
+	/// Block timestamp.
+	pub timestamp: u64,
+	/// Block number.
+	pub number: u64,
+	/// Block author.
+	pub author: Address,
+
+	/// Transactions root.
+	pub transactions_root: H256,
+	/// Block ommers hash.
+	pub ommers_hash: H256,
+	/// Block extra data.
+	pub extra_data: Vec<u8>,
+
+	/// State root.
+	pub state_root: H256,
+	/// Block receipts root.
+	pub receipts_root: H256,
+	/// Block bloom.
+	pub logs_bloom: Bloom,
+	/// Gas used for contracts execution.
+	pub gas_used: U256,
+	/// Block gas limit.
+	pub gas_limit: U256,
+
+	/// Block difficulty.
+	pub difficulty: U256,
+	/// Vector of post-RLP-encoded fields.
+	pub seal: Vec<Vec<u8>>,
+}
+
+#[cfg(feature = "std")]
+big_array! { BigArray; }
+
+/// Logs bloom.
+#[derive(Clone, Debug, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct Bloom(#[cfg_attr(feature = "std", serde(with = "BigArray"))] [u8; 256]);
+
+impl<'a> From<&'a [u8; 256]> for Bloom {
+	fn from(buffer: &'a [u8; 256]) -> Bloom {
+		Bloom(*buffer)
+	}
+}
+
+impl PartialEq<Bloom> for Bloom {
+	fn eq(&self, other: &Bloom) -> bool {
+		self.0.iter().zip(other.0.iter()).all(|(l, r)| l == r)
+	}
+}
+
+impl Default for Bloom {
+	fn default() -> Self {
+		Bloom([0; 256])
 	}
 }
--- parachain/primitives/ethereum/src/log.rs
+++ parachain/primitives/ethereum/src/log.rs
@@ -1,7 +1,8 @@
+use codec::{Encode, Decode};
 use sp_std::prelude::*;
 use ethereum_types::{H160, H256};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, Encode, Decode, PartialEq, Eq)]
 pub struct Log {
 	pub address: H160,
 	pub topics: Vec<H256>,
