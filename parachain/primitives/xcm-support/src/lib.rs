fdeb48350c38d46da77f8c3481681e657a84fe3e
Vincent Geddes <vincent@snowfork.com>
2020-11-02 23:59:16 +0200

    Add comments to xcm-primitives

--- parachain/primitives/xcm-support/src/lib.rs
+++ parachain/primitives/xcm-support/src/lib.rs
@@ -30,7 +30,15 @@ where
 	BridgedAssets: ArtemisMultiAsset<AccountId, AssetId = H160>,
 	AccountId: sp_std::fmt::Debug
 {
+	// deposit asset into who's account
+	// Need to use AccountConverter helpers to convert MultiLocation into a AccountId
 	fn deposit_asset(asset: &MultiAsset, _who: &MultiLocation) -> XcmResult {
+		if_std! {
+			println!("DEPOSIT:");
+			println!("asset: {:?}", asset);
+			println!("who: {:?}", _who);
+		}
+
 		if let MultiAsset::ConcreteFungible { id, .. } = asset {
 			match id {
 				MultiLocation::X1(Junction::Parent) => {},
@@ -44,8 +52,10 @@ where
 		Ok(())
 	}
 
+	// withdraw asset from who's account
 	fn withdraw_asset(asset: &MultiAsset, _who: &MultiLocation) -> Result<MultiAsset, Error> {
 		if_std! {
+			println!("WITHDRAW:");
 			println!("asset: {:?}", asset);
 			println!("who: {:?}", _who);
 		}
