4570e8857de663449c553a560198960ed4810294
Vincent Geddes <vincent@snowfork.com>
2020-11-24 21:56:53 +0200

    Fixes

--- parachain/pallets/commitments/src/lib.rs
+++ parachain/pallets/commitments/src/lib.rs
@@ -1,20 +1,44 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_std::prelude::*;
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
-use frame_system::ensure_signed;
-use frame_support::storage::StorageMap;
+use frame_support::{
+	decl_module, decl_storage, decl_event, decl_error,
+	storage::IterableStorageMap,
+	dispatch,
+	weights::Weight,
+	traits::Get
+};
 use artemis_core::Commitments;
 
-use sp_core::H160;
+use sp_core::{H160, H256};
+use sp_runtime::{
+	traits::Zero,
+	DigestItem
+};
+
+use codec::{Encode, Decode};
+
+use sp_std::if_std;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Encode, Decode, Copy, Clone)]
+enum OtherDigestItem {
+	Commitment(H160, H256)
+}
 
 pub trait Trait: frame_system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+
+	type PruneInterval: Get<Self::BlockNumber>;
 }
 
 decl_storage! {
 	trait Store for Module<T: Trait> as Commitments {
-		CommitInterval: T::BlockNumber;
 		Messages: map hasher(identity) H160 => Vec<Vec<u8>>;
 	}
 }
@@ -39,19 +63,44 @@ decl_module! {
 		fn deposit_event() = default;
 
 		fn on_initialize(now: T::BlockNumber) -> Weight {
-			let digest = <frame_system::Module<T>>::digest();
+			if (now % T::PruneInterval::get()).is_zero() {
+				Self::commit()
+			} else {
+				0
+			}
 		}
 
 	}
 }
 
+impl<T: Trait> Module<T> {
+
+	fn commit() -> Weight {
+		let mut digest = <frame_system::Module<T>>::digest();
+
+		for (key, value) in <Self as Store>::Messages::iter() {
+
+			if_std! {
+				println!("key: {:?}", key);
+				println!("value: {:?}", value);
+			}
+			T::Hashing::hash(value.encode())
+		}
+
+
+
+		let foo: DigestItem<T::Hash> = DigestItem::Other(Vec::from([0u8; 12]));
+
+		digest.push(foo);
+
+		0
+	}
+}
 
 impl<T: Trait> Commitments for Module<T> {
-
 	fn add(address: H160, payload: &[u8]) {
 		let mut messages: Vec<Vec<u8>> = <Self as Store>::Messages::get(address);
 		messages.push(payload.into());
 		<Self as Store>::Messages::insert(address, messages);
 	}
-
 }
---
+++ parachain/pallets/commitments/src/tests.rs
@@ -0,0 +1,22 @@
+// Copyright 2020 Parity Technologies (UK) Ltd.
+use crate::{Error, mock::*};
+
+use sp_core::H160;
+
+use frame_support::{
+	assert_ok, assert_noop,
+	traits::{Contains, OnInitialize}
+};
+
+use artemis_core::Commitments;
+
+#[test]
+fn foo() {
+	new_test_ext().execute_with(|| {
+		CommitmentsModule::add(H160::zero(), &[0u8; 80]);
+
+		<CommitmentsModule as OnInitialize<u64>>::on_initialize(20);
+
+//		assert_eq!(CommitmentsModule::something(), Some(42));
+	});
+}
