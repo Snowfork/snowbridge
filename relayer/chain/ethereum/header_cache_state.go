4f1f66df02a076bb54fb4234f3e33672d5f03377
Rizmari Versfeld <rizziepit@gmail.com>
2020-12-08 17:30:40 -0800

    Construct and cache receipt trie for block

--- relayer/chain/ethereum/header_cache_state.go
+++ relayer/chain/ethereum/header_cache_state.go
@@ -4,12 +4,99 @@
 package ethereum
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"math"
 	"sync"
 
+	gethCommon "github.com/ethereum/go-ethereum/common"
+	gethTypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+	gethTrie "github.com/ethereum/go-ethereum/trie"
 	"github.com/tranvictor/ethashproof"
 	"golang.org/x/sync/errgroup"
 )
+
+type BlockLoader interface {
+	GetBlock(ctx context.Context, hash gethCommon.Hash) (*gethTypes.Block, error)
+	GetAllReceipts(ctx context.Context, block *gethTypes.Block) (gethTypes.Receipts, error)
+}
+
+type DefaultBlockLoader struct {
+	conn *Connection
+}
+
+func (d *DefaultBlockLoader) GetBlock(ctx context.Context, hash gethCommon.Hash) (*gethTypes.Block, error) {
+	return d.conn.client.BlockByHash(ctx, hash)
+}
+
+func (d *DefaultBlockLoader) GetAllReceipts(ctx context.Context, block *gethTypes.Block) (gethTypes.Receipts, error) {
+	return GetAllReceipts(ctx, d.conn, block)
+}
+
+// Keeps the blocks and receipts for the latest block heights / numbers
+// in memory (up to `capacity` block numbers)
+type BlockCache struct {
+	capacity       int
+	hashesByNumber map[uint64][]string
+	blocks         map[string]*gethTypes.Block
+	receiptTries   map[string]*gethTrie.Trie
+}
+
+func NewBlockCache(capacity int) *BlockCache {
+	return &BlockCache{
+		capacity:       capacity,
+		hashesByNumber: make(map[uint64][]string, capacity),
+		blocks:         make(map[string]*gethTypes.Block, capacity),
+		receiptTries:   make(map[string]*gethTrie.Trie, capacity),
+	}
+}
+
+func (bc *BlockCache) Insert(block *gethTypes.Block, receiptTrie *gethTrie.Trie) {
+	hash := block.Hash().Hex()
+	_, exists := bc.blocks[hash]
+	if exists {
+		return
+	}
+
+	number := block.Number().Uint64()
+	hashesAtNumber, numberExists := bc.hashesByNumber[number]
+	// Remove oldest blocks if we've reached capacity
+	if !numberExists && len(bc.hashesByNumber) == bc.capacity {
+		var minNumber uint64 = math.MaxUint64
+		for number := range bc.hashesByNumber {
+			if number < minNumber {
+				minNumber = number
+			}
+		}
+
+		hashesToRemove := bc.hashesByNumber[minNumber]
+		delete(bc.hashesByNumber, minNumber)
+		for _, hashToRemove := range hashesToRemove {
+			delete(bc.blocks, hashToRemove)
+			delete(bc.receiptTries, hashToRemove)
+		}
+
+	}
+
+	bc.blocks[hash] = block
+	bc.receiptTries[hash] = receiptTrie
+	if numberExists {
+		bc.hashesByNumber[number] = append(hashesAtNumber, hash)
+	} else {
+		bc.hashesByNumber[number] = []string{hash}
+	}
+}
+
+func (bc *BlockCache) Get(hash gethCommon.Hash) (*gethTypes.Block, *gethTrie.Trie, bool) {
+	hashHex := hash.Hex()
+	block, exists := bc.blocks[hashHex]
+	if exists {
+		return block, bc.receiptTries[hashHex], true
+	}
+	return nil, nil, false
+}
 
 type EthashproofCacheLoader interface {
 	MakeCache(epoch uint64) (*ethashproof.DatasetMerkleTreeCache, error)
@@ -38,9 +125,11 @@ type EthashproofCacheState struct {
 	nextCache    *ethashproof.DatasetMerkleTreeCache
 }
 
-// HeaderCacheState warms up proof caches as we move along
-// the Ethereum chain
+// HeaderCacheState fetches and caches data we need to construct proofs
+// as we move along the Ethereum chain.
 type HeaderCacheState struct {
+	blockLoader            BlockLoader
+	blockCache             *BlockCache
 	ethashproofCacheLoader EthashproofCacheLoader
 	ethashproofCacheState  *EthashproofCacheState
 	eg                     *errgroup.Group
@@ -49,36 +138,72 @@ type HeaderCacheState struct {
 func NewHeaderCacheState(
 	eg *errgroup.Group,
 	initBlockHeight uint64,
-	cl EthashproofCacheLoader,
+	bl BlockLoader,
+	ecl EthashproofCacheLoader,
 ) (*HeaderCacheState, error) {
-	cacheState := EthashproofCacheState{
+	blockCache := NewBlockCache(5)
+	blockLoader := bl
+	if blockLoader == nil {
+		return nil, fmt.Errorf("BlockLoader param is nil")
+	}
+
+	ethashproofCacheState := EthashproofCacheState{
 		currentCache: nil,
 		nextCache:    nil,
 	}
 
-	cacheLoader := cl
-	if cacheLoader == nil {
-		cacheLoader = &DefaultCacheLoader{}
+	ethashproofCacheLoader := ecl
+	if ethashproofCacheLoader == nil {
+		ethashproofCacheLoader = &DefaultCacheLoader{}
 	}
 
 	state := HeaderCacheState{
-		ethashproofCacheLoader: cacheLoader,
-		ethashproofCacheState:  &cacheState,
+		blockCache:             blockCache,
+		blockLoader:            blockLoader,
+		ethashproofCacheLoader: ethashproofCacheLoader,
+		ethashproofCacheState:  &ethashproofCacheState,
 		eg:                     eg,
 	}
 
 	// Block until cache for current epoch is prepared
-	cache, err := cacheLoader.MakeCache(initBlockHeight / 30000)
+	cache, err := ethashproofCacheLoader.MakeCache(initBlockHeight / 30000)
 	if err != nil {
 		return nil, err
 	}
-	cacheState.currentCache = cache
+	ethashproofCacheState.currentCache = cache
 	// Asynchronously prepare next epoch's cache
 	eg.Go(func() error {
 		return state.prepareNextEthashproofCache()
 	})
 
 	return &state, nil
+}
+
+// GetReceiptTrie returns a Merkle Patricia trie constructed from the receipts
+// of the block specified by `hash`. If the trie isn't cached, it will block for
+// multiple seconds to fetch receipts and construct the trie.
+func (s *HeaderCacheState) GetReceiptTrie(ctx context.Context, hash gethCommon.Hash) (*gethTrie.Trie, error) {
+	_, receiptTrie, exists := s.blockCache.Get(hash)
+	if exists {
+		return receiptTrie, nil
+	}
+
+	block, err := s.blockLoader.GetBlock(ctx, hash)
+	if err != nil {
+		return nil, err
+	}
+
+	receipts, err := s.blockLoader.GetAllReceipts(ctx, block)
+	if err != nil {
+		return nil, err
+	}
+	receiptTrie = s.makeTrie(receipts)
+	if receiptTrie.Hash() != block.ReceiptHash() {
+		return nil, fmt.Errorf("Receipt trie does not match block receipt hash")
+	}
+
+	s.blockCache.Insert(block, receiptTrie)
+	return receiptTrie, nil
 }
 
 // GetEthashProofCache returns the cache used for proof generation. It will return
@@ -130,6 +255,17 @@ func (s *HeaderCacheState) GetEthashproofCache(number uint64) (*ethashproof.Data
 	})
 
 	return cacheState.currentCache, nil
+}
+
+func (s *HeaderCacheState) makeTrie(items gethTypes.DerivableList) *gethTrie.Trie {
+	keybuf := new(bytes.Buffer)
+	trie := new(gethTrie.Trie)
+	for i := 0; i < items.Len(); i++ {
+		keybuf.Reset()
+		rlp.Encode(keybuf, uint(i))
+		trie.Update(keybuf.Bytes(), items.GetRlp(i))
+	}
+	return trie
 }
 
 func (s *HeaderCacheState) prepareNextEthashproofCache() error {
