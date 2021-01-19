0c6c6eae11fed949aa286894debfbb8cd8bcd0a3
Rizmari Versfeld <rizziepit@gmail.com>
2021-01-18 18:32:49 -0800

    Use release build of Sub, header trace logging

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -38,12 +38,12 @@ start_parachain()
 {
     echo "Starting Parachain"
     pushd ../parachain
-    bin=$(pwd)/target/debug/artemis
+    bin=$(pwd)/target/release/artemis
 
-    cargo build --features "test-e2e"
+    cargo build --release --features "test-e2e"
 
     echo "Generating Parachain spec"
-    target/debug/artemis build-spec --disable-default-bootnode > $configdir/spec.json
+    target/release/artemis build-spec --disable-default-bootnode > $configdir/spec.json
 
     echo "Inserting Ganache chain info into genesis spec"
     ethereum_initial_header=$(curl http://localhost:8545 \
@@ -53,19 +53,29 @@ start_parachain()
         | node ../test/scripts/helpers/transformEthHeader.js)
     node ../test/scripts/helpers/overrideParachainSpec.js $configdir/spec.json \
         genesis.runtime.verifierLightclient.initialDifficulty 0x0 \
-        genesis.runtime.verifierLightclient.initialHeader "$ethereum_initial_header"
+        genesis.runtime.verifierLightclient.initialHeader "$ethereum_initial_header" \
+        genesis.runtime.parachainInfo.parachainId 200 \
+        para_id 200
 
     echo "Writing Polkadot configuration"
     cp config.json $configdir/polkadotConfig.json
     parachain_conf="{
-		\"bin\": \"$bin\",
-		\"id\": \"200\",
-		\"wsPort\": 11144,
-		\"port\": 31200,
-		\"balance\": \"1000000000000000000000\",
-		\"flags\": [\"--discover-local\", \"--\", \"--execution=wasm\"],
-		\"chain\": \"$configdir/spec.json\"
-	}"
+        \"bin\": \"$bin\",
+        \"id\": \"200\",
+        \"wsPort\": 11144,
+        \"port\": 31200,
+        \"balance\": \"1000000000000000000000\",
+        \"flags\": [
+            \"-lbasic_authorship=trace,import_header=trace,runtime=debug,txpool=trace\",
+            \"--rpc-cors=all\",
+            \"--offchain-worker=Always\",
+            \"--enable-offchain-indexing=true\",
+            \"--discover-local\",
+            \"--\",
+            \"--execution=wasm\"
+        ],
+        \"chain\": \"$configdir/spec.json\"
+    }"
     node ../test/scripts/helpers/overrideParachainSpec.js $configdir/polkadotConfig.json \
         parachains.0 "$parachain_conf"
 
@@ -76,7 +86,6 @@ start_parachain()
     scripts/wait-for-it.sh -t 20 localhost:11144
     sleep 5
 
-    echo "Parachain PID: $!"
 }
 
 start_relayer()
@@ -107,13 +116,16 @@ start_relayer
 echo "Process Tree:"
 pstree $$
 
+sleep 3
 until $(grep "Syncing headers starting..." $(pwd)/relay.log > /dev/null); do
     echo "Waiting for relayer to generate DAG cache..."
     sleep 20
 done
 
-echo "Waiting for relayer to sync headers..."
-sleep 10
+until $(grep "Done retrieving finalized headers" $(pwd)/relay.log > /dev/null); do
+    echo "Waiting for relayer to sync headers..."
+    sleep 5
+done
 
 echo "System has been initialized and E2E tests can be executed"
 
