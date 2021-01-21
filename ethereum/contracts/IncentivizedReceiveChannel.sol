9b3e7d41b398909f0c1a1f20d4518e0d3c5afd5e
Aidan Musnitzky <musnit@gmail.com>
2021-01-21 19:05:34 +0200

    fixes from denali PR

--- ethereum/contracts/IncentivizedReceiveChannel.sol
+++ ethereum/contracts/IncentivizedReceiveChannel.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 contract IncentivizedReceiveChannel {
     uint256 public lastProcessedNonce;
 
-    // MAX_PAYLOAD_SIZE = 1000 bytes;
-    // MAX_PAYLOAD_COST = 500000gas;
-    // MAX_GAS_PER_MESSAGE = externallCallCost + max_payload_cost + max_processing_cost;
+    uint256 public MAX_PAYLOAD_BYTE_SIZE = 1000;
+    uint256 public MAX_PAYLOAD_GAS_COST = 500000;
+    uint256 public EXTERNAL_CALL_COST = 21000;
+    uint256 public MAX_GAS_PER_MESSAGE =
+        EXTERNAL_CALL_COST + MAX_PAYLOAD_BYTE_SIZE + EXTERNAL_CALL_COST;
 
     struct Message {
         uint256 nonce;
@@ -73,30 +75,57 @@ contract IncentivizedReceiveChannel {
         // require(commitment == hash(commitmentContents))
 
         // Require there is enough gas to play all messages
-        // require(leftovergas == commitmentContents.length * MAX_GAS_PER_MESSAGE)
+        require(
+            gasleft() >= commitmentContents.messages.length * MAX_GAS_PER_MESSAGE,
+            "insufficient gas for delivery of all messages"
+        );
 
         // Require all payloads are smaller than max_payload_size
-        //for(message in commitmentContents) {
-        //require(size(message.payload) <= MAX_PAYLOAD_SIZE )
-        //}
+        for (uint256 i = 0; i < commitmentContents.messages.length; i++) {
+            require(
+                commitmentContents.messages[i].payload.length <= MAX_PAYLOAD_BYTE_SIZE,
+                "message payload bytesize exceeds maximum payload size"
+            );
+        }
         return true;
     }
 
     function processCommitmentContents(
         CommitmentContents memory commitmentContents
     ) internal {
-        // for(message in commitmentContents) {
-        // Check message nonce is correct and increment nonce for replay protection
-        // require(message.nonce == lastProcessedNonce+1)
-        // lastProcessedNonce = lastProcessedNonce + 1
-        // Deliver the message to the destination
-        // Delivery will have fixed maximum gas allowed for the destination app.
-        // g = MAX_GAS_PER_MESSAGE
-        // in = payload;
-        // ...
-        // call(g, a, v, in, insize, out, outsize)
-        // call is expected to be fire and forget. if the call reverts, runs out of gas, error,
-        // etc, its the fauly of the application and we don't care
-        // }
+        for (uint256 i = 0; i < commitmentContents.length; i++) {
+          // Check message nonce is correct and increment nonce for replay protection
+          Message memory message = commitmentContents[i];
+          require(message.nonce == lastProcessedNonce + 1, "invalid nonce");
+
+          lastProcessedNonce =  lastProcessedNonce + 1;
+
+
+          // Deliver the message to the destination
+          // Delivery will have fixed maximum gas allowed for the destination app.
+          // TODO: payload needs format: [bytes4(sha3("function_name(arg1_type,arg2_type)")) + bytes input]
+          bytes4 sig = bytes4(sha3("add(int256,int256)"));
+
+
+          assembly {
+            let x := mload(0x40)  // Find empty storage location using "free memory pointer"
+            mstore(x,sig)         // Place signature at begining of empty storage
+            mstore(add(x,0x04),a) // Place first argument directly next to signature
+            mstore(add(x,0x24),b) // Place second argument next to first, padded to 32 bytes
+
+            // Dispatch call to the receiver - it is expected to be fire and forget. If the call reverts, runs out of gas, error,
+            // etc, its the fault of the sender
+            let success := call(                                    // Pop the top stack value
+                                MAX_GAS_PER_MESSAGE,                // Allowed gas
+                                message.targetApplicationAddress,   // To addr
+                                0,                                  // No value
+                                x,                                  // Inputs are stored at location x
+                                0x44,                               // Inputs are 68 bytes long
+                                x,                                  // Store output over input (saves space)
+                                0x20)                               // Outputs are 32 bytes long
+
+            c := mload(x)                                           // Assign output value to c
+            mstore(0x40,add(x,0x44))                                // Set storage pointer to empty space
+        }
     }
 }
