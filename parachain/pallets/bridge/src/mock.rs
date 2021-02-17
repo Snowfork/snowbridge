72aee2417902221c2921c02ba75f76c5f872bd9e
Vincent Geddes <vincent@snowfork.com>
2021-02-17 14:06:15 +0200

    Update remaining tests

--- parachain/pallets/bridge/src/mock.rs
+++ parachain/pallets/bridge/src/mock.rs
@@ -1,48 +1,39 @@
 
 use super::*;
 
-use crate::Config;
 use sp_core::H256;
-use frame_support::{impl_outer_origin, impl_outer_event, parameter_types,
-	weights::Weight,
+use frame_support::{parameter_types,
 	dispatch::DispatchError
 };
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup, IdentifyAccount, Verify}, testing::Header, Perbill, MultiSignature
+	traits::{BlakeTwo256, IdentityLookup, IdentifyAccount, Verify}, testing::Header, MultiSignature
 };
 use sp_std::convert::From;
-use frame_system as system;
 
 use artemis_core::{MessageCommitment, MessageDispatch, ChannelId, MessageId, SourceChannel, SourceChannelConfig};
 use artemis_ethereum::Log;
 
-impl_outer_origin! {
-	pub enum Origin for Test {}
-}
+use crate as bridge;
 
-mod test_events {
-    pub use crate::Event;
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
 
-impl_outer_event! {
-    pub enum TestEvent for Test {
-		system<T>,
-        test_events,
-    }
-}
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Storage, Event<T>},
+		Bridge: bridge::{Module, Call, Storage, Event},
+	}
+);
 
 pub type Signature = MultiSignature;
-
 pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
-
-#[derive(Clone, Eq, PartialEq)]
-pub struct Test;
 
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: Weight = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
 }
 
 impl system::Config for Test {
@@ -50,7 +41,7 @@ impl system::Config for Test {
 	type BlockWeights = ();
 	type BlockLength = ();
 	type Origin = Origin;
-	type Call = ();
+	type Call = Call;
 	type Index = u64;
 	type BlockNumber = u64;
 	type Hash = H256;
@@ -58,11 +49,11 @@ impl system::Config for Test {
 	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
 	type DbWeight = ();
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
@@ -95,18 +86,14 @@ impl MessageDispatch<MessageId> for MockMessageDispatch {
 
 
 impl Config for Test {
-	type Event = TestEvent;
+	type Event = Event;
 	type Verifier = MockVerifier;
 	type MessageCommitment = MockMessageCommitment;
 	type MessageDispatch = MockMessageDispatch;
 }
 
-pub type System = system::Module<Test>;
-pub type Bridge = Module<Test>;
-
-
 pub fn new_tester() -> sp_io::TestExternalities {
-	new_tester_with_config::<Test>(GenesisConfig {
+	new_tester_with_config(bridge::GenesisConfig {
 		source_channels: SourceChannelConfig {
 			basic: SourceChannel {
 				address: H160::zero(),
@@ -119,7 +106,7 @@ pub fn new_tester() -> sp_io::TestExternalities {
 }
 
 pub fn new_tester_with_source_channels(basic: H160, incentivized: H160) -> sp_io::TestExternalities {
-	new_tester_with_config::<Test>(GenesisConfig {
+	new_tester_with_config(bridge::GenesisConfig {
 		source_channels: SourceChannelConfig {
 			basic: SourceChannel {
 				address: basic,
@@ -131,8 +118,8 @@ pub fn new_tester_with_source_channels(basic: H160, incentivized: H160) -> sp_io
 	})
 }
 
-pub fn new_tester_with_config<T: Config>(config: GenesisConfig) -> sp_io::TestExternalities {
-	let mut storage = system::GenesisConfig::default().build_storage::<T>().unwrap();
+pub fn new_tester_with_config(config: bridge::GenesisConfig) -> sp_io::TestExternalities {
+	let mut storage = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
 
 	config.assimilate_storage(&mut storage).unwrap();
 
