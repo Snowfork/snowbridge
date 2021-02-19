3f5efebaa772de58a8ff01875cd279c8fd06cb26
Bhushan Lodha <bhushanlodha@gmail.com>
2021-02-19 14:53:09 +0530

    Added fee on ourbound message Added gasFee on contract

--- ethereum/contracts/IncentivizedOutboundChannel.sol
+++ ethereum/contracts/IncentivizedOutboundChannel.sol
@@ -7,9 +7,20 @@ import "./OutboundChannel.sol";
 // IncentivizedOutboundChannel is a channel that sends ordered messages with an increasing nonce. It will have incentivization too.
 contract IncentivizedOutboundChannel is OutboundChannel {
 
+    uint256 gasFee;
+    address feeController;
+
     constructor() {
         nonce = 0;
+        gasFee = 0;
     }
+
+    event Message(
+        address source,
+        uint64 nonce,
+        bytes payload,
+        uint256 fee
+    );
 
     /**
      * @dev Sends a message across the channel
@@ -19,6 +30,16 @@ contract IncentivizedOutboundChannel is OutboundChannel {
         override
     {
         nonce = nonce + 1;
-        emit Message(msg.sender, nonce, payload);
+        emit Message(msg.sender, nonce, payload, msg.value);
+    }
+
+    modifier onlyFeeController {
+        require(msg.sender == feeController);
+        _;
+    }
+
+    function setGasFee(uint256 _fee) public onlyFeeController {
+        require(_fee > 0, "fee must be positive");
+        gasFee = _fee;
     }
 }
