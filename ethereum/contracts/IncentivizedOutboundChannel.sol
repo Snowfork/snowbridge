edce83d5a5f792b33b31c427eb074a93a0fcf5fc
Bhushan Lodha <bhushanlodha@gmail.com>
2021-02-22 22:29:41 +0530

    modified gasfee

--- ethereum/contracts/IncentivizedOutboundChannel.sol
+++ ethereum/contracts/IncentivizedOutboundChannel.sol
@@ -7,9 +7,21 @@ import "./OutboundChannel.sol";
 // IncentivizedOutboundChannel is a channel that sends ordered messages with an increasing nonce. It will have incentivization too.
 contract IncentivizedOutboundChannel is OutboundChannel {
 
-    constructor() {
+    uint256 public gasFee;
+    address public feeController;
+
+    constructor(uint256 _gasFee, address _feeController) {
         nonce = 0;
+        gasFee = _gasFee;
+        feeController = _feeController;
     }
+
+    event Message(
+        address source,
+        uint64 nonce,
+        bytes payload,
+        uint256 fee
+    );
 
     /**
      * @dev Sends a message across the channel
@@ -19,6 +31,26 @@ contract IncentivizedOutboundChannel is OutboundChannel {
         override
     {
         nonce = nonce + 1;
-        emit Message(msg.sender, nonce, payload);
+        emit Message(msg.sender, nonce, payload, msg.value);
+    }
+
+    modifier onlyFeeController {
+        require(msg.sender == feeController, "Caller is not fee controller");
+        _;
+    }
+
+    /**
+    * @dev Sets gasFee. Only feeController is allowed to set gasFee
+    */
+    function setGasFee(uint256 _fee) public onlyFeeController {
+        require(_fee > 0, "fee must be positive");
+        gasFee = _fee;
+    }
+
+    /**
+    * @dev Change feeController address
+    */
+    function setFeeController(address _feeControllerAddress) public onlyFeeController {
+        feeController = _feeControllerAddress;
     }
 }
