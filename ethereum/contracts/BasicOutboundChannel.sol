dc4a64491218ef1be2662ee109aa2113e894a4e3
Vladislav Manchev <v@bin.bz>
2021-02-23 17:18:43 +0200

    WIP: Use tx.origin instead of msg.sender when emitting message for relayer

--- ethereum/contracts/BasicOutboundChannel.sol
+++ ethereum/contracts/BasicOutboundChannel.sol
@@ -15,6 +15,6 @@ contract BasicOutboundChannel is OutboundChannel {
      * @dev Sends a message across the channel
      */
     function submit(bytes memory payload) public override {
-        emit Message(msg.sender, ++account_nonces[msg.sender], payload);
+        emit Message(tx.origin, ++account_nonces[tx.origin], payload);
     }
 }
