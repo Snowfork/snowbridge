a3f0474f6d31ffd8a8a0ff3a53cd85b1553fbd7a
denalimarsh <denalimarsh@gmail.com>
2021-03-14 18:01:23 +0000

    implement WriteCompleteSignatureCommitment

--- relayer/chain/parachain/writer.go
+++ relayer/chain/parachain/writer.go
@@ -19,6 +19,7 @@ import (
 	"github.com/snowfork/polkadot-ethereum/relayer/chain"
 	"github.com/snowfork/polkadot-ethereum/relayer/chain/ethereum"
 	"github.com/snowfork/polkadot-ethereum/relayer/contracts/lightclientbridge"
+	"github.com/snowfork/polkadot-ethereum/relayer/parachain"
 )
 
 const LightClientBridgeContractID = "lightclientbridge"
@@ -28,15 +29,18 @@ type Writer struct {
 	conn      *ethereum.Connection
 	contracts map[string]*lightclientbridge.Contract
 	messages  <-chan []chain.Message
+	beefy     chan parachain.BeefyCommitmentInfo
 	log       *logrus.Entry
 }
 
-func NewWriter(config *Config, conn *ethereum.Connection, messages <-chan []chain.Message, contracts map[string]*lightclientbridge.Contract, log *logrus.Entry) (*Writer, error) {
+func NewWriter(config *Config, conn *ethereum.Connection, messages <-chan []chain.Message, beefy chan parachain.BeefyCommitmentInfo,
+	contracts map[string]*lightclientbridge.Contract, log *logrus.Entry) (*Writer, error) {
 	return &Writer{
 		config:    config,
 		conn:      conn,
 		contracts: contracts,
 		messages:  messages,
+		beefy:     beefy,
 		log:       log,
 	}, nil
 }
@@ -52,6 +56,7 @@ func (wr *Writer) Start(ctx context.Context, eg *errgroup.Group) error {
 	eg.Go(func() error {
 		return wr.writeLoop(ctx)
 	})
+
 	return nil
 }
 
@@ -71,22 +76,28 @@ func (wr *Writer) writeLoop(ctx context.Context) error {
 			return wr.onDone(ctx)
 		case msgs := <-wr.messages:
 			for _, msg := range msgs {
+				beefyInfo, ok := msg.(parachain.BeefyCommitmentInfo)
+				if !ok {
+					return fmt.Errorf("Invalid message")
+				}
 
-				switch msg.(type) {
-				case chain.NewSignatureCommitmentMessage:
-					concreteMsg, ok := msg.(chain.NewSignatureCommitmentMessage)
-					if !ok {
-						return fmt.Errorf("Invalid message")
+				switch beefyInfo.Status {
+				case parachain.CommitmentWitnessed:
+					err := wr.WriteNewSignatureCommitment(ctx, beefyInfo)
+					if err != nil {
+						wr.log.WithError(err).Error("Error submitting message to ethereum")
 					}
-
-					err := wr.WriteNewSignatureCommitment(ctx, &concreteMsg)
+				// TODO: will these cases even be hit? it's in a different channel.
+				case parachain.InitialVerificationTxSent, parachain.InitialVerificationTxConfirmed:
+					continue // Ethereum listener is responsible for checking tx confirmation
+				case parachain.ReadyToComplete:
+					err := wr.WriteCompleteSignatureCommitment(ctx, beefyInfo)
 					if err != nil {
 						wr.log.WithError(err).Error("Error submitting message to ethereum")
 					}
 				default:
-					wr.log.Info("Unsupported message type")
+					wr.log.Info("Invalid beefy commitment status")
 				}
-				// TODO: add case for CompleteSignatureCommitmentMessage
 			}
 		}
 	}
@@ -100,9 +111,13 @@ func (wr *Writer) signerFn(_ common.Address, tx *types.Transaction) (*types.Tran
 	return signedTx, nil
 }
 
-// Submit sends a SCALE-encoded message to an application deployed on the Ethereum network
-func (wr *Writer) WriteNewSignatureCommitment(ctx context.Context, msg *chain.NewSignatureCommitmentMessage) error {
+func (wr *Writer) WriteNewSignatureCommitment(ctx context.Context, beefyInfo parachain.BeefyCommitmentInfo) error {
 	wr.log.Info("Parachain writer received msg")
+
+	msg, err := beefyInfo.BuildNewSignatureCommitmentMessage()
+	if err != nil {
+		return err
+	}
 
 	contract := wr.contracts[LightClientBridgeContractID] // TODO: don't hardcode this
 	if contract == nil {
@@ -126,7 +141,47 @@ func (wr *Writer) WriteNewSignatureCommitment(ctx context.Context, msg *chain.Ne
 
 	wr.log.WithFields(logrus.Fields{
 		"txHash": tx.Hash().Hex(),
-	}).Info("Transaction submitted")
+	}).Info("New Signature Commitment transaction submitted")
+
+	beefyInfo.Status = parachain.InitialVerificationTxSent
+	beefyInfo.InitialVerificationTxHash = tx.Hash()
+	wr.beefy <- beefyInfo
+
+	return nil
+}
+
+// WriteCompleteSignatureCommitment sends a CompleteSignatureCommitment tx to the LightClientBridge contract
+func (wr *Writer) WriteCompleteSignatureCommitment(ctx context.Context, beefyInfo parachain.BeefyCommitmentInfo) error {
+	wr.log.Info("Parachain writer received msg")
+
+	msg, err := beefyInfo.BuildCompleteSignatureCommitmentMessage()
+	if err != nil {
+		return err
+	}
+
+	contract := wr.contracts[LightClientBridgeContractID] // TODO: don't hardcode this
+	if contract == nil {
+		return fmt.Errorf("Unknown contract")
+	}
+
+	options := bind.TransactOpts{
+		From:     wr.conn.GetKeyPair().CommonAddress(),
+		Signer:   wr.signerFn,
+		Context:  ctx,
+		GasLimit: 5000000, // TODO: reasonable gas limit
+	}
+
+	tx, err := contract.CompleteSignatureCommitment(&options, msg.ID, msg.Payload, msg.RandomSignatureCommitments,
+		msg.RandomSignatureBitfieldPositions, msg.RandomValidatorAddresses, msg.RandomPublicKeyMerkleProofs)
+
+	if err != nil {
+		wr.log.WithError(err).Error("Failed to submit transaction")
+		return err
+	}
+
+	wr.log.WithFields(logrus.Fields{
+		"txHash": tx.Hash().Hex(),
+	}).Info("Complete Signature Commitment transaction submitted")
 
 	return nil
 }
