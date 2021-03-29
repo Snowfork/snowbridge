9859a0ca2078bbdcacb6d1309a9bd9b7a35a7747
denalimarsh <denalimarsh@gmail.com>
2021-03-29 22:32:54 +0000

    refactor relay.go

--- relayer/core/relay.go
+++ relayer/core/relay.go
@@ -19,15 +19,14 @@ import (
 
 	"github.com/snowfork/polkadot-ethereum/relayer/chain"
 	"github.com/snowfork/polkadot-ethereum/relayer/chain/ethereum"
-	"github.com/snowfork/polkadot-ethereum/relayer/chain/relaychain"
 	"github.com/snowfork/polkadot-ethereum/relayer/chain/substrate"
-	relaychaintypes "github.com/snowfork/polkadot-ethereum/relayer/relaychain"
+	"github.com/snowfork/polkadot-ethereum/relayer/store"
 )
 
 type Relay struct {
-	subChain    chain.Chain
-	ethSubChain chain.Chain
-	relayChain  chain.Chain
+	subChain chain.Chain
+	ethChain chain.Chain
+	database *store.Database
 }
 
 type Direction int
@@ -44,10 +43,9 @@ type RelayConfig struct {
 }
 
 type Config struct {
-	Relay      RelayConfig       `mapstructure:"relay"`
-	Eth        ethereum.Config   `mapstructure:"ethereum"`
-	Sub        substrate.Config  `mapstructure:"substrate"`
-	Relaychain relaychain.Config `mapstructure:"relaychain"`
+	Relay RelayConfig      `mapstructure:"relay"`
+	Eth   ethereum.Config  `mapstructure:"ethereum"`
+	Sub   substrate.Config `mapstructure:"substrate"`
 }
 
 func NewRelay() (*Relay, error) {
@@ -56,20 +54,34 @@ func NewRelay() (*Relay, error) {
 		return nil, err
 	}
 
+	// TODO: integrate with config
+	configJson := `
+	{"db_config": {
+			"dialect": "sqlite3",
+			"db_path": "./tmp.db"
+		}
+	}`
+	dbConfig := store.ParseConfigFromJson(configJson)
+
+	db, err := store.PrepareDatabase(dbConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	beefyMessages := make(chan store.DatabaseCmd, 1)
+	logger := log.WithField("database", "Beefy")
+	database := store.NewDatabase(db, beefyMessages, logger)
+
 	subChain, err := substrate.NewChain(&config.Sub)
 	if err != nil {
 		return nil, err
 	}
 
-	ethSubChain, err := ethereum.NewChain(&config.Eth)
+	ethChain, err := ethereum.NewChain(&config.Eth, database)
 	if err != nil {
 		return nil, err
 	}
 
-	relayChain, err := relaychain.NewChain(&config.Relaychain)
-	if err != nil {
-		return nil, err
-	}
 	direction := config.Relay.Direction
 	headersOnly := config.Relay.HeadersOnly
 	if direction == Bidirectional || direction == EthToSub {
@@ -82,11 +94,11 @@ func NewRelay() (*Relay, error) {
 		// can guarantee that a header is forwarded before we send dependent messages)
 		ethHeaders := make(chan chain.Header)
 
-		err = subChain.SetReceiver(ethMessages, ethHeaders, nil)
+		err = subChain.SetReceiver(ethMessages, ethHeaders, beefyMessages)
 		if err != nil {
 			return nil, err
 		}
-		err = ethSubChain.SetSender(ethMessages, ethHeaders, nil)
+		err = ethChain.SetSender(ethMessages, ethHeaders, beefyMessages)
 		if err != nil {
 			return nil, err
 		}
@@ -99,38 +111,20 @@ func NewRelay() (*Relay, error) {
 			subMessages = make(chan []chain.Message, 1)
 		}
 
-		err := subChain.SetSender(subMessages, nil, nil)
+		err := subChain.SetSender(subMessages, nil, beefyMessages)
 		if err != nil {
 			return nil, err
 		}
-		err = ethSubChain.SetReceiver(subMessages, nil, nil)
+		err = ethChain.SetReceiver(subMessages, nil, beefyMessages)
 		if err != nil {
 			return nil, err
 		}
-	}
-
-	beefyMessages := make(chan relaychaintypes.BeefyCommitmentInfo, 1)
-
-	var relayMessages chan []chain.Message
-	if !headersOnly {
-		relayMessages = make(chan []chain.Message, 1)
-	}
-	err = relayChain.SetSender(relayMessages, nil, beefyMessages)
-	if err != nil {
-		return nil, err
-	}
-
-	// Write to PolkadotRelayChainBridge contract on Ethereum
-	ethHeadersTwo := make(chan chain.Header)
-	err = relayChain.SetReceiver(relayMessages, ethHeadersTwo, beefyMessages)
-	if err != nil {
-		return nil, err
 	}
 
 	return &Relay{
-		subChain:    subChain,
-		ethSubChain: ethSubChain,
-		relayChain:  relayChain,
+		subChain: subChain,
+		ethChain: ethChain,
+		database: database,
 	}, nil
 }
 
@@ -156,24 +150,31 @@ func (re *Relay) Start() {
 		return nil
 	})
 
+	err := re.database.Start(ctx, eg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"database": "Beefy",
+			"error":    err,
+		}).Error("Failed to start database")
+		return
+	}
+	log.WithField("database", "Beefy").Info("Started database")
+
 	// Short-lived channels that communicate initialization parameters
 	// between the two chains. The chains close them after startup.
 	subInit := make(chan chain.Init, 1)
 	ethSubInit := make(chan chain.Init, 1)
 
-	paraInit := make(chan chain.Init, 1)
-	ethParaInit := make(chan chain.Init, 1)
-
-	err := re.ethSubChain.Start(ctx, eg, subInit, ethSubInit)
+	err = re.ethChain.Start(ctx, eg, subInit, ethSubInit)
 	if err != nil {
 		log.WithFields(log.Fields{
-			"chain": re.ethSubChain.Name(),
+			"chain": re.ethChain.Name(),
 			"error": err,
 		}).Error("Failed to start chain")
 		return
 	}
-	log.WithField("name", re.ethSubChain.Name()).Info("Started chain")
-	defer re.ethSubChain.Stop()
+	log.WithField("name", re.ethChain.Name()).Info("Started chain")
+	defer re.ethChain.Stop()
 
 	err = re.subChain.Start(ctx, eg, ethSubInit, subInit)
 	if err != nil {
@@ -185,17 +186,6 @@ func (re *Relay) Start() {
 	}
 	log.WithField("name", re.subChain.Name()).Info("Started chain")
 	defer re.subChain.Stop()
-
-	err = re.relayChain.Start(ctx, eg, ethParaInit, paraInit)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"chain": re.relayChain.Name(),
-			"error": err,
-		}).Error("Failed to start chain")
-		return
-	}
-	log.WithField("name", re.relayChain.Name()).Info("Started chain")
-	defer re.relayChain.Stop()
 
 	notifyWaitDone := make(chan struct{})
 
@@ -225,9 +215,10 @@ func (re *Relay) Start() {
 		}
 
 		log.WithError(ctx.Err()).Error("Goroutines appear deadlocked. Killing process")
-		re.ethSubChain.Stop()
+		re.ethChain.Stop()
 		re.subChain.Stop()
-		re.relayChain.Stop()
+		// re.database.Stop() // TODO: graceful shutdown
+
 		relayProc, err := os.FindProcess(os.Getpid())
 		if err != nil {
 			log.WithError(err).Error("Failed to kill this process")
@@ -260,22 +251,20 @@ func LoadConfig() (*Config, error) {
 	}
 	config.Eth.PrivateKey = strings.TrimPrefix(value, "0x")
 
+	// TODO: auto populate contract addresses
+	config.Eth.Contracts.PolkadotRelayChainBridge = "0x8cF6147918A5CBb672703F879f385036f8793a24"
+	config.Eth.Contracts.ValidatorRegistry = "0xB1185EDE04202fE62D38F5db72F71e38Ff3E8305"
+	// TODO: query from 'BLOCK_WAIT_PERIOD' on RelayBridgeLightClient contract
+	config.Eth.BeefyBlockDelay = 5
+
 	value, ok = os.LookupEnv("ARTEMIS_SUBSTRATE_KEY")
 	if !ok {
 		return nil, fmt.Errorf("environment variable not set: ARTEMIS_SUBSTRATE_KEY")
 	}
-	config.Sub.PrivateKey = value
-
-	// TODO: use actual config
-	config.Relaychain.Relaychain.Endpoint = "ws://127.0.0.1:9944"
-	config.Relaychain.Relaychain.PrivateKey = "//Alice"
-	config.Relaychain.Ethereum.Endpoint = config.Eth.Endpoint
-	config.Relaychain.Ethereum.PrivateKey = config.Eth.PrivateKey
-	// TODO: auto populate contract addresses
-	config.Relaychain.Ethereum.Contracts.RelayBridgeLightClient = "0xB1185EDE04202fE62D38F5db72F71e38Ff3E8305"
-	config.Relaychain.Ethereum.Contracts.ValidatorRegistry = "0xEE9170ABFbf9421Ad6DD07F6BDec9D89F2B581E0"
-	// TODO: query from 'BLOCK_WAIT_PERIOD' on RelayBridgeLightClient contract
-	config.Relaychain.Ethereum.BeefyBlockDelay = 5
+	config.Sub.Parachain.PrivateKey = value
+	config.Sub.Parachain.Endpoint = "ws://127.0.0.1:11144"
+	config.Sub.Relaychain.Endpoint = "ws://127.0.0.1:9944"
+	config.Sub.Relaychain.PrivateKey = "//Alice" // TODO: proper configuration
 
 	return &config, nil
 }
