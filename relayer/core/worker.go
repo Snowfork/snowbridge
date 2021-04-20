34421b0ffe94f3380187552e5aa795329c6fb898
Rizmari Versfeld <rizziepit@gmail.com>
2021-04-20 21:21:36 +0000

    Worker interface + WorkerPool run logic

---
+++ relayer/core/worker.go
@@ -0,0 +1,91 @@
+// Copyright 2021 Snowfork
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package core
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type Worker interface {
+	Name() string
+
+	SetUp() error
+	TearDown()
+
+	Start(ctx context.Context, eg *errgroup.Group) error
+	Stop()
+
+}
+
+
+type WorkerPool struct {
+	workers []Worker
+}
+
+func NewWorkerPool(workers []Worker) *WorkerPool {
+	return &WorkerPool{
+		workers,
+	}
+}
+
+func (wp *WorkerPool) runWorker(ctx context.Context, worker Worker) error {
+	// Ensure we always clean up after ourselves
+	defer worker.TearDown()
+
+	err := worker.SetUp()
+	if err != nil {
+		return err
+	}
+
+	childEg, childCtx := errgroup.WithContext(ctx)
+	err = worker.Start(childCtx, childEg)
+	if err != nil {
+		return err
+	}
+
+	return childEg.Wait()
+}
+
+func (wp *WorkerPool) run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// Ensure clean termination upon SIGINT, SIGTERM
+	eg.Go(func() error {
+		notify := make(chan os.Signal, 1)
+		signal.Notify(notify, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-notify:
+			// TODO: add logging back in
+			//log.WithField("signal", sig.String()).Info("Received signal")
+			cancel()
+		}
+
+		return nil
+	})
+
+	for _, worker := range wp.workers {
+		eg.Go(func() error {
+			for {
+				// TODO: log starting worker
+				err := wp.runWorker(ctx, worker)
+				// TODO: log ending worker
+				if err != nil {
+					// TODO: retry with backoff up to X retries
+					return err
+				}
+			}
+		})
+	}
+
+	return eg.Wait()
+}
