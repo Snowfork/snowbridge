b15f0eb878ba098ba51414d0214c74cffd67340f
Aidan Musnitzky <musnit@gmail.com>
2021-04-21 15:44:59 +0000

    useful mage command

--- relayer/magefile.go
+++ relayer/magefile.go
@@ -26,3 +26,13 @@ func Lint() error {
 func Install() error {
 	return sh.Run("go", "build", "-o", "$GOPATH/bin/artemis-relay", "main.go")
 }
+
+func Dev() error {
+	cmd := "go"
+	env := map[string]string{
+		"ARTEMIS_ETHEREUM_KEY":   "0x935b65c833ced92c43ef9de6bff30703d941bd92a2637cb00cfad389f5862109",
+		"ARTEMIS_PARACHAIN_KEY":  "//Relay",
+		"ARTEMIS_RELAYCHAIN_KEY": "//Alice",
+	}
+	return sh.RunWithV(env, cmd, "run", "./main.go", "run", "--config", "$configdir/config.toml")
+}
