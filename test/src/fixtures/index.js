362c6222311996dc278a2dccfa71674542669ad6
Aidan Musnitzky <musnit@gmail.com>
2021-04-22 19:08:19 +0000

    done

--- test/src/fixtures/index.js
+++ test/src/fixtures/index.js
@@ -5,6 +5,7 @@ const SubClient = require('../../src/subclient').SubClient;
 const polkadotRecipient = "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d";
 const polkadotRecipientSS58 = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
 const polkadotSenderSS58 = polkadotRecipientSS58;
+const treasuryAddressSS58 = "5EYCAe5jHEaRUtbinpdbTLuTyGiVt2TJGQPi9fdvVpNLNfSS";
 const ethEndpoint = 'ws://localhost:8545';
 const parachainEndpoint = 'ws://localhost:11144';
 const testNetworkID = '344';
@@ -24,7 +25,7 @@ async function bootstrap() {
 
 module.exports = {
   bootstrap, polkadotRecipient,
-  polkadotRecipientSS58, polkadotSenderSS58,
+  polkadotRecipientSS58, polkadotSenderSS58, treasuryAddressSS58,
   TestTokenAddress,
   ETH_TO_PARA_WAIT_TIME, PARA_TO_ETH_WAIT_TIME
 };
