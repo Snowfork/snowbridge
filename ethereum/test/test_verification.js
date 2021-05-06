1062fe42fdb824cf3d1aa3225a37c5667de329f0
Vladislav Manchev <v@bin.bz>
2021-05-06 16:09:09 +0200

    WIP: Replace test data with relayer generated data

--- ethereum/test/test_verification.js
+++ ethereum/test/test_verification.js
@@ -5,6 +5,8 @@ const {
 } = require("./helpers");
 const ETHApp = artifacts.require("ETHApp");
 const { keccakFromHexString, keccak } = require("ethereumjs-util");
+const { blake2AsHex } = require("@polkadot/util-crypto");
+
 
 require("chai")
   .use(require("chai-as-promised"))
@@ -30,9 +32,9 @@ contract("IncentivizedInboundChannel", function (accounts) {
 
       this.lightClientBridge = await deployLightClientBridge(validatorsMerkleTree.getHexRoot());
       const newCommitment = await this.lightClientBridge.newSignatureCommitment(
-        "0xbe5e918869d703844eb1ae212e52162825737036f83ce8febb41a65ce3c41656",
+        blake2AsHex("0x8f667b0d8050a5d544d6fb139485847ea3f654bbbd065b08aad2f04d93ba2b7cae0000000000000000000000", 256),
         [parseBitfield("11")],
-        signatureSubstrateToEthereum("0x254b25cc1983e66edbc0e531f0ea11d848b27ccdacdc4cc44ac783aacaa7bf6d3a702aefb5e2250dacb647c22fa7710fadbbe42e36864635eec11fcd0d46788001"),
+        signatureSubstrateToEthereum("0xbf866f5a4c66d5716e736d8e4737c8386c59e47fb618a03fc4cc3716f8e0a0987eec8076ee5824600d6f84825d693e4e5dac8f90ca27d639b520ab435e2d221900"),
         0,
         "0xE04CC55ebEE1cBCE552f250e85c57B70B2E2625b",
         this.validator0PubKeyMerkleProof
@@ -41,8 +43,8 @@ contract("IncentivizedInboundChannel", function (accounts) {
       const currentId = await this.lightClientBridge.currentId();
       const completeCommitment = await this.lightClientBridge.completeSignatureCommitment(
         currentId.sub(new web3.utils.BN(1)),
-        "0xbe5e918869d703844eb1ae212e52162825737036f83ce8febb41a65ce3c41656",
-        [signatureSubstrateToEthereum("0xa97a20d7df37b94696534c668400213a2759deb70c868b17b101837c6a7966792c5ceb72665e459c2e632ebe1b41a21a5ecb253932027e7d1a5a45d43072a98700")],
+        blake2AsHex("0x8f667b0d8050a5d544d6fb139485847ea3f654bbbd065b08aad2f04d93ba2b7cae0000000000000000000000", 256),
+        [signatureSubstrateToEthereum("0x9646258c491fc1d32c03521d468b530ca0eb9f3acd0de9fea7255dc21ff63ddb2a45c06552a0f5cc079c9df7b320eca99c9819619a4efa816f1593cb908cd40901")],
         [1],
         ["0x25451A4de12dcCc2D166922fA938E900fCc4ED24"],
         [this.validator1PubKeyMerkleProof]
@@ -73,19 +75,20 @@ contract("IncentivizedInboundChannel", function (accounts) {
       };
       const messages = [messageOne, messageTwo];
       const commitment = buildCommitment(messages);
-      const leaf = "0xc101140000006246f3967d617df1412181ba09847c156a12750d0111fc34dc679797d79eeb1fbc36789e7a1e281436464229828f817d6612f7b477d66591ff96a9e064bcc98a01000000000000000200000007b13d25743592825cea32c9a24ba67b50b7e90d92cbd1d0f4eab2dc94dba5c6";
-      console.log(keccakFromHexString(leaf));
+      const leaf = "0x3c390519c0130a826f3b96ec71ef9ce43f9fbc67fb0fc22ab4aaa1b3293394f7";
       const proofs = [
-        "0x7ab091f10e52fc1aa828867cee14c250110c24ca7d1bc4481a911fcb83c1bac0",
-        "0x3c480c16b80148cdcd62385a3835fa93ad2119268429b7593f9df0f425f61934",
-        "0x023da63409fac7ff8427cdaf06e32fa94a968eb52df8e4b7c3b0b74e8b8f2af3"
+        "0xfe4a3df5f147d73b9edd54b6f665f68738a475243b0f5c583495b7fc34d7e127",
+        "0x890fc5d842bac18f3af8ddabea7a28e39cd05fe13a5132460aaa5ef5c27c5969",
+        "0xbe855ba734eef6617931c4bab11ce648892cad733df6a1c8287872eef656528a",
+        "0x4bcd5e2699bd1870f4a85e0c91ebab5198b419b71b89c81b14e251b57b0f4348",
+        "0xc1aad420d483a83b4dce6bf5e44c7af18653b46b0f6ca875ca0cb0ef6371adf7"
       ];
       const tx = await this.inbound.submit(
         messages,
         commitment,
-        keccakFromHexString(leaf),
-        20,
-        22,
+        leaf,
+        174,
+        175,
         proofs,
         { from: userOne }
       );
