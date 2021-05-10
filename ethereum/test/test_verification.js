445013c66bf0c29f46338db39f2d73c1fe2afc25
Vladislav Manchev <v@bin.bz>
2021-05-10 17:29:04 +0200

    WIP: Add required data for testing to relayer, add fixture, change test to use fixture

--- ethereum/test/test_verification.js
+++ ethereum/test/test_verification.js
@@ -6,6 +6,7 @@ const {
 const ETHApp = artifacts.require("ETHApp");
 const { keccakFromHexString, keccak } = require("ethereumjs-util");
 const { blake2AsHex } = require("@polkadot/util-crypto");
+const fixture = require('./fixtures/beefy-fixture-data.json');
 
 
 require("chai")
@@ -31,15 +32,10 @@ contract("IncentivizedInboundChannel", function (accounts) {
       this.validator1PubKeyMerkleProof = validatorsMerkleTree.getHexProof(this.validatorsLeaf1);
 
       this.lightClientBridge = await deployLightClientBridge(validatorsMerkleTree.getHexRoot());
-      const commitment = {
-        payload:,
-        blockNumber:,
-        validatorSetId:,
-      }
       const newCommitment = await this.lightClientBridge.newSignatureCommitment(
-        blake2AsHex("0x8f667b0d8050a5d544d6fb139485847ea3f654bbbd065b08aad2f04d93ba2b7cae0000000000000000000000", 256),
-        [parseBitfield("11")],
-        signatureSubstrateToEthereum("0xbf866f5a4c66d5716e736d8e4737c8386c59e47fb618a03fc4cc3716f8e0a0987eec8076ee5824600d6f84825d693e4e5dac8f90ca27d639b520ab435e2d221900"),
+        fixture.commitmentHash,
+        fixture.bitfield,
+        signatureSubstrateToEthereum(fixture.signature0),
         0,
         "0xE04CC55ebEE1cBCE552f250e85c57B70B2E2625b",
         this.validator0PubKeyMerkleProof
@@ -48,8 +44,9 @@ contract("IncentivizedInboundChannel", function (accounts) {
       const currentId = await this.lightClientBridge.currentId();
       const completeCommitment = await this.lightClientBridge.completeSignatureCommitment(
         currentId.sub(new web3.utils.BN(1)),
-        blake2AsHex("0x8f667b0d8050a5d544d6fb139485847ea3f654bbbd065b08aad2f04d93ba2b7cae0000000000000000000000", 256),
-        [signatureSubstrateToEthereum("0x9646258c491fc1d32c03521d468b530ca0eb9f3acd0de9fea7255dc21ff63ddb2a45c06552a0f5cc079c9df7b320eca99c9819619a4efa816f1593cb908cd40901")],
+        fixture.commitmentHash,
+        fixture.commitment,
+        [signatureSubstrateToEthereum(fixture.signature1)],
         [1],
         ["0x25451A4de12dcCc2D166922fA938E900fCc4ED24"],
         [this.validator1PubKeyMerkleProof]
@@ -80,21 +77,13 @@ contract("IncentivizedInboundChannel", function (accounts) {
       };
       const messages = [messageOne, messageTwo];
       const commitment = buildCommitment(messages);
-      const leaf = "0x3c390519c0130a826f3b96ec71ef9ce43f9fbc67fb0fc22ab4aaa1b3293394f7";
-      const proofs = [
-        "0xfe4a3df5f147d73b9edd54b6f665f68738a475243b0f5c583495b7fc34d7e127",
-        "0x890fc5d842bac18f3af8ddabea7a28e39cd05fe13a5132460aaa5ef5c27c5969",
-        "0xbe855ba734eef6617931c4bab11ce648892cad733df6a1c8287872eef656528a",
-        "0x4bcd5e2699bd1870f4a85e0c91ebab5198b419b71b89c81b14e251b57b0f4348",
-        "0xc1aad420d483a83b4dce6bf5e44c7af18653b46b0f6ca875ca0cb0ef6371adf7"
-      ];
       const tx = await this.inbound.submit(
         messages,
         commitment,
-        leaf,
-        174,
-        175,
-        proofs,
+        fixture.leaf,
+        fixture.leafIndex,
+        fixture.leafCount,
+        fixture.proofs,
         { from: userOne }
       );
       console.log(tx);
