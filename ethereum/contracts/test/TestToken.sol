6118964c33e3072073a1c7fdfa656b8fc2ad66a0
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-05-31 13:12:46 +0200

    chore: add hardhat

--- ethereum/contracts/test/TestToken.sol
+++ ethereum/contracts/test/TestToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
     constructor(
---
+++ ethereum/hardhat.config.ts
@@ -0,0 +1,46 @@
+import { config as dotenv } from "dotenv";
+import { resolve } from "path";
+dotenv({ path: resolve(__dirname, ".env") });
+
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-ethers";
+
+const getenv = (name: string) => {
+  if (name in process.env) {
+    return process.env[name]
+  } else {
+    throw new Error(`Please set your ${name} in a .env file`);
+  }
+}
+
+const mnemonic = getenv("MNEMONIC");
+const infuraKey = getenv("INFURA_PROJECT_ID");
+
+export default {
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: 'stone speak what ritual switch pigeon weird dutch burst shaft nature shove',
+      }
+    },
+    ropsten: {
+      chainId: 3,
+      url: `https://ropsten.infura.io/v3/${infuraKey}`,
+      accounts: {
+        mnemonic: mnemonic,
+      }
+    }
+  },
+  solidity: {
+    version: "0.7.6"
+  },
+  paths: {
+    sources: "contracts",
+    tests: "test",
+    cache: "cache",
+    artifacts: "artifacts"
+  },
+};
