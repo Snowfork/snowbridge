19cb0f80280964de79b0ffc5bc2e563b321492cf
Aidan Musnitzky <musnit@gmail.com>
2021-06-28 13:08:05 +0000

    revert scale whitespace

--- ethereum/contracts/ScaleCodec.sol
+++ ethereum/contracts/ScaleCodec.sol
@@ -76,40 +76,20 @@ library ScaleCodec {
         v = input;
 
         // swap bytes
-        v =
-            ((v &
-                0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >>
-                8) |
-            ((v &
-                0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) <<
-                8);
+        v = ((v & 0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >> 8) |
+            ((v & 0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) << 8);
 
         // swap 2-byte long pairs
-        v =
-            ((v &
-                0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >>
-                16) |
-            ((v &
-                0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) <<
-                16);
+        v = ((v & 0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >> 16) |
+            ((v & 0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) << 16);
 
         // swap 4-byte long pairs
-        v =
-            ((v &
-                0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >>
-                32) |
-            ((v &
-                0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) <<
-                32);
+        v = ((v & 0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >> 32) |
+            ((v & 0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) << 32);
 
         // swap 8-byte long pairs
-        v =
-            ((v &
-                0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >>
-                64) |
-            ((v &
-                0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) <<
-                64);
+        v = ((v & 0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >> 64) |
+            ((v & 0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) << 64);
 
         // swap 16-byte long pairs
         v = (v >> 128) | (v << 128);
@@ -119,18 +99,15 @@ library ScaleCodec {
         v = input;
 
         // swap bytes
-        v =
-            ((v & 0xFF00FF00FF00FF00FF00FF00FF00FF00) >> 8) |
+        v = ((v & 0xFF00FF00FF00FF00FF00FF00FF00FF00) >> 8) |
             ((v & 0x00FF00FF00FF00FF00FF00FF00FF00FF) << 8);
 
         // swap 2-byte long pairs
-        v =
-            ((v & 0xFFFF0000FFFF0000FFFF0000FFFF0000) >> 16) |
+        v = ((v & 0xFFFF0000FFFF0000FFFF0000FFFF0000) >> 16) |
             ((v & 0x0000FFFF0000FFFF0000FFFF0000FFFF) << 16);
 
         // swap 4-byte long pairs
-        v =
-            ((v & 0xFFFFFFFF00000000FFFFFFFF00000000) >> 32) |
+        v = ((v & 0xFFFFFFFF00000000FFFFFFFF00000000) >> 32) |
             ((v & 0x00000000FFFFFFFF00000000FFFFFFFF) << 32);
 
         // swap 8-byte long pairs
@@ -141,10 +118,12 @@ library ScaleCodec {
         v = input;
 
         // swap bytes
-        v = ((v & 0xFF00FF00FF00FF00) >> 8) | ((v & 0x00FF00FF00FF00FF) << 8);
+        v = ((v & 0xFF00FF00FF00FF00) >> 8) |
+            ((v & 0x00FF00FF00FF00FF) << 8);
 
         // swap 2-byte long pairs
-        v = ((v & 0xFFFF0000FFFF0000) >> 16) | ((v & 0x0000FFFF0000FFFF) << 16);
+        v = ((v & 0xFFFF0000FFFF0000) >> 16) |
+            ((v & 0x0000FFFF0000FFFF) << 16);
 
         // swap 4-byte long pairs
         v = (v >> 32) | (v << 32);
@@ -154,7 +133,8 @@ library ScaleCodec {
         v = input;
 
         // swap bytes
-        v = ((v & 0xFF00FF00) >> 8) | ((v & 0x00FF00FF) << 8);
+        v = ((v & 0xFF00FF00) >> 8) |
+            ((v & 0x00FF00FF) << 8);
 
         // swap 2-byte long pairs
         v = (v >> 16) | (v << 16);
