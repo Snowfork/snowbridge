16aaa49429bc7824aff69c2da83e4f0e0353c8fd
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-07-02 11:30:29 +0200

    feat: add access control to erc721 contract

--- ethereum/contracts/ERC721App.sol
+++ ethereum/contracts/ERC721App.sol
@@ -1,38 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.6;
+pragma solidity ^0.8.5;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol";
 import "./ScaleCodec.sol";
 import "./OutboundChannel.sol";
 
 enum ChannelId {Basic, Incentivized}
 
-contract ERC721App {
+contract ERC721App is AccessControl {
     using ScaleCodec for uint256;
 
     mapping(ChannelId => Channel) public channels;
 
-    bytes2 constant MINT_CALL = 0x0d01;
+    bytes2 constant MINT_CALL = 0x4301;
 
     event Locked(
-                 address token,
+                 address tokenContract,
+                 uint256 tokenId,
                  address sender,
-                 bytes32 recipient,
-                 uint256 tokenId
+                 bytes32 recipient
                  );
 
     event Unlocked(
-                   address token,
+                   address tokenContract,
+                   uint256 tokenId,
                    bytes32 sender,
-                   address recipient,
-                   uint256 tokenId
+                   address recipient
                    );
 
     struct Channel {
         address inbound;
         address outbound;
     }
+
+    bytes32 public constant INBOUND_CHANNEL_ROLE =
+        keccak256("INBOUND_CHANNEL_ROLE");
 
     constructor(Channel memory _basic, Channel memory _incentivized) {
         Channel storage c1 = channels[ChannelId.Basic];
@@ -42,18 +46,21 @@ contract ERC721App {
         Channel storage c2 = channels[ChannelId.Incentivized];
         c2.inbound = _incentivized.inbound;
         c2.outbound = _incentivized.outbound;
+
+        _setupRole(INBOUND_CHANNEL_ROLE, _basic.inbound);
+        _setupRole(INBOUND_CHANNEL_ROLE, _incentivized.inbound);
     }
 
     /**
      * @notice Locks ERC721 token in ERC721App contract
-     * @param _token The NFT contract
-     * @param _recipient Polkadot address of the receiver
+     * @param _tokenContract The NFT contract
      * @param _tokenId The NFT to lock
+     * @param _recipient Polkadot address of the receiver
      * @param _channelId The Channel to use to send token
      */
-    function lock(address _token,
-                  bytes32 _recipient,
+    function lock(address _tokenContract,
                   uint256 _tokenId,
+                  bytes32 _recipient,
                   ChannelId _channelId
                   ) public payable {
         require(
@@ -62,15 +69,15 @@ contract ERC721App {
                 "Invalid channel ID"
                 );
 
-        IERC721Metadata token = IERC721Metadata(_token);
+        IERC721Metadata token = IERC721Metadata(_tokenContract);
 
         require(token.ownerOf(_tokenId) == msg.sender, "Transfer of token that is not own");
 
         token.transferFrom(msg.sender, address(this), _tokenId);
 
-        emit Locked(_token, msg.sender, _recipient, _tokenId);
+        emit Locked(_tokenContract, _tokenId, msg.sender, _recipient);
 
-        bytes memory call = encodeCall(msg.sender, _recipient, _token, _tokenId, token.tokenURI(_tokenId));
+        bytes memory call = encodeCall(msg.sender, _recipient, _tokenContract, _tokenId, token.tokenURI(_tokenId));
 
         OutboundChannel channel =
             OutboundChannel(channels[_channelId].outbound);
@@ -80,31 +87,35 @@ contract ERC721App {
 
     /**
      * @notice Unlocks ERC721 token from ERC721App contract
-     * @param _token The NFT contract
+     * @param _tokenContract The NFT contract
+     * @param _tokenId The NFT to lock
      * @param _sender Polkadot address of the sender
      * @param _recipient The ETHApp
-     * @param _tokenId The NFT to lock
      */
     function unlock(
-             address _token,
+             address _tokenContract,
              bytes32 _sender,
              address _recipient,
              uint256 _tokenId
              ) public {
-        // TODO: Ensure message sender is a known inbound channel
-        IERC721Metadata token = IERC721Metadata(_token);
+        require(
+            hasRole(INBOUND_CHANNEL_ROLE, msg.sender),
+            "Caller is not an inbound channel"
+        );
+
+        IERC721Metadata token = IERC721Metadata(_tokenContract);
 
         require(token.ownerOf(_tokenId) == address(this), "Transfer of token that is not own");
 
         token.transferFrom(address(this), _recipient, _tokenId);
-        emit Unlocked(_token, _sender, _recipient, _tokenId);
+        emit Unlocked(_tokenContract, _tokenId, _sender, _recipient);
     }
 
     // SCALE-encode payload
     function encodeCall(
                         address _sender,
                         bytes32 _recipient,
-                        address _token,
+                        address _tokenContract,
                         uint256 _tokenId,
                         string memory _tokenURI
                         ) private pure returns (bytes memory) {
@@ -112,9 +123,9 @@ contract ERC721App {
             abi.encodePacked(
                              MINT_CALL,
                              _sender,
-                             byte(0x00), // Encode recipient as MultiAddress::Id
+                             bytes1(0x00), // Encode recipient as MultiAddress::Id
                              _recipient,
-                             _token,
+                             _tokenContract,
                              _tokenId.encode256(),
                              bytes(_tokenURI)
                              );
