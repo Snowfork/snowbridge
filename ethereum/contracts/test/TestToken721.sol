51fa9b7b54e4dc4210cdda3f2d4225f335ad8f00
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-07-06 00:48:06 +0200

    refactor: simplify TestToken721

--- ethereum/contracts/test/TestToken721.sol
+++ ethereum/contracts/test/TestToken721.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract TestToken721 is ERC721, ERC721URIStorage {
+contract TestToken721 is ERC721URIStorage {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function mint(address to, uint256 tokenId) public {
@@ -14,18 +13,5 @@ contract TestToken721 is ERC721, ERC721URIStorage {
     function mintWithTokenURI(address to, uint256 tokenId, string memory _tokenURI) public {
         mint(to, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }
