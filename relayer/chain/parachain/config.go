4cea540a06204a03fc85618cf65056468e9bf74e
Vincent Geddes <vincent@snowfork.com>
2021-07-14 13:54:13 +0000

    fixes

--- relayer/chain/parachain/config.go
+++ relayer/chain/parachain/config.go
@@ -3,5 +3,4 @@ package parachain
 type Config struct {
 	Endpoint   string `mapstructure:"endpoint"`
 	PrivateKey string `mapstructure:"private-key"`
-	ParaID     string `mapstructure:"private-key"`
 }
