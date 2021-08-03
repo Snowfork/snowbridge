b3056102fa76de4a1790f1263272d1039219dae0
Aidan Musnitzky <musnit@gmail.com>
2021-08-03 19:24:51 +0000

    improve logs

--- relayer/relays/parachain/merkle-proof.go
+++ relayer/relays/parachain/merkle-proof.go
@@ -1,6 +1,8 @@
 package parachain
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"sort"
 
 	log "github.com/sirupsen/logrus"
@@ -17,13 +19,45 @@ func (b ByParaID) Less(i, j int) bool { return b[i].ParaID < b[j].ParaID }
 func (b ByParaID) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
 
 type MerkleProofData struct {
-	PreLeaves       [][]byte
-	NumberOfLeaves  int
-	ProvenPreLeaf   []byte
-	ProvenLeaf      []byte
-	ProvenLeafIndex int64
-	Root            []byte
-	Proof           [][32]byte
+	PreLeaves       PreLeaves `json:"preLeaves"`
+	NumberOfLeaves  int       `json:"numberOfLeaves"`
+	ProvenPreLeaf   HexBytes  `json:"provenPreLeaf"`
+	ProvenLeaf      HexBytes  `json:"provenLeaf"`
+	ProvenLeafIndex int64     `json:"provenLeafIndex"`
+	Root            HexBytes  `json:"root"`
+	Proof           Proof     `json:"proof"`
+}
+
+type PreLeaves [][]byte
+type Proof [][32]byte
+type HexBytes []byte
+
+func (h HexBytes) MarshalJSON() ([]byte, error) {
+	b, _ := json.Marshal("0x" + hex.EncodeToString(h))
+	return b, nil
+}
+
+func (d PreLeaves) MarshalJSON() ([]byte, error) {
+	items := make([]string, 0, len(d))
+	for _, v := range d {
+		items = append(items, "0x"+hex.EncodeToString(v))
+	}
+	b, _ := json.Marshal(items)
+	return b, nil
+}
+
+func (d Proof) MarshalJSON() ([]byte, error) {
+	items := make([]string, 0, len(d))
+	for _, v := range d {
+		items = append(items, "0x"+hex.EncodeToString(v[:]))
+	}
+	b, _ := json.Marshal(items)
+	return b, nil
+}
+
+func (d MerkleProofData) String() string {
+	b, _ := json.Marshal(d)
+	return string(b)
 }
 
 func CreateParachainMerkleProof(heads map[uint32]relaychain.ParaHead, paraID uint32) (MerkleProofData, error) {
