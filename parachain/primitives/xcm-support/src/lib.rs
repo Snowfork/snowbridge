8dda4f1bb09c693bbcd7b5ad4af30d367ba66344
Vincent Geddes <vincent@snowfork.com>
2021-08-04 10:38:25 +0000

    Refactor TransactAsset implementation

--- parachain/primitives/xcm-support/src/lib.rs
+++ parachain/primitives/xcm-support/src/lib.rs
@@ -6,15 +6,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_core::U256;
-use sp_std::{marker::PhantomData, prelude::*};
+use sp_std::{result, marker::PhantomData, prelude::*};
+use codec::Decode;
 
 use xcm::v0::{Error as XcmError, Junction, MultiAsset, MultiLocation, Result as XcmResult};
-
 use xcm_executor::traits::{Convert, TransactAsset};
 
 use snowbridge_core::assets::{AssetId, MultiAsset as SnowbridgeMultiAsset};
-
-use codec::Decode;
 
 pub struct AssetsTransactor<Assets, AccountIdConverter, AccountId>(
 	PhantomData<(Assets, AccountIdConverter, AccountId)>,
@@ -23,47 +21,52 @@ pub struct AssetsTransactor<Assets, AccountIdConverter, AccountId>(
 impl<
 		Assets: SnowbridgeMultiAsset<AccountId>,
 		AccountIdConverter: Convert<MultiLocation, AccountId>,
-		AccountId: sp_std::fmt::Debug + Clone,
+		AccountId: Clone,
+	> AssetsTransactor<Assets, AccountIdConverter, AccountId> {
+	fn match_assets(a: &MultiAsset) -> result::Result<(AssetId, U256), XcmError> {
+		let (id, amount) = match a {
+			MultiAsset::ConcreteFungible { id, amount } => (id, amount),
+			_ => return Err(XcmError::AssetNotFound),
+		};
+
+		let key = match id.last() {
+			Some(Junction::GeneralKey(key)) => key,
+			_ => return Err(XcmError::AssetNotFound),
+		};
+
+		let asset_id: AssetId = AssetId::decode(&mut key.as_ref())
+			.map_err(|_| XcmError::FailedToTransactAsset("AssetIdConversionFailed"))?;
+
+		let value: U256 = (*amount).into();
+
+		Ok((asset_id, value))
+	}
+}
+
+impl<
+		Assets: SnowbridgeMultiAsset<AccountId>,
+		AccountIdConverter: Convert<MultiLocation, AccountId>,
+		AccountId: Clone,
 	> TransactAsset for AssetsTransactor<Assets, AccountIdConverter, AccountId>
 {
 	fn deposit_asset(asset: &MultiAsset, location: &MultiLocation) -> XcmResult {
+		let (asset_id, amount) = Self::match_assets(asset)?;
 		let who = AccountIdConverter::convert_ref(location)
 			.map_err(|()| XcmError::FailedToTransactAsset("AccountIdConversionFailed"))?;
-
-		if let MultiAsset::ConcreteFungible { id, amount } = asset {
-			if let Some(Junction::GeneralKey(key)) = id.last() {
-				let asset_id: AssetId =
-					AssetId::decode(&mut key.as_ref()).map_err(|_| XcmError::Undefined)?;
-				let value: U256 = (*amount).into();
-				Assets::deposit(asset_id, &who, value).map_err(|_| XcmError::Undefined)?;
-				Ok(())
-			} else {
-				Err(XcmError::Undefined)
-			}
-		} else {
-			Err(XcmError::Undefined)
-		}
+		Assets::deposit(asset_id, &who, amount)
+			.map_err(|e| XcmError::FailedToTransactAsset(e.into()))?;
+		return Ok(())
 	}
 
 	fn withdraw_asset(
 		asset: &MultiAsset,
 		location: &MultiLocation,
 	) -> Result<xcm_executor::Assets, XcmError> {
+		let (asset_id, amount) = Self::match_assets(asset)?;
 		let who = AccountIdConverter::convert_ref(location)
 			.map_err(|()| XcmError::FailedToTransactAsset("AccountIdConversionFailed"))?;
-
-		if let MultiAsset::ConcreteFungible { id, amount } = asset {
-			if let Some(Junction::GeneralKey(key)) = id.last() {
-				let asset_id: AssetId =
-					AssetId::decode(&mut key.as_ref()).map_err(|_| XcmError::Undefined)?;
-				let value: U256 = (*amount).into();
-				Assets::withdraw(asset_id, &who, value).map_err(|_| XcmError::Undefined)?;
-				Ok(asset.clone().into())
-			} else {
-				Err(XcmError::Undefined)
-			}
-		} else {
-			Err(XcmError::Undefined)
-		}
+		Assets::withdraw(asset_id, &who, amount)
+			.map_err(|e| XcmError::FailedToTransactAsset(e.into()))?;
+		Ok(asset.clone().into())
 	}
 }
