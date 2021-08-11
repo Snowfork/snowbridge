ebc26a15f9ae38a9a39d47b9f856afc8de76a13c
Aidan Musnitzky <musnit@gmail.com>
2021-08-11 14:24:55 +0000

    fix gas tests

--- ethereum/test/test_beefy_light_client_gas.js
+++ ethereum/test/test_beefy_light_client_gas.js
@@ -18,35 +18,49 @@ describe("Beefy Light Client Gas Usage", function () {
   const testCases = [
     {
       totalNumberOfValidators: 10,
+      totalNumberOfSignatures: 10,
     },
     {
       totalNumberOfValidators: 200,
+      totalNumberOfSignatures: 200,
+    },
+    {
+      totalNumberOfValidators: 200,
+      totalNumberOfSignatures: 134,
     },
     {
       totalNumberOfValidators: 255,
+      totalNumberOfSignatures: 255,
     },
     {
       totalNumberOfValidators: 257,
+      totalNumberOfSignatures: 257,
     },
     {
       totalNumberOfValidators: 1000,
+      totalNumberOfSignatures: 1000,
       fail: true
     },
     {
       totalNumberOfValidators: 1000,
+      totalNumberOfSignatures: 1000,
+    },
+    {
+      totalNumberOfValidators: 1000,
+      totalNumberOfSignatures: 667,
     },
   ]
 
   for (const testCase of testCases) {
-    it(`runs full flow with ${testCase.totalNumberOfValidators} validators with the complete transaction ${testCase.fail ? 'failing' : 'succeeding'}`,
+    it(`runs full flow with ${testCase.totalNumberOfValidators} validators and ${testCase.totalNumberOfSignatures} signers with the complete transaction ${testCase.fail ? 'failing' : 'succeeding'}`,
       async function () {
-        this.timeout(10 * 4000);
-        await runFlow(testCase.totalNumberOfValidators, testCase.fail)
+        this.timeout(1000 * 65);
+        await runFlow(testCase.totalNumberOfValidators, testCase.totalNumberOfSignatures, testCase.fail)
       });
   }
 
-  const runFlow = async function (totalNumberOfValidators, fail) {
-    console.log(`Running flow with ${totalNumberOfValidators} validators with the complete transaction ${fail ? 'failing' : 'succeeding'}: `)
+  const runFlow = async function (totalNumberOfValidators, totalNumberOfSignatures, fail) {
+    console.log(`Running flow with ${totalNumberOfValidators} validators and ${totalNumberOfSignatures} signatures with the complete transaction ${fail ? 'failing' : 'succeeding'}: `)
 
     const fixture = await createBeefyValidatorFixture(
       totalNumberOfValidators
@@ -54,9 +68,7 @@ describe("Beefy Light Client Gas Usage", function () {
     const beefyLightClient = await deployBeefyLightClient(fixture.root,
       totalNumberOfValidators);
 
-    const requiredNumberOfSignatures = (await beefyLightClient.requiredNumberOfSignatures()).toNumber()
-
-    const initialBitfieldPositions = await createRandomPositions(requiredNumberOfSignatures, totalNumberOfValidators)
+    const initialBitfieldPositions = await createRandomPositions(totalNumberOfSignatures, totalNumberOfValidators)
 
     const firstPosition = initialBitfieldPositions[0]
 
