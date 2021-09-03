5df8991c011175f6046e89b205f0c6ecc40fa58d
Alistair Singh <alistair.singh7@gmail.com>
2021-09-03 19:17:32 +0200

    Sync historical beefy justifications on relayer start up (#510)
    * added configuration and types
    
    * added synchronization step
    
    * re-use log fields
    
    * renamed config setting

--- relayer/relays/beefy/beefy-relaychain-listener.go
+++ relayer/relays/beefy/beefy-relaychain-listener.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/sirupsen/logrus"
 	"github.com/snowfork/go-substrate-rpc-client/v3/types"
 	"golang.org/x/sync/errgroup"
 
@@ -40,17 +39,110 @@ func NewBeefyRelaychainListener(
 func (li *BeefyRelaychainListener) Start(ctx context.Context, eg *errgroup.Group) error {
 	eg.Go(func() error {
 		defer close(li.beefyMessages)
-		err := li.subBeefyJustifications(ctx)
-		log.WithField("reason", err).Info("Shutting down polkadot listener")
+
+		err := li.syncBeefyJustifications(ctx)
 		if err != nil {
 			if errors.Is(err, context.Canceled) {
 				return nil
 			}
 			return err
 		}
+
+		err = li.subBeefyJustifications(ctx)
+		log.WithField("reason", err).Info("Shutting down polkadot listener")
+		if err != nil && !errors.Is(err, context.Canceled) {
+			return err
+		}
 		return nil
 	})
 	return nil
+}
+
+func (li *BeefyRelaychainListener) syncBeefyJustifications(ctx context.Context) error {
+	startingBlock := li.config.Source.Polkadot.BeefyStartingBlock
+	beefySkipPeriod := li.config.Source.BeefySkipPeriod
+
+	log.WithFields(
+		log.Fields{
+			"startingBlock":  startingBlock,
+			"beefSkipPeriod": beefySkipPeriod,
+		}).Info("Synchronizing beefy relaychain listener")
+
+	current := startingBlock
+	for {
+		finalizedHash, err := li.relaychainConn.API().RPC.Chain.GetFinalizedHead()
+		if err != nil {
+			log.WithError(err).Error("Failed to fetch finalized head.")
+			return err
+		}
+		finalizedHeader, err := li.relaychainConn.API().RPC.Chain.GetHeader(finalizedHash)
+		if err != nil {
+			log.WithError(err).WithField("finalizedBlockHash", finalizedHash.Hex()).Error("Failed to fetch header for finalised head.")
+			return err
+		}
+
+		logFields := log.Fields{
+			"blockNumber":          current,
+			"finalizedBlockNumber": finalizedHeader.Number}
+
+		finalizedBlockNumber := uint64(finalizedHeader.Number)
+		if current > finalizedBlockNumber {
+			log.WithFields(logFields).Info("Beefy relaychain listener synchronizing complete.")
+			return nil
+		}
+
+		log.WithFields(logFields).Info("Probing block.")
+
+		hash, err := li.relaychainConn.API().RPC.Chain.GetBlockHash(current)
+		if err != nil {
+			log.WithError(err).WithFields(logFields).Error("Failed to fetch block hash.")
+			return err
+		}
+		logFields["blockHash"] = hash.Hex()
+
+		block, err := li.relaychainConn.API().RPC.Chain.GetBlock(hash)
+		if err != nil {
+			log.WithError(err).WithFields(logFields).Error("Failed to fetch block hash.")
+			return err
+		}
+
+		commitments := []store.SignedCommitment{}
+		for j := range block.Justifications {
+			sc := store.OptionalSignedCommitment{}
+			if block.Justifications[j].EngineID() == "BEEF" {
+				err := types.DecodeFromBytes(block.Justifications[j].Payload(), &sc)
+				if err != nil {
+					log.WithFields(logFields).WithError(err).Error("Failed to decode BEEFY commitment messages")
+				} else if sc.IsSome() {
+					commitments = append(commitments, sc.Value)
+				}
+			}
+		}
+
+		for c := range commitments {
+			log.WithFields(logFields).WithFields(log.Fields{
+				"signedCommitment.Commitment.BlockNumber":    commitments[c].Commitment.BlockNumber,
+				"signedCommitment.Commitment.Payload":        commitments[c].Commitment.Payload.Hex(),
+				"signedCommitment.Commitment.ValidatorSetID": commitments[c].Commitment.ValidatorSetID,
+				"signedCommitment.Signatures":                commitments[c].Signatures,
+			}).Info("Synchronizing a BEEFY commitment.")
+
+			err = li.processBeefyJustifications(ctx, &commitments[c])
+			if err != nil {
+				return err
+			}
+		}
+
+		if len(commitments) > 0 {
+			log.WithFields(logFields).Info("Justifications found.")
+			// The beefy relayer can skip a certain amount of blocks provided it does not skip a whole
+			// validator sessions worth of blocks.
+			current += beefySkipPeriod
+		} else {
+			log.WithFields(logFields).Info("Justifications not found.")
+			current += 1
+		}
+	}
 }
 
 func (li *BeefyRelaychainListener) subBeefyJustifications(ctx context.Context) error {
@@ -84,83 +176,93 @@ func (li *BeefyRelaychainListener) subBeefyJustifications(ctx context.Context) e
 				log.WithError(err).Error("Failed to decode BEEFY commitment messages")
 			}
 
-			log.WithFields(logrus.Fields{
+			log.WithFields(log.Fields{
 				"signedCommitment.Commitment.BlockNumber":    signedCommitment.Commitment.BlockNumber,
 				"signedCommitment.Commitment.Payload":        signedCommitment.Commitment.Payload.Hex(),
 				"signedCommitment.Commitment.ValidatorSetID": signedCommitment.Commitment.ValidatorSetID,
 				"signedCommitment.Signatures":                signedCommitment.Signatures,
-			}).Info("Witnessed a new BEEFY commitment: ", msg.(string))
-			if len(signedCommitment.Signatures) == 0 {
-				log.Info("BEEFY commitment has no signatures, skipping...")
-				continue
-			}
+				"rawMessage":                                 msg.(string),
+			}).Info("Witnessed a new BEEFY commitment.")
 
-			signedCommitmentBytes, err := json.Marshal(signedCommitment)
+			err = li.processBeefyJustifications(ctx, signedCommitment)
 			if err != nil {
-				log.WithError(err).Error("Failed to marshal signed commitment:", signedCommitment)
-				continue
-			}
-
-			blockNumber := uint64(signedCommitment.Commitment.BlockNumber)
-
-			beefyAuthorities, err := li.getBeefyAuthorities(blockNumber)
-			if err != nil {
-				log.WithError(err).Error("Failed to get Beefy authorities from on-chain storage")
 				return err
-			}
-
-			beefyAuthoritiesBytes, err := json.Marshal(beefyAuthorities)
-			if err != nil {
-				log.WithError(err).Error("Failed to marshal BEEFY authorities:", beefyAuthorities)
-				return err
-			}
-
-			blockHash, err := li.relaychainConn.API().RPC.Chain.GetBlockHash(uint64(blockNumber))
-			if err != nil {
-				log.WithError(err).Error("Failed to get block hash")
-				return err
-			}
-			log.WithField("blockHash", blockHash.Hex()).Info("Got next blockhash")
-
-			latestMMRProof, err := li.relaychainConn.GetMMRLeafForBlock(blockNumber-1, blockHash, li.config.Source.Polkadot.BeefyStartingBlock)
-			if err != nil {
-				log.WithError(err).Error("Failed get MMR Leaf")
-				return err
-			}
-
-			mmrLeafCount, err := li.relaychainConn.FetchMMRLeafCount(blockHash)
-			if err != nil {
-				log.WithError(err).Error("Failed get MMR Leaf Count")
-				return err
-			}
-
-			if mmrLeafCount == 0 {
-				err := fmt.Errorf("MMR is empty and has no leaves")
-				log.WithError(err)
-				return err
-			}
-
-			serializedProof, err := types.EncodeToBytes(latestMMRProof)
-			if err != nil {
-				log.WithError(err).Error("Failed to serialize MMR Proof")
-				return err
-			}
-			log.WithField("latestMMRProof", latestMMRProof.Leaf.Version).Info("Got latestMMRProof")
-
-			info := store.BeefyRelayInfo{
-				ValidatorAddresses:       beefyAuthoritiesBytes,
-				SignedCommitment:         signedCommitmentBytes,
-				Status:                   store.CommitmentWitnessed,
-				SerializedLatestMMRProof: serializedProof,
-				MMRLeafCount:             mmrLeafCount,
-			}
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case li.beefyMessages <- info:
 			}
 		}
+	}
+}
+
+func (li *BeefyRelaychainListener) processBeefyJustifications(ctx context.Context, signedCommitment *store.SignedCommitment) error {
+	if len(signedCommitment.Signatures) == 0 {
+		log.Info("BEEFY commitment has no signatures, skipping...")
+		return nil
+	}
+
+	signedCommitmentBytes, err := json.Marshal(signedCommitment)
+	if err != nil {
+		log.WithField("signedCommitment", signedCommitment).WithError(err).Error("Failed to marshal signed commitment.")
+		return nil
+	}
+
+	blockNumber := uint64(signedCommitment.Commitment.BlockNumber)
+
+	beefyAuthorities, err := li.getBeefyAuthorities(blockNumber)
+	if err != nil {
+		log.WithError(err).Error("Failed to get Beefy authorities from on-chain storage")
+		return err
+	}
+
+	beefyAuthoritiesBytes, err := json.Marshal(beefyAuthorities)
+	if err != nil {
+		log.WithField("beefyAuthorities", beefyAuthorities).WithError(err).Error("Failed to marshal BEEFY authorities.")
+		return err
+	}
+
+	blockHash, err := li.relaychainConn.API().RPC.Chain.GetBlockHash(uint64(blockNumber))
+	if err != nil {
+		log.WithError(err).Error("Failed to get block hash")
+		return err
+	}
+	log.WithField("blockHash", blockHash.Hex()).Info("Got next blockhash")
+
+	latestMMRProof, err := li.relaychainConn.GetMMRLeafForBlock(blockNumber-1, blockHash, li.config.Source.Polkadot.BeefyStartingBlock)
+	if err != nil {
+		log.WithError(err).Error("Failed get MMR Leaf")
+		return err
+	}
+
+	mmrLeafCount, err := li.relaychainConn.FetchMMRLeafCount(blockHash)
+	if err != nil {
+		log.WithError(err).Error("Failed get MMR Leaf Count")
+		return err
+	}
+
+	if mmrLeafCount == 0 {
+		err := fmt.Errorf("MMR is empty and has no leaves")
+		log.WithError(err)
+		return err
+	}
+
+	serializedProof, err := types.EncodeToBytes(latestMMRProof)
+	if err != nil {
+		log.WithError(err).Error("Failed to serialize MMR Proof")
+		return err
+	}
+	log.WithField("latestMMRProof", latestMMRProof.Leaf.Version).Info("Got latestMMRProof")
+
+	info := store.BeefyRelayInfo{
+		ValidatorAddresses:       beefyAuthoritiesBytes,
+		SignedCommitment:         signedCommitmentBytes,
+		Status:                   store.CommitmentWitnessed,
+		SerializedLatestMMRProof: serializedProof,
+		MMRLeafCount:             mmrLeafCount,
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case li.beefyMessages <- info:
+		return nil
 	}
 }
 
