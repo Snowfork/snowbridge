085cfff62e215f82eaf7504fd7db5fc2db3a0758
Sumit Rajput <sumit@snowfork.com>
2022-02-14 17:49:43 +0530

    script for generating contract address list

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -5,6 +5,7 @@ root_dir="$(realpath ..)"
 parachain_dir="$root_dir/parachain"
 ethereum_dir="$root_dir/ethereum"
 relay_dir="$root_dir/relayer"
+test_dir="$root_dir/test"
 
 output_dir=/tmp/snowbridge
 
@@ -52,6 +53,15 @@ deploy_contracts()
     )
 
     echo "Exported contract artifacts: $output_dir/contracts.json"
+}
+
+contract_addresses()
+{
+    echo "Store Contract Addresses inside JSON file"
+    (
+        cd "$test_dir/scripts/helpers"
+        node contractAddresses.js
+    )
 }
 
 start_polkadot_launch()
@@ -233,6 +243,7 @@ mkdir "$output_dir"
 
 start_geth
 deploy_contracts
+contract_addresses
 start_polkadot_launch
 
 echo "Waiting for consensus between polkadot and parachain"
