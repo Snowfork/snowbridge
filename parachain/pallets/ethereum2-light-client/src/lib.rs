3503e65075c583f51fbbee737afef4d385cbab86
claravanstaden <claravanstaden64@gmail.com>
2022-02-15 11:33:07 +0000

    Adds light client update methods

--- parachain/pallets/ethereum2-light-client/src/lib.rs
+++ parachain/pallets/ethereum2-light-client/src/lib.rs
@@ -26,21 +26,39 @@ use scale_info::TypeInfo;
 use sp_runtime::RuntimeDebug;
 use sp_std::prelude::*;
 use sp_core::H256;
+use std::cmp;
 
 pub use snowbridge_ethereum::{
 	Header as EthereumHeader,
 };
 
 /// https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/sync-protocol.md#misc
-/// The minimum number of validators
+/// The minimum number of validators that needs to sign update
 const MIN_SYNC_COMMITTEE_PARTICIPANTS: u8 = 1;
-/// SLOTS_PER_EPOCH * EPOCHS_PER_SYNC_COMMITTEE_PERIOD in seconds	
-const UPDATE_TIMEOUT: u64 = 8; // TODO update
+
+const EPOCHS_PER_SYNC_COMMITTEE_PERIOD: u32 = 512;
+
+const SECONDS_PER_SLOT: u32 = 12;
+
+const SLOTS_PER_EPOCH: u32 = 32;
+
+const SYNC_COMMITTEE_SIZE: u32 = 256;
+
+const UPDATE_TIMEOUT: u32 = SLOTS_PER_EPOCH * EPOCHS_PER_SYNC_COMMITTEE_PERIOD;
+
 
 /// Beacon block header as it is stored in the runtime storage.
 #[derive(Clone, Default, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+// https://yeeth.github.io/BeaconChain.swift/Structs/BeaconBlockHeader.html#/s:11BeaconChain0A11BlockHeaderV9signature10Foundation4DataVvp
 pub struct BeaconBlockHeader {
-    // TODO: Add
+	// The slot for which this block is created. Must be greater than the slot of the block defined by parentRoot.
+    pub slot: u64,
+	// The block root of the parent block, forming a block chain.
+	pub parent_root: H256,
+	// The hash root of the post state of running the state transition through this block.
+	pub state_root: H256,
+	// BLS Signature of the block by the block proposer, TODO type isn't right yet
+	pub signature: String,
 }
 
 /// Sync committee as it is stored in the runtime storage.
@@ -68,7 +86,7 @@ pub struct LightClientUpdate {
 	/// Vector[Bytes32, floorlog2(NEXT_SYNC_COMMITTEE_INDEX)]
     pub next_sync_committee_branch: Vec<H256>,
     /// The finalized beacon block header attested to by Merkle branch
-    pub finalized_header: BeaconBlockHeader,
+    pub finalized_header: Option<BeaconBlockHeader>,
 	/// Vector[Bytes32, floorlog2(FINALIZED_ROOT_INDEX)]
     pub finality_branch: Vec<H256>,
     ///  Sync committee aggregate signature
@@ -77,7 +95,7 @@ pub struct LightClientUpdate {
     pub pubfork_version: Version,
 }
 
-pub use pallet::*;
+pub use frame_system::pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -129,7 +147,7 @@ pub mod pallet {
 
     /// Best available header to switch finalized head to if we see nothing else
     #[pallet::storage]
-    pub(super) type BestValidUpdate<T: Config> = StorageValue<_, BeaconBlockHeader, ValueQuery>;
+    pub(super) type BestValidUpdate<T: Config> = StorageValue<_, Option<LightClientUpdate>, ValueQuery>; // TODO: maybe I should use OptionQuery here?
 
     /// Most recent available reasonably-safe header
     #[pallet::storage]
@@ -181,6 +199,78 @@ pub mod pallet {
 			);
 
 			Ok(())
+		}
+	
+	}
+
+	impl<T: Config> Pallet<T> {
+		fn process_slot_for_light_client_store(current_slot: u64) {
+			if current_slot % UPDATE_TIMEOUT as u64 == 0 {
+				let curr_max_active_participants = <PreviousMaxActiveParticipants<T>>::get();
+				<PreviousMaxActiveParticipants<T>>::put(curr_max_active_participants);
+				<CurrentMaxActiveParticipants<T>>::put(0);
+			}
+
+			let finalized_header = <FinalizedHeader<T>>::get();
+			let best_valid_update = <BestValidUpdate<T>>::get();
+				
+			if current_slot > (finalized_header.slot + UPDATE_TIMEOUT as u64) && best_valid_update.is_some() {
+				// Forced best update when the update timeout has elapsed
+				Self::apply_light_client_update(best_valid_update.unwrap());
+				<BestValidUpdate<T>>::kill(); // TODO does this set the value to None?
+			}
+		}		
+
+		fn apply_light_client_update(update: LightClientUpdate) {
+			let active_header = Self::get_active_header(update);
+
+			let finalized_period = Self::compute_sync_committee_period(Self::compute_epoch_at_slot(store.finalized_header.slot));
+			let update_period = Self::compute_sync_committee_period(Self::compute_epoch_at_slot(active_header.slot));
+
+			if update_period == (finalized_period + 1) {
+				<CurrentSyncCommittee<T>>::put(<NextSyncCommittee<T>>::get());
+				<NextSyncCommittee<T>>::put(update.next_sync_committee);
+			}
+				
+			<FinalizedHeader<T>>::put(active_header.clone());
+
+			let optimistic_header = <OptimisticHeader<T>>::get();
+
+			let finalized_header = <FinalizedHeader<T>>::get();
+
+			if finalized_header.slot > optimistic_header.slot {
+				<OptimisticHeader<T>>::put(finalized_header.clone());
+			}		
+		}
+
+		fn compute_epoch_at_slot(slot: u64) -> u64 {
+			1
+		}
+
+		fn compute_sync_committee_period(epoch_at_slot: u64) -> u64 {
+			1
+		}
+
+		//** Helper functions **//
+		// https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/sync-protocol.md#helper-functions
+
+		fn get_subtree_index() -> u64 {
+			// return uint64(generalized_index % 2**(floorlog2(generalized_index)))
+			1
+		}
+
+		fn get_active_header(update: LightClientUpdate) -> BeaconBlockHeader {
+			// The "active header" is the header that the update is trying to convince us
+    		// to accept. If a finalized header is present, it's the finalized header,
+    		// otherwise it's the attested header
+			match update.finalized_header {
+				Some(finalized_header) => finalized_header,
+				None => update.attested_header,
+			}
+		}
+
+		fn get_safety_threshold(prev_max_active_participants: u64, curr_max_active_participants: u64) -> u64 {
+			cmp::max(prev_max_active_participants, curr_max_active_participants)
 		}
 	}
 }
