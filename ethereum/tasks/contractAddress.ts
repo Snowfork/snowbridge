a9c00aa825a920fe33122ea55148cd7fcf9628a8
Sumit Rajput <sumit@snowfork.com>
2022-02-28 18:48:42 +0530

    Fixed console

--- ethereum/tasks/contractAddress.ts
+++ ethereum/tasks/contractAddress.ts
@@ -1,11 +1,11 @@
 import { task } from "hardhat/config";
 
-// to be executed as 'npx hardhat  --network localhost contractAddressList'
 task("contractAddressList", "Print the list of deployed contract addresses.")
     .setAction(async (TaskArguments, hre) => {
         
         let { name, chainId } = await hre.ethers.provider.getNetwork()
         name = name === 'unknown' ? 'localhost' : name
+
         console.log("name", name)
         console.log("chainId", chainId)
 
@@ -19,15 +19,15 @@ task("contractAddressList", "Print the list of deployed contract addresses.")
         const ERC721App = await hre.deployments.get("ERC721App")
         const DOTAppContract = await hre.ethers.getContractAt("DOTApp", DOTApp.address);
         const SnowDOTAddress = await DOTAppContract.token();
-        
-        console.log("-----basicinbound---", BasicInboundChannel.address)
-        console.log("-----BasicOutboundChannel---", BasicOutboundChannel.address)
-        console.log("-----IncentivizedInboundChannel---", IncentivizedInboundChannel.address)
-        console.log("-----IncentivizedOutboundChannel---", IncentivizedOutboundChannel.address)
-        console.log("-----ETHApp---", ETHApp.address)
-        console.log("-----ERC20App---", ERC20App.address)
-        console.log("-----DOTApp---", DOTApp.address)
-        console.log("-----ERC721App---", ERC721App.address)
-        console.log("-----SnowDOTAddress---", SnowDOTAddress)
+
+        console.log("BasicInboundChannel", BasicInboundChannel.address)
+        console.log("BasicOutboundChannel", BasicOutboundChannel.address)
+        console.log("IncentivizedInboundChannel", IncentivizedInboundChannel.address)
+        console.log("IncentivizedOutboundChannel", IncentivizedOutboundChannel.address)
+        console.log("ETHApp", ETHApp.address)
+        console.log("ERC20App", ERC20App.address)
+        console.log("DOTApp", DOTApp.address)
+        console.log("ERC721App", ERC721App.address)
+        console.log("SnowDOTAddress", SnowDOTAddress)
 
     });
