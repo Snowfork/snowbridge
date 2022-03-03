ab26f469a4fdad0f7a34a217e35396f38bfd7271
Alistair Singh <alistair.singh7@gmail.com>
2022-03-03 21:45:09 +0000

    fetched nonces

--- test/scripts/detect-fork.ts
+++ test/scripts/detect-fork.ts
@@ -5,6 +5,7 @@ import { exit } from "process";
 import Web3 from "web3";
 import yargs from "yargs";
 import { createInterface } from "readline";
+import * as https from "https";
 
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 const keyring = new Keyring({ type: "sr25519" });
@@ -73,6 +74,90 @@ const fetchImported = async (api: ApiPromise, hash: string) => {
     .toJSON();
 };
 
+const getContracts = (url: string): Promise<any> => {
+  return new Promise<any>(function (ok, err) {
+    const buffers: Array<Buffer> = [];
+    https
+      .get(url, (res) => {
+        if (res.statusCode === 200) {
+          res.on("data", (d) => {
+            buffers.push(d);
+          });
+          res.on("end", () => {
+            ok(JSON.parse(Buffer.concat(buffers).toString("utf8")));
+          });
+        } else err(res.statusMessage);
+      })
+      .on("error", (e) => {
+        err(e);
+      });
+  });
+};
+
+const NONCES = [
+  { name: "BasicInboundChannel", event: "MessageDispatched", storageKey: "0x684b82bef882079feeabe54a5bd7b94a718368a0ace36e2b1b8b6dbd7f8093c0", },
+  { name: "IncentivizedInboundChannel", event: "MessageDispatched", storageKey: "0xf0f4d0b91e760c07da58bc0498033acb718368a0ace36e2b1b8b6dbd7f8093c0" },
+  { name: "BasicOutboundChannel", event: "Message", storageKey: "0x664ff6e369f56e1c7deca5487e631a5c718368a0ace36e2b1b8b6dbd7f8093c0" },
+  { name: "IncentivizedOutboundChannel", event: "Message", storageKey: "0x557df379daaf1cd514a7452dcbf6fccc718368a0ace36e2b1b8b6dbd7f8093c0" },
+];
+
+const fetchEthNonces = async (
+  contractsConfig: any,
+  ethApi: Web3,
+  commonAnscestorBlockNumber: number
+): Promise<any> => {
+
+  const pastEvents = {};
+  const nonces = {};
+  for (const nonce of NONCES) {
+    var contract = new ethApi.eth.Contract(
+      contractsConfig[nonce.name].abi,
+      contractsConfig[nonce.name].address,
+    );
+    // get all nonce changing events that happened after the new finalized
+    pastEvents[nonce.name] = contract.getPastEvents(nonce.event, {
+      fromBlock: commonAnscestorBlockNumber + 1 - 8,
+      toBlock: "latest",
+    });
+    nonces[nonce.name] = contract.methods.nonce().call();
+  }
+
+  await Promise.all(Object.values(pastEvents).concat(Object.values(nonces)));
+
+  const result = {};
+  for (const nonce of NONCES) {
+    const events = await pastEvents[nonce.name];
+    // take the first event if there are any else get take the current nonce.
+    if(events.length > 0) {
+      result[nonce.name] = Number(events[0].returnValues["nonce"]) - 1;
+    } else {
+      result[nonce.name] = Number(await nonces[nonce.name]);
+    }
+  }
+  return result;
+};
+
+const fetchParachainNonces = async (parachainApi: ApiPromise): Promise<any> => {
+  const promises = {};
+  for (const nonce of NONCES) {
+    promises[nonce.name] = parachainApi.query[`${nonce.name[0].toLowerCase()}${nonce.name.substring(1)}`].nonce();
+  }
+
+  await Promise.all(Object.values(promises));
+
+  const result = {};
+  for (const nonce of NONCES) {
+    result[nonce.name] = (await promises[nonce.name]).toNumber();
+  }
+  return result;
+};
+
+const generateUpdates = (ethNonces, parachainNonces) => {
+  const result = [];
+
+  return result;
+};
+
 const main = async () => {
   const argv = yargs.options({
     "eth-url": {
@@ -84,6 +169,11 @@ const main = async () => {
       type: "string",
       demandOption: true,
       describe: "API endpoint of source parachain",
+    },
+    "contracts-url": {
+      type: "string",
+      demandOption: true,
+      describe: "url to contracts.json file.",
     },
     blocks: {
       type: "number",
@@ -97,13 +187,17 @@ const main = async () => {
       describe: "The ethereum block number or hash to start the search.",
       default: null,
     },
-    "fix": {
+    fix: {
       type: "string",
       demandOption: false,
       describe: "Fix the fork with the following user. e.g. '//Alice'",
       default: null,
     },
   }).argv as any;
+
+  console.log("Fetching contracts.");
+  const contractsConfig = await getContracts(argv["contracts-url"]);
+  console.log("Fetch complete.");
 
   // intialize api clients
   const parachainApi = await ApiPromise.create({
@@ -132,12 +226,26 @@ const main = async () => {
   );
   if (ethFinalized.hash === paraFinalized.hash) {
     console.log("There is no fork.");
-    process.exit(0);
+    // process.exit(0);
   }
 
   // Walk backwards until we find a finalized block.
-  console.log("Fork found");
+  console.log("Fork found. Checking nonces.");
 
+  const ethNonces = await fetchEthNonces(contractsConfig.contracts, ethApi, 12007719);
+  const parachainNonces = await fetchParachainNonces(parachainApi);
+
+  console.log("Nonces                Parachain -> ETH");
+  console.log(`Basic Channel:        ${parachainNonces.BasicOutboundChannel} -> ${ethNonces.BasicInboundChannel}`);
+  console.log(`Incentivized Channel: ${parachainNonces.IncentivizedOutboundChannel} -> ${ethNonces.IncentivizedInboundChannel}`);
+
+  console.log("Nonces                Parachain <- ETH");
+  console.log(`Basic Channel:        ${parachainNonces.BasicInboundChannel} <- ${ethNonces.BasicOutboundChannel}`);
+  console.log(`Incentivized Channel: ${parachainNonces.IncentivizedInboundChannel} <- ${ethNonces.IncentivizedOutboundChannel}`);
+
+  console.log(generateUpdates(ethNonces, parachainNonces));
+
+  process.exit(1);// TODO:::::: REMOVE
   const startNumber = (
     await ethApi.eth.getBlock(
       argv["probe-from"] ?? ethFinalized.number - 1,
