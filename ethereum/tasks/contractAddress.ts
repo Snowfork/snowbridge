15ef738827c1d724505488afe3e404c8c68453e5
Sumit Rajput <sumit@snowfork.com>
2022-03-04 13:47:52 +0530

    Formatted JSON response

--- ethereum/tasks/contractAddress.ts
+++ ethereum/tasks/contractAddress.ts
@@ -6,9 +6,6 @@ task("contractAddressList", "Print the list of deployed contract addresses.")
         let { name, chainId } = await hre.ethers.provider.getNetwork()
         name = name === 'unknown' ? 'localhost' : name
 
-        console.log("name", name)
-        console.log("chainId", chainId)
-
         const BasicInboundChannel = await hre.deployments.get("BasicInboundChannel")
         const BasicOutboundChannel = await hre.deployments.get("BasicOutboundChannel")
         const IncentivizedInboundChannel = await hre.deployments.get("IncentivizedInboundChannel")
@@ -20,14 +17,20 @@ task("contractAddressList", "Print the list of deployed contract addresses.")
         const DOTAppContract = await hre.ethers.getContractAt("DOTApp", DOTApp.address);
         const SnowDOTAddress = await DOTAppContract.token();
 
-        console.log("BasicInboundChannel", BasicInboundChannel.address)
-        console.log("BasicOutboundChannel", BasicOutboundChannel.address)
-        console.log("IncentivizedInboundChannel", IncentivizedInboundChannel.address)
-        console.log("IncentivizedOutboundChannel", IncentivizedOutboundChannel.address)
-        console.log("ETHApp", ETHApp.address)
-        console.log("ERC20App", ERC20App.address)
-        console.log("DOTApp", DOTApp.address)
-        console.log("ERC721App", ERC721App.address)
-        console.log("SnowDOTAddress", SnowDOTAddress)
+        const addresses = {
+            "BasicInboundChannel": BasicInboundChannel.address,
+            "BasicOutboundChannel": BasicOutboundChannel.address,
+            "IncentivizedInboundChannel": IncentivizedInboundChannel.address,
+            "IncentivizedOutboundChannel": IncentivizedOutboundChannel.address,
+            "ETHApp": ETHApp.address,
+            "ERC20App": ERC20App.address,
+            "DOTApp": DOTApp.address,
+            "ERC721App": ERC721App.address,
+            "SnowDOTAddress": SnowDOTAddress,
+            "name": name,
+            "chainId": chainId,
+        }
+
+        console.log(JSON.stringify(addresses, undefined, 2))
 
     });
