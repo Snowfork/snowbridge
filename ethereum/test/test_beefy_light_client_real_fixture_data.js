c4550e75296878e41e3230b03a8cc00ff2281341
Vincent Geddes <vincent@snowfork.com>
2022-04-07 18:16:10 +0000

    Update more unit tests

--- ethereum/test/test_beefy_light_client_real_fixture_data.js
+++ ethereum/test/test_beefy_light_client_real_fixture_data.js
@@ -3,7 +3,8 @@ const {
   deployBeefyLightClient,
   mine, catchRevert, printBitfield
 } = require("./helpers");
-const fixture = require('./fixtures/full-flow-basic.json');
+
+const fixture = require('./fixtures/beefy-relay-basic.json')
 
 const SimpleMMRVerification = artifacts.require("SimplifiedMMRVerification");
 
@@ -24,80 +25,51 @@ describe("Beefy Light Client", function () {
 
   it("encodes, hashes and verifies beefy mmr leaves correctly", async function () {
     await this.beefyLightClient.verifyNewestMMRLeaf(
-      fixture.completeSubmitInput.latestMMRLeaf,
-      fixture.completeSubmitInput.commitment.payload,
+      fixture.finalSignatureCommitment.leaf,
+      fixture.finalSignatureCommitment.commitment.payload.mmrRootHash,
       {
-        merkleProofItems: fixture.completeSubmitInput.simplifiedMMRProof.merkleProofItems,
-        merkleProofOrderBitField: fixture.completeSubmitInput.simplifiedMMRProof.merkleProofOrderBitField
+        merkleProofItems: fixture.finalSignatureCommitment.proof.merkleProofItems,
+        merkleProofOrderBitField: fixture.finalSignatureCommitment.proof.merkleProofOrderBitField
       }
     ).should.be.fulfilled
   });
 
   it("runs new signature commitment and complete signature commitment correctly", async function () {
 
-    const initialBitfield = await this.beefyLightClient.createInitialBitfield(
-      fixture.completeSubmitInput.validatorProof.positions,
+    const bitfield = await this.beefyLightClient.createInitialBitfield(
+      fixture.finalSignatureCommitment.validatorProof.positions,
       2
     );
-    expect(printBitfield(initialBitfield)).to.eq('1')
-
-    const commitmentHash = await this.beefyLightClient.createCommitmentHash(
-      fixture.completeSubmitInput.commitment
-    );
+    expect(printBitfield(bitfield)).to.eq('100')
 
     const tx = this.beefyLightClient.newSignatureCommitment(
-      commitmentHash,
-      initialBitfield,
-      fixture.completeSubmitInput.validatorProof.signatures[0],
-      fixture.completeSubmitInput.validatorProof.positions[0],
-      fixture.completeSubmitInput.validatorProof.publicKeys[0],
-      fixture.completeSubmitInput.validatorProof.publicKeyMerkleProofs[0],
+      fixture.commitmentHash,
+      bitfield,
+      fixture.finalSignatureCommitment.validatorProof.signatures[0],
+      fixture.finalSignatureCommitment.validatorProof.positions[0],
+      fixture.finalSignatureCommitment.validatorProof.publicKeys[0],
+      fixture.finalSignatureCommitment.validatorProof.publicKeyMerkleProofs[0],
     )
 
     await tx.should.be.fulfilled
 
     const lastId = (await this.beefyLightClient.currentId()).sub(new web3.utils.BN(1));
 
-    await catchRevert(this.beefyLightClient.createRandomBitfield(lastId), 'Error: Block wait period not over');
-
-    await mine(45);
-
-    const bitfield = await this.beefyLightClient.createRandomBitfield(lastId);
-    const bitfieldString = printBitfield(bitfield);
-    const bitFieldHasOneBit = bitfieldString === '1' || bitfieldString === '10' // (trailing 0's are removed)
-    expect(bitFieldHasOneBit).to.be.true
-
-    const validatorProofs = {
-      signatures: [],
-      positions: [],
-      publicKeys: [],
-      publicKeyMerkleProofs: [],
-    }
-
-    const ascendingBitfield = bitfieldString.split('').reverse().join('');
-    for (let position = 0; position < ascendingBitfield.length; position++) {
-      const bit = ascendingBitfield[position]
-      if (bit === '1') {
-        validatorProofs.signatures.push(fixture.completeSubmitInput.validatorProof.signatures[position])
-        validatorProofs.positions.push(fixture.completeSubmitInput.validatorProof.positions[position])
-        validatorProofs.publicKeys.push(fixture.completeSubmitInput.validatorProof.publicKeys[position])
-        validatorProofs.publicKeyMerkleProofs.push(fixture.completeSubmitInput.validatorProof.publicKeyMerkleProofs[position])
-      }
-    }
+    await mine(3);
 
     await this.beefyLightClient.completeSignatureCommitment(
       lastId,
-      fixture.completeSubmitInput.commitment,
-      validatorProofs,
-      fixture.completeSubmitInput.latestMMRLeaf,
+      fixture.finalSignatureCommitment.commitment,
+      fixture.finalSignatureCommitment.validatorProof,
+      fixture.finalSignatureCommitment.leaf,
         {
-          merkleProofItems: fixture.completeSubmitInput.simplifiedMMRProof.merkleProofItems,
-          merkleProofOrderBitField: fixture.completeSubmitInput.simplifiedMMRProof.merkleProofOrderBitField
+          merkleProofItems: fixture.finalSignatureCommitment.proof.merkleProofItems,
+          merkleProofOrderBitField: fixture.finalSignatureCommitment.proof.merkleProofOrderBitField
         }
     ).should.be.fulfilled
 
-    latestMMRRoot = await this.beefyLightClient.latestMMRRoot()
-    expect(latestMMRRoot).to.eq(fixture.completeSubmitInput.commitment.payload)
+    root = await this.beefyLightClient.latestMMRRoot()
+    expect(root).to.eq(fixture.finalSignatureCommitment.commitment.payload.mmrRootHash)
   });
 
 
