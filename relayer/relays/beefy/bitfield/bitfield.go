a3445eb9eecd54a4052b20aa6eea8269bc47270a
Vincent Geddes <vincent@snowfork.com>
2022-05-31 09:00:13 +0000

    improve

--- relayer/relays/beefy/bitfield/bitfield.go
+++ relayer/relays/beefy/bitfield/bitfield.go
@@ -12,6 +12,8 @@ func reverse(thing []byte) {
 	}
 }
 
+/// New returns a Bitfield initialized from the Solidity representation of a Bitfield (an array of big integers). See below:
+/// https://github.com/Snowfork/snowbridge/blob/18c6225b21782170156729d54a35404d876a2c7b/ethereum/contracts/utils/Bitfield.sol
 func New(input []*big.Int) Bitfield {
 	result := make(Bitfield, 256 * len(input))
 	for i, chunk := range input {
@@ -23,9 +25,13 @@ func New(input []*big.Int) Bitfield {
 	return result
 }
 
+/// Members returns the set bits in the bitfield
 func (b Bitfield) Members() []uint64 {
 	results := []uint64{}
 	for idx, bits := range b {
+		if bits == 0 {
+			continue
+		}
 		for i := 0; i < 8; i++ {
 			if bits & byte(1 << i) > 0 {
 				results = append(results, uint64(idx + i))
