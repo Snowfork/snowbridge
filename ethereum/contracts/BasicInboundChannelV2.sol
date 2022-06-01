dce8b1138e35944cdc326372b1f4855bc1c7fffd
Vincent Geddes <vincent@snowfork.com>
2022-06-01 17:05:08 +0000

    update code match latest main

--- ethereum/contracts/BasicInboundChannelV2.sol
+++ ethereum/contracts/BasicInboundChannelV2.sol
@@ -1,93 +1,83 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.5;
-pragma experimental ABIEncoderV2;
-import "./ParachainLightClient.sol";
-import "./BeefyLightClient.sol";
-import "./SimplifiedMMRVerification.sol";
+pragma solidity ^0.8.9;
+
+import "./ParachainClient.sol";
 import "./utils/MerkleProof.sol";
 
 contract BasicInboundChannelV2 {
     uint256 public constant MAX_GAS_PER_MESSAGE = 100000;
     uint256 public constant GAS_BUFFER = 60000;
 
-    mapping(bytes32 => uint64) public userNonce;
+    uint8 public immutable sourceChannelID;
 
-    BeefyLightClient public beefyLightClient;
+    mapping(bytes32 => uint64) public nonces;
 
-    struct Leaf {
+    ParachainClient public parachainClient;
+
+    struct MessageBundle {
+        uint8 sourceChannelID;
         bytes32 account;
         uint64 nonce;
         Message[] messages;
     }
 
     struct Message {
+        uint64 id;
         address target;
         bytes payload;
     }
 
     event MessageDispatched(uint64 nonce, bool result);
 
-    constructor(BeefyLightClient _beefyLightClient) {
-        beefyLightClient = _beefyLightClient;
+    constructor(uint8 _sourceChannelID, ParachainClient _parachainClient) {
+        sourceChannelID = _sourceChannelID;
+        parachainClient = _parachainClient;
     }
 
     function generateCommitmentHash(
-        Leaf calldata _leaf,
-        bytes32[] calldata _leafProof,
+        MessageBundle calldata leaf,
+        bytes32[] calldata leafProof,
         bool[] calldata nodeSide
     ) internal pure returns (bytes32) {
-        bytes32 leafNodeHash = keccak256(abi.encode(_leaf));
-
+        bytes32 leafHash = keccak256(abi.encode(leaf));
         return
             MerkleProof.computeRootFromProofAndSide(
-                leafNodeHash,
-                _leafProof,
+                leafHash,
+                leafProof,
                 nodeSide
             );
     }
 
     function submit(
-        Leaf calldata _leaf,
+        MessageBundle calldata leaf,
         bytes32[] calldata leafProof,
         bool[] calldata nodeSide,
-        ParachainLightClient.ParachainVerifyInput
-            calldata _parachainVerifyInput,
-        ParachainLightClient.BeefyMMRLeafPartial calldata _beefyMMRLeafPartial,
-        SimplifiedMMRProof calldata proof
+        bytes calldata proof
     ) public {
-        bytes32 commitment = generateCommitmentHash(_leaf, leafProof, nodeSide);
+        bytes32 commitment = generateCommitmentHash(leaf, leafProof, nodeSide);
 
-        ParachainLightClient.verifyCommitmentInParachain(
-            commitment,
-            _parachainVerifyInput,
-            _beefyMMRLeafPartial,
-            proof,
-            beefyLightClient
-        );
-
+        require(parachainClient.verifyCommitment(commitment, proof), "Invalid proof");
+        require(leaf.sourceChannelID == sourceChannelID, "Invalid source channel");
+        require(leaf.nonce == nonces[leaf.account] + 1, "Invalid nonce");
         // Require there is enough gas to play all messages
         require(
-            gasleft() >= (_leaf.messages.length * MAX_GAS_PER_MESSAGE) + GAS_BUFFER,
+            gasleft() >= (leaf.messages.length * MAX_GAS_PER_MESSAGE) + GAS_BUFFER,
             "insufficient gas for delivery of all messages"
         );
 
-        processMessages(_leaf);
+        nonces[leaf.account]++;
+        dispatch(leaf);
     }
 
-    function processMessages(Leaf calldata _leaf) internal {
-        // User nonce for replay protection
-        require(userNonce[_leaf.account] + 1 == _leaf.nonce, "invalid nonce");
-
-        userNonce[_leaf.account] = userNonce[_leaf.account] + 1;
-
-        for (uint256 i = 0; i < _leaf.messages.length; i++) {
+    function dispatch(MessageBundle calldata bundle) internal {
+        for (uint256 i = 0; i < bundle.messages.length; i++) {
+            Message calldata message = bundle.messages[i];
             // Deliver the message to the target
-            (bool success, ) = _leaf.messages[i].target.call{
+            (bool success, ) = message.target.call{
                 value: 0,
                 gas: MAX_GAS_PER_MESSAGE
-            }(_leaf.messages[i].payload);
- 
-            emit MessageDispatched(userNonce[_leaf.account], success);
+            }(message.payload);
+            emit MessageDispatched(message.id, success);
         }
     }
 }
