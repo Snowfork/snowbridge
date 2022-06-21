55bea0310c6a134bc24aeeebb0cb044e32b1e9af
claravanstaden <claravanstaden64@gmail.com>
2022-06-21 07:02:11 +0000

    Ropsten script changes.

--- test/src/fixtures/index.js
+++ test/src/fixtures/index.js
@@ -11,6 +11,7 @@ const polkadotRecipient = "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5
 const polkadotRecipientSS58 = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
 const polkadotSenderSS58 = polkadotRecipientSS58;
 const treasuryAddressSS58 = "5EYCAe5jHEaRUtbinpdbTLuTyGiVt2TJGQPi9fdvVpNLNfSS";
+const ethEndpoint = 'ws://localhost:8546';
 const parachainEndpoint = 'ws://localhost:11144';
 const testParachainEndpoint = 'ws://localhost:13144';
 const infuraProjectId = env.INFURA_PROJECT_ID || '';
