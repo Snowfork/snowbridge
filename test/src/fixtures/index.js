ef2b3e6c55be5467db363d6454dbb6007f3c2f45
claravanstaden <claravanstaden64@gmail.com>
2022-06-21 07:02:11 +0000

    Ropsten script changes.

--- test/src/fixtures/index.js
+++ test/src/fixtures/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { env } = require('process');
 
 const contracts = JSON.parse(fs.readFileSync('/tmp/snowbridge/contracts.json', 'utf8'));
 
@@ -10,10 +11,10 @@ const polkadotRecipient = "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5
 const polkadotRecipientSS58 = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
 const polkadotSenderSS58 = polkadotRecipientSS58;
 const treasuryAddressSS58 = "5EYCAe5jHEaRUtbinpdbTLuTyGiVt2TJGQPi9fdvVpNLNfSS";
-const ethEndpoint = 'ws://localhost:8546';
 const parachainEndpoint = 'ws://localhost:11144';
 const testParachainEndpoint = 'ws://localhost:13144';
-const testNetworkID = '15';
+let ethEndpoint = 'ws://localhost:8546';
+let testNetworkID = '15';
 
 const TestTokenAddress = TestToken.address;
 
@@ -21,6 +22,7 @@ const ETH_TO_PARA_WAIT_TIME = 60000;
 const PARA_TO_ETH_WAIT_TIME = 100000;
 
 async function bootstrap() {
+  getEnvs();
   const ethClient = new EthClient(ethEndpoint, testNetworkID);
   const subClient = new SubClient(parachainEndpoint);
   await subClient.connect();
@@ -28,6 +30,19 @@ async function bootstrap() {
   await testSubClient.connect();
   await ethClient.initialize();
   return { ethClient, subClient, testSubClient };
+}
+
+function getEnvs() {
+  if (process.env.ETH_ENDPOINT != undefined) {
+    console.log('Using ETH_ENDPOINT provided');
+    ethEndpoint = 'wss://' + process.env.ETH_ENDPOINT;
+    if (process.env.NETWORK_ID != undefined) {
+      console.log('Using NETWORK_ID provided');
+      testNetworkID = process.env.NETWORK_ID;
+    } else {
+      console.warn('No NETWORK_ID provided');
+    }
+  }
 }
 
 module.exports = {
