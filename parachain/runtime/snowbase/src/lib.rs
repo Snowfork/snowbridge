7bccf17606156df9e089398c5d699a7058f35ba8
Alistair Singh <alistair.singh7@gmail.com>
2022-06-29 12:16:35 +0200

    Update correct runtime version (#649)

--- parachain/runtime/snowbase/src/lib.rs
+++ parachain/runtime/snowbase/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("snowbase"),
 	impl_name: create_runtime_str!("snowbase"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 1,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
--- parachain/runtime/snowblink/src/lib.rs
+++ parachain/runtime/snowblink/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("snowblink"),
 	impl_name: create_runtime_str!("snowblink"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
