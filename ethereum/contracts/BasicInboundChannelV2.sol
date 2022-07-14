4f1216cb95b3cc344a5e0727653dffb5b4c39b92
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-07-14 19:58:07 +0200

    Add V2 of basic inbound channel

---
+++ ethereum/contracts/BasicInboundChannelV2.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.9;
+
+import "./ParachainClient.sol";
+
+contract BasicInboundChannel {
+    uint256 public constant MAX_GAS_PER_MESSAGE = 100000;
+    uint256 public constant GAS_BUFFER = 60000;
+
+    uint8 public immutable sourceChannelID;
+
+    mapping(bytes32 => uint64) public nonces;
+
+    ParachainClient public parachainClient;
+
+    struct MessageBundle {
+        uint8 sourceChannelID;
+        bytes32 account;
+        uint64 nonce;
+        Message[] messages;
+    }
+
+    struct Message {
+        uint64 id;
+        address target;
+        bytes payload;
+    }
+
+    event MessageDispatched(uint64 id, bool result);
+
+    constructor(uint8 _sourceChannelID, ParachainClient _parachainClient) {
+        sourceChannelID = _sourceChannelID;
+        parachainClient = _parachainClient;
+    }
+
+    function submit(MessageBundle calldata bundle,  bytes calldata proof) external {
+        bytes32 commitment = keccak256(abi.encode(bundle));
+
+        require(parachainClient.verifyCommitment(commitment, proof), "Invalid proof");
+        require(bundle.sourceChannelID == sourceChannelID, "Invalid source channel");
+        require(bundle.nonce == nonce + 1, "Invalid nonce");
+        require(
+            gasleft() >= (bundle.messages.length * MAX_GAS_PER_MESSAGE) + GAS_BUFFER,
+            "insufficient gas for delivery of all messages"
+        );
+        nonce++;
+        dispatch(bundle);
+    }
+
+    function dispatch(MessageBundle calldata bundle) internal {
+        for (uint256 i = 0; i < bundle.messages.length; i++) {
+            Message calldata message = bundle.messages[i];
+            (bool success, ) = message.target.call{ value: 0, gas: MAX_GAS_PER_MESSAGE }(
+                message.payload
+            );
+            emit MessageDispatched(message.id, success);
+        }
+    }
+}
