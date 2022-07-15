5e200ddc8941c5df64458e01978cb45ae59b46f9
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-07-15 16:22:48 +0200

    Generate basic inbound channel v2 go bindings

--- relayer/contracts/generate.go
+++ relayer/contracts/generate.go
@@ -1,6 +1,7 @@
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/utils/OpaqueProof.sol/OpaqueProof.json | abigen --abi - --type OpaqueProof --pkg opaqueproof --out opaqueproof/contract.go"
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/BeefyClient.sol/BeefyClient.json | abigen --abi - --type BeefyClient --pkg beefyclient --out beefyclient/contract.go"
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/BasicInboundChannel.sol/BasicInboundChannel.json | abigen --abi - --type BasicInboundChannel --pkg basic --out basic/inbound.go"
+//go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/BasicInboundChannelV2.sol/BasicInboundChannelV2.json | abigen --abi - --type BasicInboundChannelV2 --pkg basic --out basic/inboundv2.go"
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/BasicOutboundChannel.sol/BasicOutboundChannel.json | abigen --abi - --type BasicOutboundChannel --pkg basic --out basic/outbound.go"
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/IncentivizedInboundChannel.sol/IncentivizedInboundChannel.json | abigen --abi - --type IncentivizedInboundChannel --pkg incentivized --out incentivized/inbound.go"
 //go:generate bash -c "jq .abi ../../ethereum/artifacts/contracts/IncentivizedOutboundChannel.sol/IncentivizedOutboundChannel.json | abigen --abi - --type IncentivizedOutboundChannel --pkg incentivized --out incentivized/outbound.go"
