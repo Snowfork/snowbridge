2dede7fa3f497d2d7c59da34cb055a2420270095
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-08-23 18:42:14 +0200

    Add comment explaining ACCOUNT_IDS

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -13,6 +13,8 @@ infura_endpoint_ws="${ETH_WS_ENDPOINT:-ws://localhost:8546}/${INFURA_PROJECT_ID:
 parachain_relay_eth_key="${PARACHAIN_RELAY_ETH_KEY:-0x8013383de6e5a891e7754ae1ef5a21e7661f1fe67cd47ca8ebf4acd6de66879a}"
 beefy_relay_eth_key="${BEEFY_RELAY_ETH_KEY:-0x935b65c833ced92c43ef9de6bff30703d941bd92a2637cb00cfad389f5862109}"
 
+# Accounts for which the relayer will relay messages over the basic channel.
+# Currently only works for messages from Polkadot to Ethereum until SNO-305 is complete.
 account_ids="${ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48}"
 
 start_beacon_sync="${START_BEACON_SYNC:-false}"
