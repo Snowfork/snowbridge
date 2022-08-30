7d663888f0d2df8358230e409fc06a9d3f6fb23c
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-08-30 15:01:38 +0200

    Add transactions for the remaining 5 accounts
    Tests are failing because only a single transaction is reflected in the
    eth account.

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -15,7 +15,7 @@ beefy_relay_eth_key="${BEEFY_RELAY_ETH_KEY:-0x935b65c833ced92c43ef9de6bff30703d9
 
 # Accounts for which the relayer will relay messages over the basic channel.
 # Currently only works for messages from Polkadot to Ethereum until SNO-305 is complete.
-account_ids="${ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48}"
+account_ids="${ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48,0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22,0x306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20,0xe659a7a1628cdd93febc04a4e0646ea20e9f5f0ce097d9a05290d4a9e054df4e,0x1cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c}"
 
 beacon_endpoint_http="${BEACON_HTTP_ENDPOINT:-http://localhost:9596}"
 
