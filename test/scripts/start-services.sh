bbe8360bb454df15b6e046d670b075b5a5cff373
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-09-07 17:10:16 +0200

    Add note about account ids

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -15,6 +15,7 @@ beefy_relay_eth_key="${BEEFY_RELAY_ETH_KEY:-0x935b65c833ced92c43ef9de6bff30703d9
 
 # Accounts for which the relayer will relay messages over the basic channel.
 # Currently only works for messages from Polkadot to Ethereum until SNO-305 is complete.
+# These IDs are for the test accounts Alice, Bob, Charlie, Dave, Eve and Ferdie, in order
 account_ids="${ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48,0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22,0x306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20,0xe659a7a1628cdd93febc04a4e0646ea20e9f5f0ce097d9a05290d4a9e054df4e,0x1cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c}"
 
 beacon_endpoint_http="${BEACON_HTTP_ENDPOINT:-http://localhost:9596}"
@@ -142,12 +143,12 @@ start_polkadot_launch()
 
     initial_beacon_block=""
     while [ -z "$initial_beacon_block" ] || [ "$initial_beacon_block" == "0x0000000000000000000000000000000000000000000000000000000000000000" ]
-    do 
+    do
         echo "Waiting for beacon chain to finalize to get initial block..."
         initial_beacon_block=$(curl -s "$beacon_endpoint_http/eth/v1/beacon/states/head/finality_checkpoints" \
             | jq -r '.data.finalized.root')
         sleep 3
-    done 
+    done
 
     echo "Found initial finalized block: $initial_beacon_block"
     curl -s "$beacon_endpoint_http/eth/v1/beacon/light_client/bootstrap/$initial_beacon_block" \
