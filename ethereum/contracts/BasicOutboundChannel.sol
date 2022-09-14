03af9affcfc0a8b132e96d75659ccd3c1638f37e
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-09-14 10:02:59 +0200

    SNO-324: Enable multiple accounts in outbound channel contract (#685)
    * Track nonces by source address
    
    * Remove principal from basic outbound channel
    
    * Update relayer bindings
    
    * Track nonces by origin address
    
    * Fix outbound channel tests

--- ethereum/contracts/BasicOutboundChannel.sol
+++ ethereum/contracts/BasicOutboundChannel.sol
@@ -11,13 +11,14 @@ contract BasicOutboundChannel is OutboundChannel, ChannelAccess, AccessControl {
     // Governance contracts will administer using this role.
     bytes32 public constant CONFIG_UPDATE_ROLE = keccak256("CONFIG_UPDATE_ROLE");
 
-    uint64 public nonce;
+    mapping(address => uint64) public nonces;
 
     // Only messages originating from this account will
     // be allowed through the channel.
     address public principal;
 
     event Message(
+        address origin,
         address source,
         uint64 nonce,
         bytes payload
@@ -30,13 +31,11 @@ contract BasicOutboundChannel is OutboundChannel, ChannelAccess, AccessControl {
     // Once-off post-construction call to set initial configuration.
     function initialize(
         address _configUpdater,
-        address _principal,
         address[] memory defaultOperators
     )
     external onlyRole(DEFAULT_ADMIN_ROLE) {
         // Set initial configuration
         grantRole(CONFIG_UPDATE_ROLE, _configUpdater);
-        principal = _principal;
         for (uint i = 0; i < defaultOperators.length; i++) {
             _authorizeDefaultOperator(defaultOperators[i]);
         }
@@ -55,29 +54,15 @@ contract BasicOutboundChannel is OutboundChannel, ChannelAccess, AccessControl {
         _revokeDefaultOperator(operator);
     }
 
-    // Update the principal.
-    function setPrincipal(address _principal) external onlyRole(CONFIG_UPDATE_ROLE) {
-        principal = _principal;
-    }
-
     /**
      * @dev Sends a message across the channel
      *
      * Submission is a privileged action involving two parties: The operator and the origin.
      * Apps (aka operators) need to be authorized by the `origin` to submit messages via this channel.
-     *
-     * Furthermore, this channel restricts the origin to a single account, that of the principal.
-     * In essence this ensures that only the principal account can send messages via this channel.
-     *
-     * For pre-production testing, the restriction to the principal account can be bypassed by using
-     * `setPrincipal` to set the principal to the address 0x0000000000000000000000000000000000000042.
      */
     function submit(address _origin, bytes calldata _payload) external override {
         require(isOperatorFor(msg.sender, _origin), "Caller is unauthorized");
-        if (principal != address(0x0000000000000000000000000000000000000042)) {
-            require(_origin == principal, "Origin is not an authorized principal");
-        }
-        nonce = nonce + 1;
-        emit Message(msg.sender, nonce, _payload);
+        nonces[_origin] = nonces[_origin] + 1;
+        emit Message(_origin, msg.sender, nonces[_origin], _payload);
     }
 }
