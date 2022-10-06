9a2db3e74d84c128e18072e7cb9795cd4751dfb2
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-10-06 18:06:53 +0200

    Add default eth addresses for basic channel

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -13,12 +13,12 @@ infura_endpoint_ws="${ETH_WS_ENDPOINT:-ws://localhost:8546}/${INFURA_PROJECT_ID:
 parachain_relay_eth_key="${PARACHAIN_RELAY_ETH_KEY:-0x8013383de6e5a891e7754ae1ef5a21e7661f1fe67cd47ca8ebf4acd6de66879a}"
 beefy_relay_eth_key="${BEEFY_RELAY_ETH_KEY:-0x935b65c833ced92c43ef9de6bff30703d941bd92a2637cb00cfad389f5862109}"
 
-# Accounts for which the relayer will relay messages over the basic channel.
-# Currently only works for messages from Polkadot to Ethereum until SNO-305 is complete.
+# Parachain accounts for which the relayer will relay messages over the basic channel.
 # These IDs are for the test accounts Alice, Bob, Charlie, Dave, Eve and Ferdie, in order
 basic_parachain_account_ids="${BASIC_PARACHAIN_ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48,0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22,0x306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20,0xe659a7a1628cdd93febc04a4e0646ea20e9f5f0ce097d9a05290d4a9e054df4e,0x1cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c}"
-# TODO: fill in correct addresses
-basic_eth_addresses="${BASIC_ETH_ADDRESSES:-0xd43593c715fdd31c61141abd04a99fd6822c8558}"
+# Ethereum addresses for which the relayer will relay messages over the basic channel.
+# These addresses are for the EthApp and ERC20App contracts.
+basic_eth_addresses="${BASIC_ETH_ADDRESSES:-0xee9170abfbf9421ad6dd07f6bdec9d89f2b581e0,0x8cf6147918a5cbb672703f879f385036f8793a24}"
 
 beacon_endpoint_http="${BEACON_HTTP_ENDPOINT:-http://localhost:9596}"
 
