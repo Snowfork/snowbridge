957db8d5d10305558e07e6a918e0968d4f9457ab
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-10-10 10:54:32 +0200

    Switch to default eth account for E2E script

--- test/scripts/start-services.sh
+++ test/scripts/start-services.sh
@@ -17,8 +17,8 @@ beefy_relay_eth_key="${BEEFY_RELAY_ETH_KEY:-0x935b65c833ced92c43ef9de6bff30703d9
 # These IDs are for the test accounts Alice, Bob, Charlie, Dave, Eve and Ferdie, in order
 basic_parachain_account_ids="${BASIC_PARACHAIN_ACCOUNT_IDS:-0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d,0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48,0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22,0x306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20,0xe659a7a1628cdd93febc04a4e0646ea20e9f5f0ce097d9a05290d4a9e054df4e,0x1cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c}"
 # Ethereum addresses for which the relayer will relay messages over the basic channel.
-# These addresses are for the EthApp and ERC20App contracts.
-basic_eth_addresses="${BASIC_ETH_ADDRESSES:-0xee9170abfbf9421ad6dd07f6bdec9d89f2b581e0,0x8cf6147918a5cbb672703f879f385036f8793a24}"
+# This address is for the default eth account used in the E2E tests, taken from test/src/ethclient/index.js.
+basic_eth_addresses="${BASIC_ETH_ADDRESSES:-0x89b4ab1ef20763630df9743acf155865600daff2}"
 
 beacon_endpoint_http="${BEACON_HTTP_ENDPOINT:-http://localhost:9596}"
 
@@ -157,7 +157,7 @@ start_polkadot_launch()
     echo "Found initial finalized block: $initial_beacon_block"
     bootstrap_header=""
     while [ -z "$bootstrap_header" ] || [ "$bootstrap_header" == "" ] || [ "$bootstrap_header" == "null" ]
-    do 
+    do
         echo "Waiting for beacon to get initial bootstrap..."
         bootstrap_header=$(curl -s "$beacon_endpoint_http/eth/v1/beacon/light_client/bootstrap/$initial_beacon_block" \
             | jq -r '.data.header')
