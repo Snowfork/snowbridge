a259a80a0d6a82f4b1a676c71f3378636f87b925
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-10-24 13:22:29 +0200

    SNO-316: Switch to Twox64Concat hasher for Nonces map (#705)

--- parachain/pallets/basic-channel/src/outbound/mod.rs
+++ parachain/pallets/basic-channel/src/outbound/mod.rs
@@ -163,7 +163,7 @@ pub mod pallet {
 		StorageValue<_, BoundedVec<EnqueuedMessageOf<T>, T::MaxMessagesPerCommit>, ValueQuery>;
 
 	#[pallet::storage]
-	pub type Nonce<T: Config> = StorageMap<_, Identity, T::AccountId, u64, ValueQuery>;
+	pub type Nonce<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, u64, ValueQuery>;
 
 	#[pallet::storage]
 	pub type NextId<T: Config> = StorageValue<_, u64, ValueQuery>;
