2a2c905eebed920b2378d58213254606dd1a9672
Alistair Singh <alistair.singh7@gmail.com>
2022-11-01 09:43:26 +0000

    fixed comment

--- ethereum/contracts/ERC20Vault.sol
+++ ethereum/contracts/ERC20Vault.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// Holds ETH on behalf of ETHApp
+// Holds ERC20 Tokens on behalf of ERC20App
 contract ERC20Vault is Ownable {
     using SafeERC20 for IERC20;
 
