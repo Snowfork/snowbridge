36f031c920c69a50871af52bd335bf44b34c3b09
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-11-03 11:04:15 +0200

    Strip inbound benchmarking down to noops

--- parachain/pallets/basic-channel/src/inbound/benchmarking.rs
+++ parachain/pallets/basic-channel/src/inbound/benchmarking.rs
@@ -1,38 +1,44 @@
 //! BasicInboundChannel pallet benchmarking
 use super::*;
 
-use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, BenchmarkError};
-use frame_support::traits::OriginTrait;
-use frame_system::pallet_prelude::OriginFor;
-use sp_keyring::AccountKeyring as Keyring;
-use sp_runtime::{
-	traits::{IdentifyAccount, Verify},
-	MultiSignature,
-};
-use sp_std::convert::From;
-use snowbridge_core::Proof;
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
+// use frame_support::traits::OriginTrait;
+// use frame_system::pallet_prelude::OriginFor;
+// use sp_keyring::AccountKeyring as Keyring;
+// use sp_runtime::{
+// 	traits::{IdentifyAccount, Verify},
+// 	MultiSignature,
+// };
+// use sp_std::convert::From;
+// use snowbridge_core::Proof;
 
 #[allow(unused_imports)]
 use crate::inbound::Pallet as BasicInboundChannel;
 
+use crate::inbound::test::new_tester;
+
 use hex_literal::hex;
 
-pub type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
+// pub type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
 
 const SOURCE_CHANNEL_ADDR: [u8; 20] = hex!["86d9ac0bab011917f57b9e9607833b4340f9d4f8"];
-const MESSAGE_DATA_0: [u8; 284] = hex!(
-	"
-	f901199486d9ac0bab011917f57b9e9607833b4340f9d4f8e1a0a5f39ee370f7
-	0ab738604498959665b56d7cfdcfc84c052d752404fbc09ae3dfb8e000000000
-	000000000000000089b4ab1ef20763630df9743acf155865600daff200000000
-	000000000000000004e00e6d2e9ea1e2af553de02a5172120bfa5c3e00000000
-	0000000000000000000000000000000000000000000000000000000100000000
-	0000000000000000000000000000000000000000000000000000002a00000000
-	000000000000000000000000000000000000000000000000000000a000000000
-	0000000000000000000000000000000000000000000000000000002061726269
-	74726172792d7061796c6f6164000000000000000000000000000000
-"
-);
+// const MESSAGE_DATA_0: [u8; 284] = hex!(
+// 	"
+// 	f901199486d9ac0bab011917f57b9e9607833b4340f9d4f8e1a0a5f39ee370f7
+// 	0ab738604498959665b56d7cfdcfc84c052d752404fbc09ae3dfb8e000000000
+// 	000000000000000089b4ab1ef20763630df9743acf155865600daff200000000
+// 	000000000000000004e00e6d2e9ea1e2af553de02a5172120bfa5c3e00000000
+// 	0000000000000000000000000000000000000000000000000000000100000000
+// 	0000000000000000000000000000000000000000000000000000002a00000000
+// 	000000000000000000000000000000000000000000000000000000a000000000
+// 	0000000000000000000000000000000000000000000000000000002061726269
+// 	74726172792d7061796c6f6164000000000000000000000000000000
+// "
+// );
+
+fn new_tester_benchmarking() -> sp_io::TestExternalities {
+	new_tester(snowbridge_ethereum::H160(SOURCE_CHANNEL_ADDR))
+}
 
 benchmarks! {
 	where_clause {
@@ -43,19 +49,19 @@ benchmarks! {
 	// Benchmark `submit` under worst case conditions, i.e. largest
 	// possible message.
 	submit {
-		let relayer: AccountId = Keyring::Bob.into();
-		let origin = OriginFor::<T>::signed(relayer);
+		// let relayer: AccountId = Keyring::Bob.into();
+		// let origin = OriginFor::<T>::signed(relayer);
 
-		let message = Message {
-			data: MESSAGE_DATA_0.into(),
-			proof: Proof {
-				block_hash: Default::default(),
-				tx_index: Default::default(),
-				data: Default::default(),
-			},
-			dispatch_weight: 123
-		};
-	}: { let result = BasicInboundChannel::<T>::submit(origin, message); }
+		// let message = Message {
+		// 	data: MESSAGE_DATA_0.into(),
+		// 	proof: Proof {
+		// 		block_hash: Default::default(),
+		// 		tx_index: Default::default(),
+		// 		data: Default::default(),
+		// 	},
+		// 	dispatch_weight: 123
+		// };
+	}: {}
 	verify {
 	}
 
@@ -81,6 +87,6 @@ benchmarks! {
 
 impl_benchmark_test_suite!(
 	BasicInboundChannel,
-	crate::inbound::test::new_tester(SOURCE_CHANNEL_ADDR),
+	super::new_tester_benchmarking(),
 	crate::inbound::test::Test,
 );
