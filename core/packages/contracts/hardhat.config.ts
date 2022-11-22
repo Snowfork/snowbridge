0fff31c4c0f548ace3c38bf46c67a5a6f31e64a5
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-11-22 13:59:01 +0100

    Re-enable solidity optimizer

--- core/packages/contracts/hardhat.config.ts
+++ core/packages/contracts/hardhat.config.ts
@@ -55,7 +55,7 @@ const config: HardhatUserConfig = {
         version: "0.8.9",
         settings: {
             optimizer: {
-                enabled: false,
+                enabled: true,
                 runs: 200
             }
         }
