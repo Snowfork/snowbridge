8bfe96c67f24d67fbe3d1ffecedcf7ae4030c9df
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-11-24 12:58:18 +0100

    Revert hardhat-exposed for ScaleCodec tests

--- core/packages/contracts/test/libs/scale_codec.test.ts
+++ core/packages/contracts/test/libs/scale_codec.test.ts
@@ -1,16 +1,18 @@
 import { ethers, expect, loadFixture } from "../setup"
+import { ScaleCodec__factory } from "../../src"
 
 describe("ScaleCodec", function () {
     async function fixture() {
-        let codecFactory = await ethers.getContractFactory("$ScaleCodec")
-        let codecLib = await codecFactory.deploy()
-        return { codecLib }
+        let [owner] = await ethers.getSigners()
+        let codec = await new ScaleCodec__factory(owner).deploy()
+        await codec.deployed()
+        return { codec }
     }
 
     describe("encoding unsigned integers", function () {
         it("should encode uint256", async function () {
-            let { codecLib } = await loadFixture(fixture)
-            let output = await codecLib.$encodeU256(
+            let { codec } = await loadFixture(fixture)
+            let output = await codec.encodeU256(
                 "12063978950259949786323707366460749298097791896371638493358994162204017315152"
             )
             expect(output).to.be.equal(
@@ -18,28 +20,28 @@ describe("ScaleCodec", function () {
             )
         })
 
-        it("should encode uint128", async function () {
-            let { codecLib } = await loadFixture(fixture)
-            let output = await codecLib.$encodeU128("35452847761173902980759433963665451267")
+         it("should encode uint128", async function () {
+            let { codec } = await loadFixture(fixture)
+            let output = await codec.encodeU128("35452847761173902980759433963665451267")
             expect(output).to.be.equal("0x036935fa9a1488629b109d3d59f8ab1a")
-        })
+         })
 
-        it("should encode uint64", async function () {
-            let { codecLib } = await loadFixture(fixture)
-            let output = await codecLib.$encodeU64("1921902728173129883")
+         it("should encode uint64", async function () {
+            let { codec } = await loadFixture(fixture)
+            let output = await codec.encodeU64("1921902728173129883")
             expect(output).to.be.equal("0x9b109d3d59f8ab1a")
-        })
+         })
 
-        it("should encode uint32", async function () {
-            let { codecLib } = await loadFixture(fixture)
-            let output = await codecLib.$encodeU32("447477849")
+         it("should encode uint32", async function () {
+            let { codec } = await loadFixture(fixture)
+            let output = await codec.encodeU32("447477849")
             expect(output).to.be.equal("0x59f8ab1a")
-        })
+         })
 
-        it("should encode uint16", async function () {
-            let { codecLib } = await loadFixture(fixture)
-            let output = await codecLib.$encodeU16("6827")
+         it("should encode uint16", async function () {
+            let { codec } = await loadFixture(fixture)
+            let output = await codec.encodeU16("6827")
             expect(output).to.be.equal("0xab1a")
-        })
-    })
+         })
+     })
 })
