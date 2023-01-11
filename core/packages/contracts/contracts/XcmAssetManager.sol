f16daf2c847fb36a63f51921b6657bbdd72d4c41
Alistair Singh <alistair.singh7@gmail.com>
2023-01-11 09:00:25 +0000

    removed functions

--- core/packages/contracts/contracts/XcmAssetManager.sol
+++ core/packages/contracts/contracts/XcmAssetManager.sol
@@ -23,18 +23,4 @@ contract XcmAssetManager is XcmAssetLookup {
     function lookup(bytes32 assetHash) external view override returns (XcmFungibleAsset) {
         return fungibleAssets[assetHash];
     }
-
-    function iToHex(bytes32 buffer) internal pure returns (string memory) {
-        // Fixed buffer size for hexadecimal convertion
-        bytes memory converted = new bytes(buffer.length * 2);
-
-        bytes memory _base = "0123456789abcdef";
-
-        for (uint256 i = 0; i < buffer.length; i++) {
-            converted[i * 2] = _base[uint8(buffer[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(buffer[i]) % _base.length];
-        }
-
-        return string(converted);
-    }
 }
