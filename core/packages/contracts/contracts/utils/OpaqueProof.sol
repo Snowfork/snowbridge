1649db542d9a62faa9fb58d0c0d488e7a7ada9d4
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-01-12 20:54:49 +0400

    Remove unused OpaqueProof

--- core/packages/contracts/contracts/utils/OpaqueProof.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.9;
-
-import "../ParachainClient.sol";
-
-contract OpaqueProof {
-    // solhint-disable-next-line no-empty-blocks
-    function dummy(ParachainClient.Proof memory proof) public pure {}
-}
