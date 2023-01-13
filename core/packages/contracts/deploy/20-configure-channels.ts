3d3657b0c33392bd5a58277a4039dd82bcd86f98
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-01-13 18:05:11 +0400

    Remove other references to apps & custom token

--- core/packages/contracts/deploy/20-configure-channels.ts
+++ core/packages/contracts/deploy/20-configure-channels.ts
@@ -3,10 +3,6 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 module.exports = async ({ ethers, deployments, getUnnamedAccounts }: HardhatRuntimeEnvironment) => {
     let [deployer] = await getUnnamedAccounts()
 
-    let dotApp = await deployments.get("DOTApp")
-    let ethApp = await deployments.get("ETHApp")
-    let erc20App = await deployments.get("ERC20App")
-
     const feeData = await ethers.provider.getFeeData()
 
     console.log("Configuring BasicOutboundChannel")
@@ -20,7 +16,7 @@ module.exports = async ({ ethers, deployments, getUnnamedAccounts }: HardhatRunt
         },
         "initialize",
         deployer,
-        [dotApp.address, ethApp.address, erc20App.address]
+        []
     )
 
     // Mark deployment to run only once
