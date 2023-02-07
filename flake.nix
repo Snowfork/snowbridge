8943e1da1d593134f47f20fd8e21b45affec4d0f
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-02-07 13:56:25 +0400

    Set up Rust and system compatibility

--- flake.nix
+++ flake.nix
@@ -1,52 +1,60 @@
 {
-    description = "A flake for Snowbridge";
+    description = "Snowbridge flake";
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-22.11";
-
-        /* rust-overlay = { */
-        /* url = "github:oxalica/rust-overlay"; */
-        /* inputs.nixpkgs.follows = "nixpkgs"; */
-        /* }; */
+        rust-overlay.url = "github:oxalica/rust-overlay";
+        flake-utils.url  = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs }: # , rust-overlay
+    outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
         let
-            pkgs = import nixpkgs { system = "aarch64-darwin"; };
-            dependencies = (with pkgs; [
-                git
-                curl
-                jq
-                moreutils
-                direnv
-                typos
-                go-ethereum
+            overlays = [ (import rust-overlay) ];
+            pkgs = import nixpkgs { inherit system overlays; };
+        in
+        with pkgs;
+        {
+            devShells.default = pkgs.mkShell {
+                buildInputs = [
+                    git
+                    curl
+                    jq
+                    moreutils
+                    direnv
+                    typos
+                    go-ethereum
 
-                # typescript packages
-                nodejs-18_x
-                nodePackages.pnpm
+                    # typescript packages
+                    nodejs-18_x
+                    nodePackages.pnpm
 
-                # relayer
-                go
-                mage
-                revive
+                    # relayer
+                    go
+                    mage
+                    revive
 
-                # parachain
-                rustup
-                gcc
-                openssl
-                libiconv
-                cmake
-                protobuf
-                clang
+                    # parachain
+                    rustup
+                    gcc
+                    openssl
+                    libiconv
+                    cmake
+                    protobuf
+                    clang
 
-                cowsay
-            ]);
-        in {
-            devShell.aarch64-darwin = pkgs.mkShell {
-                buildInputs = dependencies;
+                    cowsay
+                ];
+
+                shellHook = ''
+                    eval "$(direnv hook bash)"
+
+                    cowsay "Snowbridge Dev Environment"
+                '';
             };
-        };
+        }
+    );
 
     nixConfig = {};
 }
