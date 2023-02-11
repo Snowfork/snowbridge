236ad063520b6e3e4e3fcb24f76a48678e070325
ron <yrong1997@gmail.com>
2023-02-11 10:28:27 +0800

    Fix bug

--- core/packages/test/scripts/start-ethereum.sh
+++ core/packages/test/scripts/start-ethereum.sh
@@ -8,6 +8,7 @@ start_chains()
 {   #1 start ethereum and deploy contracts
     echo "start ethereum and deploy bridge contrancts"
     deploy_ethereum
+    echo "Chain started!"
 }
 
 if [ -z "${from_start_services:-}" ]; then
