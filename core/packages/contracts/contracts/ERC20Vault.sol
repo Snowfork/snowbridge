07326a6b820ba764ab3ed7fdfc004a4bcc3e8439
Alistair Singh <alistair.singh7@gmail.com>
2023-02-17 14:00:47 +0000

    removed underscores

--- core/packages/contracts/contracts/ERC20Vault.sol
+++ core/packages/contracts/contracts/ERC20Vault.sol
@@ -15,14 +15,14 @@ contract ERC20Vault is Ownable {
     /// @param sender The address of the sender.
     /// @param token The address of the ERC20 token.
     /// @param amount The amount being deposited.
-    event Deposit(address account, address sender, address token, uint128 amount);
+    event Deposit(address account, address sender, address token, uint256 amount);
 
     /// @dev Emitted when funds are withdrawn.
     /// @param account The address of the ERC20App contract.
     /// @param recipient The address of the sender.
     /// @param token The address of the ERC20 token.
     /// @param amount The amount being withdrawn.
-    event Withdraw(address account, address recipient, address token, uint128 amount);
+    event Withdraw(address account, address recipient, address token, uint256 amount);
 
     /// @dev Token Transfer failed.
     error TokenTransferFailed();
@@ -34,29 +34,29 @@ contract ERC20Vault is Ownable {
     mapping(address => uint256) public balances;
 
     /// @dev Accepts a ERC20 Token from the caller.
-    /// @param _sender The address of the sender.
-    /// @param _token The address of the Token.
-    /// @param _amount The amount being deposited.
-    function deposit(address _sender, address _token, uint128 _amount) external onlyOwner {
-        balances[_token] = balances[_token] + _amount;
+    /// @param sender The address of the sender.
+    /// @param token The address of the Token.
+    /// @param amount The amount being deposited.
+    function deposit(address sender, address token, uint256 amount) external onlyOwner {
+        balances[token] = balances[token] + amount;
         // TODO: Transfer ERC20 tokens safely. https://linear.app/snowfork/issue/SNO-366
-        if (!IERC20(_token).transferFrom(_sender, address(this), _amount)) {
+        if (!IERC20(token).transferFrom(sender, address(this), amount)) {
             revert TokenTransferFailed();
         }
-        emit Deposit(msg.sender, _sender, _token, _amount);
+        emit Deposit(msg.sender, sender, token, amount);
     }
 
     /// @dev Returns ETH to the caller.
-    /// @param _recipient The address that will receive funds.
-    /// @param _token The address of the Token.
-    /// @param _amount The amount being deposited.
-    function withdraw(address _recipient, address _token, uint128 _amount) external onlyOwner {
-        if (_amount > balances[_token]) {
+    /// @param recipient The address that will receive funds.
+    /// @param token The address of the Token.
+    /// @param amount The amount being deposited.
+    function withdraw(address recipient, address token, uint256 amount) external onlyOwner {
+        if (amount > balances[token]) {
             revert InsufficientBalance();
         }
 
-        balances[_token] = balances[_token] - _amount;
-        IERC20(_token).safeTransfer(_recipient, _amount);
-        emit Withdraw(msg.sender, _recipient, _token, _amount);
+        balances[token] = balances[token] - amount;
+        IERC20(token).safeTransfer(recipient, amount);
+        emit Withdraw(msg.sender, recipient, token, amount);
     }
 }
