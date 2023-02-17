98eb8a6fb626cb8c04523cbf820de1bbf769aa97
ron <yrong1997@gmail.com>
2023-02-17 20:14:43 +0800

    Refactor: move to initialize hook & some cleanup

--- flake.nix
+++ flake.nix
@@ -74,7 +74,10 @@
                     # LIBCLANG_PATH points rocksdb to a clang.so on Linux
                     export LIBCLANG_PATH="$(readlink -f ${pkgs.clang}/resource-root/include | xargs dirname | xargs dirname | xargs dirname)"
 
-                    cowsay "Snowbridge Dev Environment"
+                    echo "Initializing Dev Environment..."
+                    ./init.sh
+
+                    cowsay "Snowbridge Dev Environment Ready"
                 '';
             };
         }
---
+++ init.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env bash
+
+cd core && pnpm install && cd -
+
+echo "Hack SLOTS_IN_EPOCH in lodestar"
+cd core/packages/test && ./scripts/hack-ethereum.sh && cd -
+
+echo "Install foundry libraries"
+cd core/packages/contracts && forge install && cd -
+
+echo "Install husky hook"
+husky install
