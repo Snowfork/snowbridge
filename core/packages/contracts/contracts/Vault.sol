f43b37724c43c0f10d1247d5fe9f6cbed94e002a
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-02-17 12:17:33 +0400

    Reduce visibility modifiers in Vault

--- core/packages/contracts/contracts/Vault.sol
+++ core/packages/contracts/contracts/Vault.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.9;
 // This contract actually holds Ether balances for each sovereignID.
 contract Vault {
     // Mapping of sovereignID to balance
-    mapping(bytes32 => uint256) public balances;
+    mapping(bytes32 => uint256) private balances;
 
     // TODO: restrict access to SovereignTreasury
-    function deposit(bytes32 sovereignID) public payable {
+    function deposit(bytes32 sovereignID) external payable {
         balances[sovereignID] += msg.value;
     }
 
     // TODO: restrict access to SovereignTreasury
-    function withdraw(bytes32 sovereignID, uint256 amount) public {
+    function withdraw(bytes32 sovereignID, uint256 amount) external {
         require(balances[sovereignID] >= amount, "Insufficient funds for withdrawal");
 
         balances[sovereignID] -= amount;
