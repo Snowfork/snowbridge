c5e13cb85160f76ff561c0056a92a0e455832abc
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-02-17 13:27:21 +0400

    Make Vault Ownable

--- core/packages/contracts/contracts/Vault.sol
+++ core/packages/contracts/contracts/Vault.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 // This contract actually holds Ether balances for each sovereignID.
-contract Vault {
+// TODO: transfer ownership from deployer to SovereignTreasury
+contract Vault is Ownable {
     // Mapping of sovereignID to balance
     mapping(bytes32 => uint256) private _balances;
 
-    // TODO: restrict access to SovereignTreasury
-    function deposit(bytes32 sovereignID) external payable {
+    function deposit(bytes32 sovereignID) external payable onlyOwner {
         _balances[sovereignID] += msg.value;
     }
 
-    // TODO: restrict access to SovereignTreasury
-    function withdraw(bytes32 sovereignID, address payable recipient, uint256 amount) external {
+    function withdraw(
+        bytes32 sovereignID,
+        address payable recipient,
+        uint256 amount
+    ) external onlyOwner {
         require(_balances[sovereignID] >= amount, "Insufficient funds for withdrawal");
 
         _balances[sovereignID] -= amount;
