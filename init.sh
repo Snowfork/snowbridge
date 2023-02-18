c3e9cb2d58f5d65a7e6a4efe0bddb63faf297220
yrong <yrong1997@gmail.com>
2023-02-18 11:57:30 +0800

    Update foundry libs with submodule

--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
 
 #!/usr/bin/env bash
 
+echo "Initialize foundry libraries from submodule"
+git submodule update --init
+
+echo "Install node dependencies"
 cd core && pnpm install && cd -
 
 echo "Hack SLOTS_IN_EPOCH in lodestar"
 cd core/packages/test && ./scripts/hack-ethereum.sh && cd -
-
-echo "Install foundry libraries"
-cd core/packages/contracts && forge install && cd -
 
 echo "Install husky hook"
 ./core/node_modules/.bin/husky install
