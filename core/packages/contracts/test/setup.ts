81be78d839d35bc01edcbca6e8c1784caf95bcc5
Vincent Geddes <vincent.geddes@hey.com>
2023-03-04 12:26:35 +0200

    shuffle

--- core/packages/contracts/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "hardhat"
-import { expect } from "chai"
-import { loadFixture, mine, setPrevRandao } from "@nomicfoundation/hardhat-network-helpers"
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs"
-import { deployMockContract } from "@ethereum-waffle/mock-contract"
-import "@nomicfoundation/hardhat-chai-matchers"
-
-export { ethers, expect, loadFixture, mine, setPrevRandao, anyValue, deployMockContract }
