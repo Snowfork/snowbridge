c3bd614dffad1dae5996b8b67e6cfd3023669111
Alistair Singh <alistair.singh7@gmail.com>
2023-03-21 18:56:22 +0200

    Add bridge hub and statemint/e to e2e environment (#785)
    * make directory before copying
    
    * added cumulus build step
    
    * fixed path
    
    * launch statemint and bridge hub
    
    * fixed whitespace
    
    * fixed versions
    
    * add cumulus bin to envrc-template
    
    * removed snowbridge
    
    * generate bridge hub chain specs
    
    * remove uneeded mutations in the spec
    
    * use snowbridge cumulus fork
    
    * removed sub beefy magefile command
    
    * set version to snowbridge branch
    
    * updated README
    
    * removed commented out code
    
    * updated node info
    
    * update collator names
    
    * changed para id's
    
    * updated spec
    
    * changed runtime version
    
    * uncomment channel setup

--- relayer/magefile.go
+++ relayer/magefile.go
@@ -27,13 +27,3 @@ func Lint() error {
 func Install() error {
 	return sh.Run("go", "build", "-o", "$GOPATH/bin/snowbridge-relay", "main.go")
 }
-
-func SubBeef() error {
-	cmd := "go"
-	env := map[string]string{
-		"SNOWBRIDGE_BEEFY_KEY":     "0x935b65c833ced92c43ef9de6bff30703d941bd92a2637cb00cfad389f5862109",
-		"SNOWBRIDGE_MESSAGE_KEY":   "0x8013383de6e5a891e7754ae1ef5a21e7661f1fe67cd47ca8ebf4acd6de66879a",
-		"SNOWBRIDGE_PARACHAIN_KEY": "//Relay",
-	}
-	return sh.RunWithV(env, cmd, "run", "./main.go", "sub-beefy", "--config", "/tmp/snowbridge-e2e-config/config.toml")
-}
