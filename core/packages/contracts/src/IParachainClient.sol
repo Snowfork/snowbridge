31beba784eeb888de5710d1d3ae3d1265fe960c0
Vincent Geddes <vincent.geddes@hey.com>
2023-05-08 14:14:16 +0200

    Revert "Solmate proofs (#818)" (#825)
    This reverts commit 2fe5d29d423485e0784f09d7946a53dcfcf47915.

--- core/packages/contracts/src/IParachainClient.sol
+++ core/packages/contracts/src/IParachainClient.sol
@@ -2,32 +2,8 @@
 pragma solidity ^0.8.19;
 
 interface IParachainClient {
-    struct HeadProof {
-        uint256 pos;
-        uint256 width;
-        bytes32[] proof;
-    }
-
-    struct MMRLeafPartial {
-        uint8 version;
-        uint32 parentNumber;
-        bytes32 parentHash;
-        uint64 nextAuthoritySetID;
-        uint32 nextAuthoritySetLen;
-        bytes32 nextAuthoritySetRoot;
-    }
-
-    struct Proof {
-        bytes headPrefix;
-        bytes headSuffix;
-        HeadProof headProof;
-        MMRLeafPartial leafPartial;
-        bytes32[] leafProof;
-        uint256 leafProofOrder;
-    }
-
     function verifyCommitment(
         bytes32 commitment,
-        Proof calldata proof
+        bytes calldata opaqueProof
     ) external view returns (bool);
 }
