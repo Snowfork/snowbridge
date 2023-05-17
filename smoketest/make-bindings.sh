a89f29a2382765212a3259f362b470958f1c44a9
Alistair Singh <alistair.singh7@gmail.com>
2023-05-17 22:19:18 +0200

    add statemine parachain

--- smoketest/make-bindings.sh
+++ smoketest/make-bindings.sh
@@ -4,5 +4,15 @@ set -eu
 
 mkdir -p src/contracts
 
-subxt codegen --url http://localhost:11144 | rustfmt --edition 2021 --emit=stdout > src/parachains/bridgehub.rs
+subxt_version=v0.27.1
+cargo_dir=".cargo"
+export PATH=$PATH:$cargo_dir/bin
+
+command -v subxt || cargo install subxt-cli \
+    --git https://github.com/paritytech/subxt.git \
+    --tag $subxt_version \
+    --root $cargo_dir
+
+subxt codegen --url ws://localhost:11144 | rustfmt --edition 2021 --emit=stdout > src/parachains/bridgehub.rs
+subxt codegen --url ws://localhost:12144 | rustfmt --edition 2021 --emit=stdout > src/parachains/statemine.rs
 forge bind --module --overwrite --bindings-path src/contracts --root ../core/packages/contracts
