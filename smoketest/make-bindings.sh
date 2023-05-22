5dbbcb802556c892a2eab03ff27130f5f52c5a75
Alistair Singh <alistair.singh7@gmail.com>
2023-05-22 23:20:30 +0200

    Revert "add statemine parachain"
    This reverts commit 2637a3fcb4a7437a551b441911aa497ff7bb2814.

--- smoketest/make-bindings.sh
+++ smoketest/make-bindings.sh
@@ -4,15 +4,5 @@ set -eu
 
 mkdir -p src/contracts
 
-subxt_version=v0.27.1
-cargo_dir=".cargo"
-export PATH=$PATH:$cargo_dir/bin
-
-command -v subxt || cargo install subxt-cli \
-    --git https://github.com/paritytech/subxt.git \
-    --tag $subxt_version \
-    --root $cargo_dir
-
-subxt codegen --url ws://localhost:11144 | rustfmt --edition 2021 --emit=stdout > src/parachains/bridgehub.rs
-subxt codegen --url ws://localhost:12144 | rustfmt --edition 2021 --emit=stdout > src/parachains/statemine.rs
+subxt codegen --url http://localhost:11144 | rustfmt --edition 2021 --emit=stdout > src/parachains/bridgehub.rs
 forge bind --module --overwrite --bindings-path src/contracts --root ../core/packages/contracts
