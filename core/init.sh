7e608805cf706564000f898d909e69d5a9ac59d8
ron <yrong1997@gmail.com>
2023-06-02 22:01:48 +0800

    Fix nix setup

--- core/init.sh
+++ core/init.sh
@@ -6,6 +6,12 @@ echo "Update submodules"
 echo "Install husky hook"
 (cd .. && ./core/node_modules/.bin/husky install)
 
+echo "Installing sszgen"
+go install github.com/ferranbt/fastssz/sszgen@latest
+
+echo "Installing foundry"
+(curl -L https://foundry.paradigm.xyz | bash && foundryup)
+
 echo "Initialize foundry libraries"
-(cd packages/contracts && (forge install||true))
+(cd packages/contracts && forge install)
 
