e587b663f715f6dbc90bf0d8606c5e12dc56d606
ron <yrong1997@gmail.com>
2023-06-05 10:11:21 +0800

    Fix ci

--- core/init.sh
+++ core/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "Update submodules"
 (cd .. && (git submodule update --init --recursive||true))
@@ -9,8 +9,10 @@ echo "Install husky hook"
 echo "Installing sszgen"
 go install github.com/ferranbt/fastssz/sszgen@latest
 
-echo "Installing foundry"
-(curl -L https://foundry.paradigm.xyz | bash && foundryup)
+if ! [ -x "$(command -v forge)" ]; then
+    echo "Installing foundry"
+    curl -L https://foundry.paradigm.xyz | bash && source $HOME/.bashrc && foundryup
+fi
 
 echo "Initialize foundry libraries"
 (cd packages/contracts && forge install)
