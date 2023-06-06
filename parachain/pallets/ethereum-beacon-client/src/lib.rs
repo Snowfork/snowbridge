e36c5855c0d16a537043e779c2ac9ed4c63ad60a
claravanstaden <Cats 4 life!>
2023-06-06 12:11:32 +0200

    A few tiny comments.

--- parachain/pallets/ethereum-beacon-client/src/lib.rs
+++ parachain/pallets/ethereum-beacon-client/src/lib.rs
@@ -596,6 +596,7 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Combines
 		pub(super) fn compute_signing_root(
 			beacon_header: &BeaconHeader,
 			domain: H256,
@@ -721,6 +722,8 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Returns the fork version based on the current epoch. The hard fork versions
+		/// are defined in pallet config.
 		pub(super) fn compute_fork_version(epoch: u64) -> ForkVersion {
 			let fork_versions = T::ForkVersions::get();
 
@@ -737,6 +740,7 @@ pub mod pallet {
 			fork_versions.genesis.version
 		}
 
+		/// Returns a vector of public keys that par
 		pub fn find_pubkeys(
 			sync_committee_bits: &[u8],
 			sync_committee_pubkeys: &[PublicKeyPrepared],
@@ -751,7 +755,7 @@ pub mod pallet {
 			pubkeys
 		}
 
-		// Calculate signing root for BeaconHeader
+		/// Calculate signing root for BeaconHeader
 		pub fn signing_root(
 			header: &BeaconHeader,
 			validators_root: H256,
