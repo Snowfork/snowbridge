3e3dbbff673afc4654c4588f11312df425dc8a40
claravanstaden <Cats 4 life!>
2023-06-20 13:37:38 +0200

    Adds some comments to the Update payload fields.

--- parachain/primitives/beacon/src/updates.rs
+++ parachain/primitives/beacon/src/updates.rs
@@ -45,13 +45,24 @@ impl<const COMMITTEE_SIZE: usize> Default for CheckpointUpdate<COMMITTEE_SIZE> {
 	serde(deny_unknown_fields, bound(serialize = ""), bound(deserialize = ""))
 )]
 pub struct Update<const COMMITTEE_SIZE: usize, const COMMITTEE_BITS_SIZE: usize> {
+	/// A recent header attesting to the finalized header, in its `state_root`
 	pub attested_header: BeaconHeader,
+	/// The signing data that the sync committee produced for this attested header, including
+	/// who participated in the vote and the resulting signature
 	pub sync_aggregate: SyncAggregate<COMMITTEE_SIZE, COMMITTEE_BITS_SIZE>,
+	/// The slot at which the sync aggregate can be found, typically attested_header.slot + 1, if
+	/// the next slot block was not missed.
 	pub signature_slot: u64,
+	/// The next sync committee for the next sync committee period, if present.
 	pub next_sync_committee_update: Option<NextSyncCommitteeUpdate<COMMITTEE_SIZE>>,
+	/// The latest finalized header.
 	pub finalized_header: BeaconHeader,
+	/// The merkle proof testifying to the finalized header, using the `attested_header.state_root`
+	/// as tree root.
 	pub finality_branch: Vec<H256>,
+	/// The finalized_header's `block_roots` root in the beacon state, used for ancestry proofs.
 	pub block_roots_root: H256,
+	/// The merkle path to prove the `block_roots_root` value.
 	pub block_roots_branch: Vec<H256>,
 }
 
