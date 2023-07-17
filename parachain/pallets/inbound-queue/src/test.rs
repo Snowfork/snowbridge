ea9406f4332cc494e74bdcfd472b51b85c5aa994
ron <yrong1997@gmail.com>
2023-07-17 10:49:40 +0800

    Halting & resuming bridge pallets

--- parachain/pallets/inbound-queue/src/test.rs
+++ parachain/pallets/inbound-queue/src/test.rs
@@ -127,14 +127,19 @@ fn expect_events(e: Vec<RuntimeEvent>) {
 	assert_eq!(last_events(e.len()), e);
 }
 
-pub fn new_tester(outbound_queue_address: H160) -> sp_io::TestExternalities {
-	new_tester_with_config(inbound_queue::GenesisConfig { allowlist: vec![outbound_queue_address] })
+pub fn new_tester<T: Config>(outbound_queue_address: H160) -> sp_io::TestExternalities {
+	new_tester_with_config::<T>(inbound_queue::GenesisConfig {
+		allowlist: vec![outbound_queue_address],
+		owner: None,
+	})
 }
 
-pub fn new_tester_with_config(config: inbound_queue::GenesisConfig) -> sp_io::TestExternalities {
+pub fn new_tester_with_config<T: Config>(
+	config: inbound_queue::GenesisConfig<T>,
+) -> sp_io::TestExternalities {
 	let mut storage = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
 
-	GenesisBuild::<Test>::assimilate_storage(&config, &mut storage).unwrap();
+	GenesisBuild::<T>::assimilate_storage(&config, &mut storage).unwrap();
 
 	let mut ext: sp_io::TestExternalities = storage.into();
 	ext.execute_with(|| System::set_block_number(1));
@@ -170,7 +175,7 @@ use snowbridge_core::ParaId;
 
 #[test]
 fn test_submit() {
-	new_tester(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
+	new_tester::<Test>(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
 
@@ -202,7 +207,7 @@ fn test_submit() {
 
 #[test]
 fn test_submit_with_invalid_outbound_queue() {
-	new_tester(H160::zero()).execute_with(|| {
+	new_tester::<Test>(H160::zero()).execute_with(|| {
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
 
@@ -229,7 +234,7 @@ fn test_submit_with_invalid_outbound_queue() {
 
 #[test]
 fn test_submit_with_invalid_nonce() {
-	new_tester(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
+	new_tester::<Test>(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
 
@@ -263,7 +268,7 @@ fn test_submit_with_invalid_nonce() {
 
 #[test]
 fn test_submit_no_funds_to_reward_relayers() {
-	new_tester(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
+	new_tester::<Test>(OUTBOUND_QUEUE_ADDRESS.into()).execute_with(|| {
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
 
@@ -292,7 +297,7 @@ fn test_submit_no_funds_to_reward_relayers() {
 
 #[test]
 fn test_add_allow_list_without_root_yields_bad_origin() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
@@ -305,7 +310,7 @@ fn test_add_allow_list_without_root_yields_bad_origin() {
 
 #[test]
 fn test_add_allow_list_with_root_succeeds() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let origin = RuntimeOrigin::root();
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 
@@ -323,7 +328,7 @@ fn test_add_allow_list_with_root_succeeds() {
 
 #[test]
 fn test_add_allow_list_ignores_duplicates() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let origin = RuntimeOrigin::root();
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 
@@ -339,7 +344,7 @@ fn test_add_allow_list_ignores_duplicates() {
 
 #[test]
 fn test_add_allow_list_fails_when_exceeding_bounds() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let origin = RuntimeOrigin::root();
 		let contract_address1 = hex!("0000000000000000000000000000000000000000").into();
 		let contract_address2 = hex!("1000000000000000000000000000000000000000").into();
@@ -363,7 +368,7 @@ fn test_add_allow_list_fails_when_exceeding_bounds() {
 
 #[test]
 fn test_remove_allow_list_without_root_yields_bad_origin() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 		let relayer: AccountId = Keyring::Bob.into();
 		let origin = RuntimeOrigin::signed(relayer);
@@ -376,7 +381,7 @@ fn test_remove_allow_list_without_root_yields_bad_origin() {
 
 #[test]
 fn test_remove_allow_list_with_root_succeeds() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let origin = RuntimeOrigin::root();
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 
@@ -396,7 +401,7 @@ fn test_remove_allow_list_with_root_succeeds() {
 
 #[test]
 fn test_remove_allow_list_event_not_emitted_for_none_existent_item() {
-	new_tester_with_config(Default::default()).execute_with(|| {
+	new_tester_with_config::<Test>(Default::default()).execute_with(|| {
 		let origin = RuntimeOrigin::root();
 		let contract_address = hex!("0000000000000000000000000000000000000000").into();
 
