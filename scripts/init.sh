a05e583d3275edc1bfcd0e2bddddc673ec6413a0
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-08-01 16:03:07 -0400

    Merge web/init into scripts/init

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+echo "Setting up submodules"
 git submodule update --init --recursive || true
-(cd web && pnpm install)
-go install github.com/ferranbt/fastssz/sszgen@latest
+
+echo "Setting up git hooks"
 ln -sf pre-commit.sh .git/hooks/pre-commit
+
+echo "Installing sszgen"
+go install github.com/ferranbt/fastssz/sszgen@latest
+
+echo "Installing web packages"
+(cd web && pnpm install)
