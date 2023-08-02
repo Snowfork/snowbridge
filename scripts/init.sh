4d476005f8da52de96b7e2185fee96f2a792a904
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-08-02 05:33:35 -0400

    Replace toolchain file with commands in init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,14 +9,17 @@ echo "Setting up git hooks"
 ln -sf pre-commit.sh .git/hooks/pre-commit
 
 echo "Installing Rust toolchains"
-# NOTE: This ensures that the toolchain in rust-toolchain.toml is installed.
+# NOTE: Switch back to the toolchain file once it supports explicit installation.
 # rustup has no subcommand (yet) for installing the toolchain in rust-toolchain.toml:
 # https://github.com/rust-lang/rustup/issues/2686
-# This auto-installation behaviour in rustup will likely change:
+# The auto-installation behaviour in rustup will likely change:
 # https://github.com/rust-lang/rustup/issues/1397
-rustup show
+rustup install --profile minimal 1.70.0
+rustup default 1.70.0
+rustup target add --toolchain 1.70.0 wasm32-unknown-unknown
+rustup component add --toolchain 1.70.0 clippy rust-analyzer rust-src
 rustup install --profile minimal nightly-2023-07-31
-rustup component add rustfmt --toolchain nightly-2023-07-31
+rustup component add --toolchain nightly-2023-07-31 rustfmt
 
 echo "Installing sszgen"
 go install github.com/ferranbt/fastssz/sszgen@latest
