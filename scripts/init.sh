5e85a0ac00797d999d50914ef32a683b68ec0458
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-08-02 13:20:12 -0400

    Remove nightly toolchain file

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,17 +8,9 @@ git submodule update --init --recursive || true
 echo "Setting up git hooks"
 ln -sf pre-commit.sh .git/hooks/pre-commit
 
-echo "Installing Rust toolchains"
-# NOTE: This ensures that the toolchain in rust-toolchain.toml is installed.
-# rustup has no subcommand (yet) for installing the toolchain in rust-toolchain.toml:
-# https://github.com/rust-lang/rustup/issues/2686
-# The auto-installation behaviour in rustup will likely be removed:
-# https://github.com/rust-lang/rustup/issues/1397
-cp rust-toolchain-stable.toml rust-toolchain.toml
-rustup show
-cp rust-toolchain-nightly.toml rust-toolchain.toml
-rustup show
-cp rust-toolchain-stable.toml rust-toolchain.toml
+echo "Installing Rust nightly toolchain"
+rustup install --profile minimal nightly-2023-05-23
+rustup component add --toolchain nightly-2023-05-23 rustfmt
 
 echo "Installing sszgen"
 go install github.com/ferranbt/fastssz/sszgen@latest
