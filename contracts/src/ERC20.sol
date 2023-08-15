f08ec04e5e0524358016dd796a1e18ec2d411e2e
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-08-15 10:51:41 -0400

    Clean up main docstring

--- contracts/src/ERC20.sol
+++ contracts/src/ERC20.sol
@@ -9,19 +9,15 @@ import {IERC20} from "./interfaces/IERC20.sol";
  *
  * This implementation is agnostic to the way tokens are created. This means
  * that a supply mechanism has to be added in a derived contract using {_mint}.
- * For a generic mechanism see {ERC20PresetMinterPauser}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
+ * This supply mechanism has been added in {ERC20Permit-mint}.
  *
  * We have followed general OpenZeppelin guidelines: functions revert instead
- * of returning `false` on failure. This behavior is nonetheless conventional
- * and does not conflict with the expectations of ERC20 applications.
+ * of returning `false` on failure. This behavior is conventional and does
+ * not conflict with the expectations of ERC20 applications.
  *
  * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
  * This allows applications to reconstruct the allowance for all accounts just
- * by listening to said events. Other implementations of the EIP may not emit
+ * by listening to these events. Other implementations of the EIP may not emit
  * these events, as it isn't required by the specification.
  *
  * Finally, the non-standard {decreaseAllowance} and {increaseAllowance}
