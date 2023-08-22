8b8f1b5af3fc0a622f207d8069a04c0821ad67ce
Alistair Singh <alistair.singh7@gmail.com>
2023-08-22 16:39:05 +0200

    Standardize variables for deployment (#928)
    * renamed statemine to asset hub and allow agent id overrides
    
    * fix unbound variable
    
    * upgrade contract hash change
    
    * update variables

--- web/packages/test/scripts/set_operating_mode.sh
+++ web/packages/test/scripts/set_operating_mode.sh
@@ -6,12 +6,12 @@ source scripts/xcm-helper.sh
 
 enable_gateway() {
     local transact_call="0x330400"
-    send_governance_transact_from_relaychain $bridgehub_para_id "$transact_call"
+    send_governance_transact_from_relaychain $BRIDGE_HUB_PARAID "$transact_call"
 }
 
 disable_gateway() {
     local transact_call="0x330401"
-    send_governance_transact_from_relaychain $bridgehub_para_id "$transact_call"
+    send_governance_transact_from_relaychain $BRIDGE_HUB_PARAID "$transact_call"
 }
 
 read -p "Enable gateway? (Y/N): " confirm
