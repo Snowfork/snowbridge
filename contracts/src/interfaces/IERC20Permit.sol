e3b20b9c7820390704afbd1ffed637003d7f3a24
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-08-24 17:38:36 +0200

    SNO-604: Add ERC20 contract (#923)
    * Add Axelar ERC20 contract
    
    * Add external mint method restricted to ERC20 owner
    
    * Fix import path
    
    * Add docstring to external mint method
    
    * Remove unused remapping
    
    * Add ERC20Permit
    
    * Clean up main docstring
    
    * Add note about frontrunning attack
    
    * Pass args to ERC20 constructor
    
    * Add copyright notices
    
    * Remove OpenZeppelin's Ownable
    
    * Merge ERC20Permit into ERC20
    
    * Make solidity versions consistent
    
    * Add Snowfork copyright

---
+++ contracts/src/interfaces/IERC20Permit.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023 Axelar Network
+// SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
+
+pragma solidity 0.8.20;
+
+interface IERC20Permit {
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function nonces(address account) external view returns (uint256);
+
+    function permit(address issuer, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
+}
