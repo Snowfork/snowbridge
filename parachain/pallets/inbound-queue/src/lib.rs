9a4376b7fe97bf48236b3b63cc6754dd4d705742
claravanstaden <Cats 4 life!>
2023-09-01 10:01:55 +0200

    Fix compiler error

--- parachain/pallets/inbound-queue/src/lib.rs
+++ parachain/pallets/inbound-queue/src/lib.rs
@@ -192,8 +192,8 @@ pub mod pallet {
 
 			// Verify message nonce
 			<Nonce<T>>::try_mutate(envelope.dest, |nonce| -> DispatchResult {
-				if nonce == u64::MAX {
-					Err(Error::<T>::MaxNonceReached.into())
+				if *nonce == u64::MAX {
+					return Err(Error::<T>::MaxNonceReached.into());
 				}
 				if envelope.nonce != nonce.saturating_add(1) {
 					Err(Error::<T>::InvalidNonce.into())
