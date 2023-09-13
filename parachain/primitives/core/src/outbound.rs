82d2ed54930c4cea467108c6533888fba40100fc
ron <yrong1997@gmail.com>
2023-09-13 11:19:28 +0800

    Chore format

--- parachain/primitives/core/src/outbound.rs
+++ parachain/primitives/core/src/outbound.rs
@@ -136,7 +136,7 @@ impl Command {
 
 	/// Compute gas cost
 	/// Todo: load by trait read from configurable storage,reference result from benchmark report
-/// | Function Name    | min    | avg    | median | max    | # calls |
+	/// | Function Name    | min    | avg    | median | max    | # calls |
 	/// | createAgent    | 839    | 184709 | 237187 | 237187 | 9       |     
 	/// | createChannel    | 399    | 31023  | 2829   | 75402  | 5       |
 	/// | updateChannel    | 817    | 15121  | 3552   | 36762  | 5     |     
