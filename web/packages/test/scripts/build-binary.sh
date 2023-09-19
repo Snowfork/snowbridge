c7f0392c9bd740463cb57b47a2a8b9060b31a4a9
Alistair Singh <alistair.singh7@gmail.com>
2023-09-19 18:07:46 +0200

    fixed bad builds

--- web/packages/test/scripts/build-binary.sh
+++ web/packages/test/scripts/build-binary.sh
@@ -5,38 +5,34 @@ source scripts/set-env.sh
 
 build_cumulus_from_source() {
     echo "Building polkadot-parachain binary"
-    pushd $root_dir/polkadot-sdk
+    pushd $root_dir/polkadot-sdk/cumulus
 
     local features=''
     if [[ "$active_spec" != "minimal" ]]; then
         features=--features beacon-spec-mainnet 
     fi
 
-    cargo build --release --locked -p polkadot-parachain-bin $features
-    cp target/release/polkadot-parachain $output_bin_dir/polkadot-parachain
+    cargo build --release --locked --bin polkadot-parachain $features
+    cp ../target/release/polkadot-parachain $output_bin_dir/polkadot-parachain
 
     echo "Building polkadot-parachain binary"
-    cargo build --release --locked -p parachain-template-node
-    cp target/release/parachain-template-node $output_bin_dir/parachain-template-node
+    cargo build --release --locked --bin parachain-template-node
+    cp ../target/release/parachain-template-node $output_bin_dir/parachain-template-node
     popd
 }
 
 build_relaychain_from_source() {
-    if [ ! -f "$output_bin_dir/polkadot" ]; then
-        echo "Building polkadot binary"
-        pushd $root_dir/polkadot-sdk
+    echo "Building polkadot binary"
+    pushd $root_dir/polkadot-sdk
 
-        cargo build --release --locked --bin polkadot --bin polkadot-execute-worker --bin polkadot-prepare-worker 
-        mkdir -p $output_bin_dir
+    cargo build --release --locked --bin polkadot --bin polkadot-execute-worker --bin polkadot-prepare-worker 
+    mkdir -p $output_bin_dir
 
-        cp target/release/polkadot $output_bin_dir/polkadot
-        cp target/release/polkadot-execute-worker $output_bin_dir/polkadot-execute-worker
-        cp target/release/polkadot-prepare-worker $output_bin_dir/polkadot-prepare-worker
+    cp target/release/polkadot $output_bin_dir/polkadot
+    cp target/release/polkadot-execute-worker $output_bin_dir/polkadot-execute-worker
+    cp target/release/polkadot-prepare-worker $output_bin_dir/polkadot-prepare-worker
 
-        popd
-    else
-        echo "SKIPPING polkadot build."
-    fi
+    popd
 }
 
 build_contracts() {
