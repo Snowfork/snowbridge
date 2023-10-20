9d3429bc51183a9d47cccb4ea88fd20e69450bc3
claravanstaden <Cats 4 life!>
2023-10-20 10:10:52 +0200

    finishing up applying changes

--- smoketest/tests/transfer_token.rs
+++ smoketest/tests/transfer_token.rs
@@ -1,20 +1,20 @@
+use assethub::api::bridge_transfer::calls::TransactionApi;
+use ethers::prelude::Middleware;
 use ethers::{
     providers::{Provider, Ws},
     types::Address,
 };
-use std::{sync::Arc, time::Duration};
-use ethers::prelude::Middleware;
-use snowbridge_smoketest::contracts::{i_gateway::IGateway, weth9::WETH9};
-use subxt::{
-    tx::{PairSigner},
-    OnlineClient,
+use futures::StreamExt;
+use hex_literal::hex;
+use snowbridge_smoketest::constants::{
+    ASSET_HUB_AGENT_ID, ASSET_HUB_WS_URL, ETHEREUM_API, GATEWAY_PROXY_CONTRACT, WETH_CONTRACT,
 };
-use snowbridge_smoketest::constants::{ASSET_HUB_WS_URL, ETHEREUM_API, GATEWAY_PROXY_CONTRACT, WETH_CONTRACT, ASSET_HUB_AGENT_ID};
-use sp_core::{sr25519::Pair, Pair as PairT};
+use snowbridge_smoketest::contracts::{i_gateway::IGateway, weth9::WETH9};
+use snowbridge_smoketest::helper::AssetHubConfig;
 use snowbridge_smoketest::{
-    contracts::weth9::{TransferFilter},
+    contracts::weth9::TransferFilter,
     parachains::{
-        assethub::api::runtime_types::xcm::{
+        assethub::api::runtime_types::staging_xcm::{
             v3::{
                 junction::{Junction, NetworkId},
                 junctions::Junctions,
@@ -26,10 +26,9 @@ use snowbridge_smoketest::{
         assethub::{self},
     },
 };
-use hex_literal::hex;
-use assethub::api::bridge_transfer::calls::TransactionApi;
-use futures::StreamExt;
-use snowbridge_smoketest::helper::AssetHubConfig;
+use sp_core::{sr25519::Pair, Pair as PairT};
+use std::{sync::Arc, time::Duration};
+use subxt::{tx::PairSigner, OnlineClient};
 
 const DESTINATION_ADDRESS: [u8; 20] = hex!("44a57ee2f2FCcb85FDa2B0B18EBD0D8D2333700e");
 
@@ -40,14 +39,14 @@ async fn transfer_token() {
         .unwrap()
         .interval(Duration::from_millis(10u64));
 
-
     let ethereum_client = Arc::new(ethereum_provider);
 
     let weth_addr: Address = WETH_CONTRACT.into();
     let weth = WETH9::new(weth_addr, ethereum_client.clone());
 
     let gateway = IGateway::new(GATEWAY_PROXY_CONTRACT, ethereum_client.clone());
-    let agent_src = gateway.agent_of(ASSET_HUB_AGENT_ID)
+    let agent_src = gateway
+        .agent_of(ASSET_HUB_AGENT_ID)
         .await
         .expect("could not get agent address");
 
@@ -64,8 +63,14 @@ async fn transfer_token() {
             parents: 2,
             interior: Junctions::X3(
                 Junction::GlobalConsensus(NetworkId::Ethereum { chain_id: 15 }),
-                Junction::AccountKey20 { network: None, key: GATEWAY_PROXY_CONTRACT.into() },
-                Junction::AccountKey20 { network: None, key: WETH_CONTRACT.into() },
+                Junction::AccountKey20 {
+                    network: None,
+                    key: GATEWAY_PROXY_CONTRACT.into(),
+                },
+                Junction::AccountKey20 {
+                    network: None,
+                    key: WETH_CONTRACT.into(),
+                },
             ),
         }),
         fun: Fungibility::Fungible(amount),
@@ -75,7 +80,10 @@ async fn transfer_token() {
         parents: 2,
         interior: Junctions::X2(
             Junction::GlobalConsensus(NetworkId::Ethereum { chain_id: 15 }),
-            Junction::AccountKey20 { network: None, key: DESTINATION_ADDRESS.into() },
+            Junction::AccountKey20 {
+                network: None,
+                key: DESTINATION_ADDRESS.into(),
+            },
         ),
     });
 
@@ -90,19 +98,35 @@ async fn transfer_token() {
         .await
         .expect("call success");
 
-    println!("bridge_transfer call issued at assethub block hash {:?}", result.block_hash());
+    println!(
+        "bridge_transfer call issued at assethub block hash {:?}",
+        result.block_hash()
+    );
 
     let wait_for_blocks = 50;
-    let mut stream = ethereum_client.subscribe_blocks().await.unwrap().take(wait_for_blocks);
+    let mut stream = ethereum_client
+        .subscribe_blocks()
+        .await
+        .unwrap()
+        .take(wait_for_blocks);
 
     let mut transfer_event_found = false;
     while let Some(block) = stream.next().await {
-        println!("Polling ethereum block {:?} for transfer event", block.number.unwrap());
-        if let Ok(transfers) =
-            weth.event::<TransferFilter>().at_block_hash(block.hash.unwrap()).query().await
+        println!(
+            "Polling ethereum block {:?} for transfer event",
+            block.number.unwrap()
+        );
+        if let Ok(transfers) = weth
+            .event::<TransferFilter>()
+            .at_block_hash(block.hash.unwrap())
+            .query()
+            .await
         {
             for transfer in transfers {
-                println!("Transfer event found at ethereum block {:?}", block.number.unwrap());
+                println!(
+                    "Transfer event found at ethereum block {:?}",
+                    block.number.unwrap()
+                );
                 assert_eq!(transfer.src, agent_src.into());
                 assert_eq!(transfer.dst, DESTINATION_ADDRESS.into());
                 assert_eq!(transfer.wad, amount.into());
