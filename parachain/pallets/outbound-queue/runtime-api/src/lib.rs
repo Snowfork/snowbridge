22716b5384d47e06eeb346bb47eb3fbafbfe712c
ron <yrong1997@gmail.com>
2023-10-23 21:40:58 +0800

    Runtime api for estimate_fee

--- parachain/pallets/outbound-queue/runtime-api/src/lib.rs
+++ parachain/pallets/outbound-queue/runtime-api/src/lib.rs
@@ -2,11 +2,14 @@
 // SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use snowbridge_core::outbound::{Message, OutboundFee};
 use snowbridge_outbound_queue_merkle_tree::MerkleProof;
 
 sp_api::decl_runtime_apis! {
 	pub trait OutboundQueueApi
 	{
 		fn prove_message(leaf_index: u64) -> Option<MerkleProof>;
+
+		fn estimate_fee(message: Message) -> Option<OutboundFee<u128>>;
 	}
 }
--- parachain/pallets/outbound-queue/src/api.rs
+++ parachain/pallets/outbound-queue/src/api.rs
@@ -4,7 +4,9 @@
 
 use crate::{Config, MessageLeaves};
 use frame_support::storage::StorageStreamIter;
+use snowbridge_core::outbound::{Message, OutboundFee, OutboundQueue};
 use snowbridge_outbound_queue_merkle_tree::{merkle_proof, MerkleProof};
+use sp_arithmetic::traits::SaturatedConversion;
 
 pub fn prove_message<Runtime>(leaf_index: u64) -> Option<MerkleProof>
 where
@@ -18,4 +20,16 @@ where
 		leaf_index,
 	);
 	Some(proof)
+}
+
+pub fn estimate_fee<Runtime>(message: Message) -> Option<OutboundFee<u128>>
+where
+	Runtime: Config,
+{
+	let fee = crate::Pallet::<Runtime>::validate(&message).ok()?.1;
+	Some(OutboundFee::<u128> {
+		base_fee: fee.base_fee.saturated_into::<u128>(),
+		delivery_fee: fee.delivery_fee.saturated_into::<u128>(),
+		voucher_required: fee.voucher_required,
+	})
 }
