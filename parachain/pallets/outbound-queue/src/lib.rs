97f08198683947137147a794df6cb14e70d3cdd5
claravanstaden <Cats 4 life!>
2023-11-02 10:24:02 +0200

    starts with adding snowbridge conf

--- parachain/pallets/outbound-queue/src/lib.rs
+++ parachain/pallets/outbound-queue/src/lib.rs
@@ -249,6 +249,8 @@ pub mod pallet {
 
 	#[pallet::type_value]
 	pub fn DefaultFeeConfig() -> FeeConfigRecord {
+		// When the FeeConfigRecord is updated, so should this fee constant on asset hub:
+		//polkadot-sdk/cumulus/parachains/common/src/snowbridge_config.rs
 		FeeConfigRecord {
 			exchange_rate: FixedU128::saturating_from_rational(1, 400),
 			fee_per_gas: 30 * GWEI,
@@ -311,7 +313,7 @@ pub mod pallet {
 		pub(crate) fn commit() {
 			let count = MessageLeaves::<T>::decode_len().unwrap_or_default() as u64;
 			if count == 0 {
-				return
+				return;
 			}
 
 			// Create merkle root of messages
@@ -337,8 +339,8 @@ pub mod pallet {
 			// Yield if the maximum number of messages has been processed this block.
 			// This ensures that the weight of `on_finalize` has a known maximum bound.
 			ensure!(
-				MessageLeaves::<T>::decode_len().unwrap_or(0) <
-					T::MaxMessagesPerBlock::get() as usize,
+				MessageLeaves::<T>::decode_len().unwrap_or(0)
+					< T::MaxMessagesPerBlock::get() as usize,
 				Yield
 			);
 
