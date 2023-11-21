ef009c0416d1614f20ac604dbacd40c4438e00b3
Alistair Singh <alistair.singh7@gmail.com>
2023-11-21 23:52:54 +0200

    create the asset on penpal

--- smoketest/tests/send_token_to_penpal.rs
+++ smoketest/tests/send_token_to_penpal.rs
@@ -6,7 +6,7 @@ use futures::StreamExt;
 use snowbridge_smoketest::{
 	constants::*,
 	contracts::{i_gateway, weth9},
-	helper::initial_clients,
+	helper::{initial_clients, PenpalConfig},
 	parachains::{
 		assethub::api::{
 			foreign_assets::events::Issued as AssetHubIssued,
@@ -21,18 +21,22 @@ use snowbridge_smoketest::{
 				},
 			},
 		},
-		penpal::api::foreign_assets::events::Issued as PenpalIssued,
+		penpal::{self, api::foreign_assets::events::Issued as PenpalIssued},
 	},
 };
-use sp_core::Encode;
-use subxt::utils::AccountId32;
+use sp_core::{sr25519::Pair, Encode, Pair as PairT};
+use subxt::{
+	tx::PairSigner,
+	utils::{AccountId32, MultiAddress},
+	OnlineClient,
+};
 
 #[tokio::test]
 async fn send_token_to_penpal() {
 	let test_clients = initial_clients().await.expect("initialize clients");
 	let ethereum_client = *(test_clients.ethereum_signed_client.clone());
-	let assethub = *(test_clients.asset_hub_client.clone());
-	let penpal = *(test_clients.penpal_client.clone());
+	let assethub_client = *(test_clients.asset_hub_client.clone());
+	let penpal_client = *(test_clients.penpal_client.clone());
 
 	let gateway_addr: Address = GATEWAY_PROXY_CONTRACT.into();
 	let gateway = i_gateway::IGateway::new(gateway_addr, ethereum_client.clone());
@@ -44,6 +48,8 @@ async fn send_token_to_penpal() {
 	let value = parse_units("1", "ether").unwrap();
 	let receipt = weth.deposit().value(value).send().await.unwrap().await.unwrap().unwrap();
 	assert_eq!(receipt.status.unwrap().as_u64(), 1u64);
+
+	ensure_penpal_asset_exists(&mut test_clients.penpal_client.clone()).await;
 
 	// Approve token spend
 	weth.approve(gateway_addr, value.into())
@@ -77,7 +83,7 @@ async fn send_token_to_penpal() {
 	assert_eq!(receipt.status.unwrap().as_u64(), 1u64);
 
 	let wait_for_blocks = 50;
-	let mut assethub_blocks = assethub
+	let mut assethub_blocks = assethub_client
 		.blocks()
 		.subscribe_finalized()
 		.await
@@ -112,7 +118,7 @@ async fn send_token_to_penpal() {
 	}
 	assert!(issued_event_found);
 
-	let mut penpal_blocks = penpal
+	let mut penpal_blocks = penpal_client
 		.blocks()
 		.subscribe_finalized()
 		.await
@@ -139,4 +145,54 @@ async fn send_token_to_penpal() {
 		}
 	}
 	assert!(issued_event_found);
+}
+
+async fn ensure_penpal_asset_exists(penpal_client: &mut OnlineClient<PenpalConfig>) {
+	use penpal::api::runtime_types::{
+		staging_xcm::v3::multilocation::MultiLocation,
+		xcm::v3::{
+			junction::{
+				Junction::{AccountKey20, GlobalConsensus},
+				NetworkId,
+			},
+			junctions::Junctions::X2,
+		},
+	};
+	let penpal_asset_id = MultiLocation {
+		parents: 2,
+		interior: X2(
+			GlobalConsensus(NetworkId::Ethereum { chain_id: ETHEREUM_CHAIN_ID }),
+			AccountKey20 { network: None, key: WETH_CONTRACT.into() },
+		),
+	};
+
+	let penpal_asset_address = penpal::api::storage().foreign_assets().asset(&penpal_asset_id);
+	let result = penpal_client
+		.storage()
+		.at(None)
+		.await
+		.unwrap()
+		.fetch(&penpal_asset_address)
+		.await
+		.unwrap();
+
+	if result.is_some() {
+		println!("WETH asset exists on penpal.");
+		return
+	}
+
+	println!("creating WETH on penpal.");
+	let admin = MultiAddress::Address32(ASSET_HUB_SOVEREIGN);
+	let keypair: Pair = Pair::from_string("//Ferdie", None).expect("cannot create keypair");
+	let signer: PairSigner<PenpalConfig, _> = PairSigner::new(keypair);
+
+	let create_asset_call = penpal::api::tx().foreign_assets().create(penpal_asset_id, admin, 1);
+	penpal_client
+		.tx()
+		.sign_and_submit_then_watch_default(&create_asset_call, &signer)
+		.await
+		.unwrap()
+		.wait_for_finalized_success()
+		.await
+		.expect("asset created");
 }
