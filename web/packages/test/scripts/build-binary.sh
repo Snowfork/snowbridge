395b5752327e8c269e0828fb9398d2ced475673a
claravanstaden <Cats 4 life!>
2023-11-29 20:30:50 +0200

    check if the relay chain needs to be built

--- web/packages/test/scripts/build-binary.sh
+++ web/packages/test/scripts/build-binary.sh
@@ -11,8 +11,17 @@ build_binaries() {
         features=--features beacon-spec-mainnet
     fi
 
-    echo "Building polkadot binary"
-    cargo build --release --locked --bin polkadot --bin polkadot-execute-worker --bin polkadot-prepare-worker
+    check_local_changes "polkadot"
+    check_local_changes "substrate"
+
+  # Check that all 3 binaries are available and no changes made in the polkadot and substrate dirs
+    if [[ ! -e "target/release/polkadot" || ! -e "target/release/polkadot-execute-worker" || ! -e "target/release/polkadot-prepare-worker" || "$changes_detected" -eq 1 ]]; then
+        echo "Building polkadot binary, due to changes detected in polkadot or substrate, or binaries not found"
+        cargo build --release --locked --bin polkadot --bin polkadot-execute-worker --bin polkadot-prepare-worker
+    else
+        echo "No changes detected in polkadot or substrate and binaries are available, not rebuilding relaychain binaries."
+    fi
+
     cp target/release/polkadot $output_bin_dir/polkadot
     cp target/release/polkadot-execute-worker $output_bin_dir/polkadot-execute-worker
     cp target/release/polkadot-prepare-worker $output_bin_dir/polkadot-prepare-worker
@@ -22,6 +31,17 @@ build_binaries() {
     cp target/release/polkadot-parachain $output_bin_dir/polkadot-parachain
 
     popd
+}
+
+changes_detected=0
+
+check_local_changes() {
+    local dir=$1
+    cd "$dir"
+    if git status --porcelain | grep .; then
+        changes_detected=1
+    fi
+    cd -
 }
 
 build_contracts() {
