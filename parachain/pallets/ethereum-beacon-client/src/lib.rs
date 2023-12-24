6dd771e1ab559d4c5b7cd30ea85408d3fe5dc8b1
Vincent Geddes <vincent.geddes@hey.com>
2023-12-24 20:16:38 +0200

    Revert "trigger ci"
    This reverts commit bc2832ca5d59089e3e282f437109a84c98b2850d.

--- parachain/pallets/ethereum-beacon-client/src/lib.rs
+++ parachain/pallets/ethereum-beacon-client/src/lib.rs
@@ -61,7 +61,7 @@ pub use pallet::*;
 
 pub use config::SLOTS_PER_HISTORICAL_ROOT;
 
-pub const LOG_TARGET: &str = "foo";
+pub const LOG_TARGET: &str = "ethereum-beacon-client";
 
 #[frame_support::pallet]
 pub mod pallet {
