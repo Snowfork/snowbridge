d1a5b8648f7f4b44041f3b9260667989a290cba2
ron <yrong1997@gmail.com>
2024-01-12 09:01:57 +0800

    Update go mod

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
 
                     # ethereum
                     foundry-bin
-                    go-ethereum
+                    # go-ethereum
                     # gnupg for forge install
                     gnupg
 
