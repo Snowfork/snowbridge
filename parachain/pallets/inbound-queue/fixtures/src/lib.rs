204924deb3e3883ed5f8352da84248878fc1430c
ron <yrong1997@gmail.com>
2024-01-25 16:53:00 +0800

    Cleanup

--- parachain/pallets/inbound-queue/fixtures/src/lib.rs
+++ parachain/pallets/inbound-queue/fixtures/src/lib.rs
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::{Decode, Encode};
 use snowbridge_beacon_primitives::CompactExecutionHeader;
 use snowbridge_core::inbound::Message;
 use sp_core::RuntimeDebug;
@@ -10,7 +9,7 @@ use sp_core::RuntimeDebug;
 pub mod register_token;
 pub mod send_token;
 
-#[derive(Clone, Encode, Decode, RuntimeDebug)]
+#[derive(Clone, RuntimeDebug)]
 pub struct InboundQueueFixture {
 	pub execution_header: CompactExecutionHeader,
 	pub message: Message,
