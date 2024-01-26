4011f8e3fd0b32ad5cd8592546e41795b0992d50
claravanstaden <Cats 4 life!>
2024-01-26 11:01:29 +0200

    Adds Ethereum constant comments

--- parachain/pallets/ethereum-client/src/config/mod.rs
+++ parachain/pallets/ethereum-client/src/config/mod.rs
@@ -46,9 +46,17 @@ pub const SIGNATURE_SIZE: usize = 96;
 
 const_assert!(SYNC_COMMITTEE_BITS_SIZE == SYNC_COMMITTEE_SIZE / 8);
 
+/// Defined in <https://github.com/ethereum/consensus-specs/tree/f1dff5f6768608d890fc0b347e548297fc3e1f1c/presets/mainnet>
+/// There are 32 slots in an epoch. An epoch is 6.4 minutes long.
 pub const SLOTS_PER_EPOCH: usize = 32;
+/// 256 epochs in a sync committee period. Frequency of sync committee (subset of Ethereum
+/// validators) change is every ~27 hours.
 pub const EPOCHS_PER_SYNC_COMMITTEE_PERIOD: usize = 256;
+/// A sync committee contains 512 randomly selected validators.
 pub const SYNC_COMMITTEE_SIZE: usize = 512;
+/// An array of sync committee block votes, one bit representing the vote of one validator.
 pub const SYNC_COMMITTEE_BITS_SIZE: usize = SYNC_COMMITTEE_SIZE / 8;
+/// The size of the block root array in the beacon state, used for ancestry proofs.
 pub const SLOTS_PER_HISTORICAL_ROOT: usize = 8192;
+/// The index of the block_roots field in the beacon state tree.
 pub const BLOCK_ROOT_AT_INDEX_DEPTH: usize = 13;
