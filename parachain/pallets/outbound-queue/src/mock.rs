70c4a67774ad748dce1c2b252ae8ba4b8abece25
claravanstaden <Cats 4 life!>
2024-01-26 14:11:50 +0200

    revert mock runtime changes

--- parachain/pallets/outbound-queue/src/mock.rs
+++ parachain/pallets/outbound-queue/src/mock.rs
@@ -3,7 +3,7 @@
 use super::*;
 
 use frame_support::{
-	derive_impl, parameter_types,
+	parameter_types,
 	traits::{Everything, Hooks},
 	weights::IdentityFee,
 };
@@ -37,8 +37,30 @@ parameter_types! {
 	pub const BlockHashCount: u64 = 250;
 }
 
-#[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Test {
+	type BaseCallFilter = Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type RuntimeTask = RuntimeTask;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = BlockHashCount;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+	type Nonce = u64;
 	type Block = Block;
 }
 
