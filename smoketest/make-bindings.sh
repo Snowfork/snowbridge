1335643d2c249860f030d6bf5b39e87d36465404
claravanstaden <Cats 4 life!>
2024-02-06 10:48:35 +0200

    revert using the snowbridge fork of subxt

--- smoketest/make-bindings.sh
+++ smoketest/make-bindings.sh
@@ -11,9 +11,9 @@ forge bind --module --overwrite \
     --root ../contracts
 
 # Install subxt
-cargo install subxt-cli \
-    --git https://github.com/Snowfork/subxt.git \
-    --branch snowbridge
+command -v subxt || cargo install subxt-cli \
+    --git https://github.com/paritytech/subxt.git \
+    --tag v0.27.1
 
 if ! lsof -Pi :11144 -sTCP:LISTEN -t >/dev/null; then
     echo "substrate nodes not running, please start with the e2e setup and rerun this script"
