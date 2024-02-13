4c185187a487bc8f6879828f05206da75d6470b6
Alistair Singh <alistair.singh7@gmail.com>
2024-02-13 19:37:15 +0200

    added ui skeleton

---
+++ web/packages/ui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
