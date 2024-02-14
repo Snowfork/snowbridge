6aca13d9a991aacecb4b31b91547265a3f76c015
claravanstaden <Cats 4 life!>
2024-02-14 21:52:54 +0200

    adjust scripts

--- web/packages/test/scripts/check-relayer.sh
+++ web/packages/test/scripts/check-relayer.sh
@@ -2,20 +2,7 @@
 set -eu
 
 url="http://localhost:8080/health"
-
-while true; do
-  status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "$url")
-
-  if [ "$status_code" -eq 200 ]; then
-    echo "Beacon relayer has started.."
-    exit 0
-  else
-    echo "Beacon relayer has not started yet. Waiting for 10 seconds."
-    sleep 10
-  fi
-done
-
-
+echo "checking start-services and relayer state"
 while true; do
   # Check if start-services.sh is still running
   pgrep -f start-services.sh > /dev/null
@@ -30,7 +17,7 @@ while true; do
   # Check if the status code is 200
   if [ "$status_code" -eq 200 ]; then
     echo "Beacon relayer has started.."
-        exit 0
+    exit 0
   else
     echo "Beacon relayer has not started yet. Waiting for 10 seconds."
     sleep 10
