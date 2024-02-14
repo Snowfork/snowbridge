f4593bddf0b55a35102de69fe7428ce52af798ca
claravanstaden <Cats 4 life!>
2024-02-14 21:45:51 +0200

    create dir

--- web/packages/test/scripts/check-relayer.sh
+++ web/packages/test/scripts/check-relayer.sh
@@ -14,3 +14,25 @@ while true; do
     sleep 10
   fi
 done
+
+
+while true; do
+  # Check if start-services.sh is still running
+  pgrep -f start-services.sh > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "start-services.sh is not running. Exiting with error."
+    exit 1 # Exit with an error condition
+  fi
+
+  # Use curl to get the HTTP status code
+  status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "$url")
+
+  # Check if the status code is 200
+  if [ "$status_code" -eq 200 ]; then
+    echo "Beacon relayer has started.."
+        exit 0
+  else
+    echo "Beacon relayer has not started yet. Waiting for 10 seconds."
+    sleep 10
+  fi
+done
