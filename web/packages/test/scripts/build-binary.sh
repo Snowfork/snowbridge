4ac5c4298e8326980d61577f3bdfc8edbd347356
claravanstaden <Cats 4 life!>
2024-02-16 10:52:02 +0200

    cleanup

--- web/packages/test/scripts/build-binary.sh
+++ web/packages/test/scripts/build-binary.sh
@@ -74,7 +74,6 @@ set_slot_time() {
     else
         sed -i "s/SECONDS_PER_SLOT: .*/SECONDS_PER_SLOT: $new_value,/g" $preset_mainnet_config_file
     fi
-    cat "$preset_mainnet_config_file"
 }
 
 build_lodestar() {
