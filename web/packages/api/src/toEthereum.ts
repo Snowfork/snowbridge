d31c058adb8d6335527c01ca4ebdd3a68df28176
Alistair Singh <alistair.singh7@gmail.com>
2024-02-19 13:10:07 +0200

    fmt

--- web/packages/api/src/toEthereum.ts
+++ web/packages/api/src/toEthereum.ts
@@ -1,10 +1,14 @@
 
 export const validateSend = () => {
+    // failure
     // TODO: brigde status
     // TODO: channel status
     // TODO: bridge latency
     // TODO: token is registered
     // TODO: foreign asset exists
-    // TODO: user has asset
+    // TODO: user has asseto
+    
+    // Success
+    // TODO: Display fees
     return 42
 }
