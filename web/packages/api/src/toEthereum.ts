7465b5f8494ee8bbfc9699280bdd5e2736eaa578
Alistair Singh <alistair.singh7@gmail.com>
2024-02-19 13:07:10 +0200

    added channel id generation

--- web/packages/api/src/toEthereum.ts
+++ web/packages/api/src/toEthereum.ts
@@ -1,4 +1,10 @@
 
-export function send() {
+export const validateSend = () => {
+    // TODO: brigde status
+    // TODO: channel status
+    // TODO: bridge latency
+    // TODO: token is registered
+    // TODO: foreign asset exists
+    // TODO: user has asset
     return 42
 }
--- web/packages/api/src/utils.ts
+++ web/packages/api/src/utils.ts
@@ -1,9 +1,19 @@
+import { createType } from "@polkadot/types";
 import { bnToU8a, stringToU8a, u8aToHex } from "@polkadot/util";
+import { keccak256AsU8a } from "@polkadot/util-crypto";
 
 export const paraIdToSovereignAccount = (type: 'para' | 'sibl', paraId: number): string => {
-    let typeEncoded = stringToU8a(type);
-    let paraIdEncoded = bnToU8a(paraId);
-    let zeroPadding = new Uint8Array(32 - typeEncoded.length - paraIdEncoded.length).fill(0);
-    let address = new Uint8Array([...typeEncoded, ...paraIdEncoded, ...zeroPadding]);
+    const typeEncoded = stringToU8a(type)
+    const paraIdEncoded = bnToU8a(paraId)
+    const zeroPadding = new Uint8Array(32 - typeEncoded.length - paraIdEncoded.length).fill(0)
+    const address = new Uint8Array([...typeEncoded, ...paraIdEncoded, ...zeroPadding])
     return u8aToHex(address)
+}
+
+export const paraIdToChannelId = (paraId: number): string => {
+    const typeEncoded = stringToU8a('para')
+    const paraIdEncoded = bnToU8a(paraId, {bitLength: 32, isLe:false})
+    const joined = new Uint8Array([...typeEncoded, ...paraIdEncoded])
+    const channelId = keccak256AsU8a(joined)
+    return u8aToHex(channelId)
 }
