e01f8ec0eac5eb527edff28b24c17a55914be985
Alistair Singh <alistair.singh7@gmail.com>
2024-03-08 05:49:21 +0700

    more refactoring

--- web/packages/api/src/toPolkadot.ts
+++ web/packages/api/src/toPolkadot.ts
@@ -52,9 +52,11 @@ export type SendValidationResult = {
     }
 }
 
-export const validateSend = async (context: Context, source: ethers.Addressable, beneficiary: string, tokenAddress: string, destinationParaId: number, amount: bigint, destinationFee: bigint, options={
+export const validateSend = async (context: Context, source: ethers.Addressable, beneficiary: string, tokenAddress: string, destinationParaId: number, amount: bigint, destinationFee: bigint, options = {
     acceptableLatencyInSeconds: 10800 /* 3 Hours */
 }): Promise<SendValidationResult> => {
+    const { ethereum, ethereum: { contracts: { gateway } }, polkadot: { api: { assetHub, bridgeHub, relaychain } } } = context
+
     const sourceAddress = await source.getAddress()
 
     // Asset checks
@@ -69,20 +71,20 @@ export const validateSend = async (context: Context, source: ethers.Addressable,
     let ethereumBalance = 0n
     let canPayFee = false
     if (tokenIsRegistered) {
-        ethereumBalance = await context.ethereum.api.getBalance(sourceAddress)
-        fee = await context.ethereum.contracts.gateway.quoteSendTokenFee(tokenAddress, destinationParaId, destinationFee)
+        ethereumBalance = await ethereum.api.getBalance(sourceAddress)
+        fee = await gateway.quoteSendTokenFee(tokenAddress, destinationParaId, destinationFee)
         canPayFee = fee < ethereumBalance
     }
 
-    const [assetHubHead, assetHubParaId, bridgeHubHead, bridgeHubParaId] = await Promise.all([
-        context.polkadot.api.assetHub.rpc.chain.getFinalizedHead(),
-        context.polkadot.api.assetHub.query.parachainInfo.parachainId(),
-        context.polkadot.api.bridgeHub.rpc.chain.getFinalizedHead(),
-        context.polkadot.api.bridgeHub.query.parachainInfo.parachainId(),
+    const [assetHubHead, assetHubParaIdCodec, bridgeHubHead, bridgeHubParaIdCodec] = await Promise.all([
+        assetHub.rpc.chain.getFinalizedHead(),
+        assetHub.query.parachainInfo.parachainId(),
+        bridgeHub.rpc.chain.getFinalizedHead(),
+        bridgeHub.query.parachainInfo.parachainId(),
     ])
 
-    const assetHub = assetHubParaId.toPrimitive() as number;
-    const assetHubChannelId = paraIdToChannelId(assetHub)
+    const assetHubParaId = assetHubParaIdCodec.toPrimitive() as number;
+    const assetHubChannelId = paraIdToChannelId(assetHubParaId)
     const [channelStatus, bridgeStatus] = await Promise.all([
         channelStatusInfo(context, assetHubChannelId),
         bridgeStatusInfo(context),
@@ -93,23 +95,23 @@ export const validateSend = async (context: Context, source: ethers.Addressable,
     let beneficiaryAccountExists = true
     let destinationChainExists = true
     let hrmpChannelSetup = true
-    const existentialDeposit = BigInt(context.polkadot.api.assetHub.consts.balances.existentialDeposit.toPrimitive() as number)
-    if (destinationParaId === assetHub) {
+    const existentialDeposit = BigInt(assetHub.consts.balances.existentialDeposit.toPrimitive() as number)
+    if (destinationParaId === assetHubParaId) {
         if (destinationFee !== 0n) throw new Error('Asset Hub does not require a destination fee.')
         if (beneficiaryAddress.kind !== 1) throw new Error('Asset Hub only supports 32 byte addresses.')
-        const account = (await context.polkadot.api.assetHub.query.system.account(beneficiaryHex)).toPrimitive() as { data: { free: string } }
+        const account = (await assetHub.query.system.account(beneficiaryHex)).toPrimitive() as { data: { free: string } }
         beneficiaryAccountExists = BigInt(account.data.free) > existentialDeposit
     } else {
         const [destinationHead, hrmpChannel] = await Promise.all([
-            context.polkadot.api.relaychain.query.paras.heads(destinationParaId),
-            context.polkadot.api.relaychain.query.hrmp.hrmpChannels({ sender: assetHub, recipient: destinationParaId }),
+            relaychain.query.paras.heads(destinationParaId),
+            relaychain.query.hrmp.hrmpChannels({ sender: assetHubParaId, recipient: destinationParaId }),
         ])
         destinationChainExists = destinationHead.toPrimitive() !== null
         hrmpChannelSetup = hrmpChannel.toPrimitive() !== null
     }
 
     const lightClientLatencyIsAcceptable = bridgeStatus.toPolkadot.latencySeconds < options.acceptableLatencyInSeconds
-    const bridgeOperational = bridgeStatus.toPolkadot.operatingMode.outbound === 'Normal' && bridgeStatus.toPolkadot.operatingMode.beacon === 'Normal' 
+    const bridgeOperational = bridgeStatus.toPolkadot.operatingMode.outbound === 'Normal' && bridgeStatus.toPolkadot.operatingMode.beacon === 'Normal'
     const channelOperational = channelStatus.toPolkadot.operatingMode.outbound === 'Normal'
     const canSend = bridgeOperational && channelOperational && canPayFee
         && beneficiaryAccountExists && foreignAssetExists && lightClientLatencyIsAcceptable
@@ -130,11 +132,11 @@ export const validateSend = async (context: Context, source: ethers.Addressable,
                 estimatedDeliverySeconds: bridgeStatus.toPolkadot.latencySeconds * 2,
                 estimatedDeliveryBlocks: bridgeStatus.toPolkadot.blockLatency * 2,
                 assetHub: {
-                    paraId: assetHub,
+                    paraId: assetHubParaId,
                     validatedAtHash: u8aToHex(assetHubHead),
                 },
                 bridgeHub: {
-                    paraId: bridgeHubParaId.toPrimitive() as number,
+                    paraId: bridgeHubParaIdCodec.toPrimitive() as number,
                     validatedAtHash: u8aToHex(bridgeHubHead),
                 },
             }
@@ -206,28 +208,31 @@ export type SendResult = {
 }
 
 export const send = async (context: Context, signer: Signer, plan: SendValidationResult, confirmations = 1): Promise<SendResult> => {
-    if (plan.failure || !plan.success) {
+    const { ethereum: { contracts: { gateway } }, polkadot: { api: { assetHub, bridgeHub } } } = context
+    const { success } = plan
+
+    if (plan.failure || !success) {
         throw new Error('Plan failed')
     }
-    if (plan.success.sourceAddress !== await signer.getAddress()) {
+    if (success.sourceAddress !== await signer.getAddress()) {
         throw new Error('Invalid signer')
     }
 
     // Get current heads to make tracking easier.
     const [assetHubHead, bridgeHubHead] = await Promise.all([
-        context.polkadot.api.assetHub.rpc.chain.getFinalizedHead(),
-        context.polkadot.api.bridgeHub.rpc.chain.getFinalizedHead(),
+        assetHub.rpc.chain.getFinalizedHead(),
+        bridgeHub.rpc.chain.getFinalizedHead(),
     ])
 
     const contract = IGateway__factory.connect(context.config.appContracts.gateway, signer)
     const response = await contract.sendToken(
-        plan.success.token,
-        plan.success.destinationParaId,
-        plan.success.beneficiaryMultiAddress,
-        plan.success.destinationFee,
-        plan.success.amount,
+        success.token,
+        success.destinationParaId,
+        success.beneficiaryMultiAddress,
+        success.destinationFee,
+        success.amount,
         {
-            value: plan.success.fee
+            value: success.fee
         }
     )
     let receipt = await response.wait(confirmations)
@@ -277,15 +282,18 @@ export const send = async (context: Context, signer: Signer, plan: SendValidatio
 }
 
 export async function* trackSendProgress(context: Context, result: SendResult, beaconUpdateTimeout = 10, scanBlocks = 200): AsyncGenerator<string> {
-    if (result.failure || !result.success || !result.success.plan.success) {
+    const { polkadot: { api: { assetHub, bridgeHub } } } = context
+    const { success } = result
+
+    if (result.failure || !success || !success.plan.success) {
         throw new Error('Send failed')
     }
 
-    if (result.success.bridgeHub.beaconUpdate === undefined) {
+    if (success.bridgeHub.beaconUpdate === undefined) {
         // Wait for light client
-        const ethereumBlockNumber = result.success.ethereum.blockNumber
+        const ethereumBlockNumber = success.ethereum.blockNumber
         const lastBeaconUpdate = await lastValueFrom(
-            context.polkadot.api.bridgeHub.rx.query.ethereumBeaconClient.latestExecutionState().pipe(
+            bridgeHub.rx.query.ethereumBeaconClient.latestExecutionState().pipe(
                 rxmap(beaconUpdate => {
                     const update = beaconUpdate.toPrimitive() as { blockNumber: number }
                     return { createdAtHash: beaconUpdate.createdAtHash?.toHex(), blockNumber: update.blockNumber }
@@ -300,16 +308,16 @@ export async function* trackSendProgress(context: Context, result: SendResult, b
         if (lastBeaconUpdate === undefined) {
             throw new Error('Timeout waiting for light client to include block')
         }
-        result.success.bridgeHub.beaconUpdate = lastBeaconUpdate
+        success.bridgeHub.beaconUpdate = lastBeaconUpdate
     }
-    yield `Included by light client in Bridge Hub block ${result.success.bridgeHub.beaconUpdate?.createdAtHash}. Waiting for message delivery to Bridge Hub.`
+    yield `Included by light client in Bridge Hub block ${success.bridgeHub.beaconUpdate?.createdAtHash}. Waiting for message delivery to Bridge Hub.`
 
-    if (result.success.bridgeHub.events === undefined) {
+    if (success.bridgeHub.events === undefined) {
         // Wait for nonce
         let extrinsicNumber: number | undefined = undefined
         let extrinsicSuccess = false
         const receivedEvents = await firstValueFrom(
-            context.polkadot.api.bridgeHub.rx.query.system.events().pipe(
+            bridgeHub.rx.query.system.events().pipe(
                 take(scanBlocks),
                 tap((events) => console.log(`Waiting for Bridge Hub inbound message block ${events.createdAtHash?.toHex()}.`)),
                 filter(events => {
@@ -317,16 +325,16 @@ export async function* trackSendProgress(context: Context, result: SendResult, b
                     let messageReceivedFound = false
                     for (const event of events_iter) {
                         let eventData = (event.event.toHuman() as any).data
-                        if (context.polkadot.api.bridgeHub.events.ethereumInboundQueue.MessageReceived.is(event.event)
-                            && eventData.nonce === result.success?.nonce.toString()
-                            && eventData.messageId.toLowerCase() === result.success?.messageId.toLowerCase()
-                            && eventData.channelId.toLowerCase() === result.success?.channelId.toLowerCase()) {
+                        if (bridgeHub.events.ethereumInboundQueue.MessageReceived.is(event.event)
+                            && eventData.nonce === success?.nonce.toString()
+                            && eventData.messageId.toLowerCase() === success?.messageId.toLowerCase()
+                            && eventData.channelId.toLowerCase() === success?.channelId.toLowerCase()) {
 
                             messageReceivedFound = true
                             extrinsicNumber = event.phase.toPrimitive().applyExtrinsic
                         }
 
-                        if (context.polkadot.api.bridgeHub.events.system.ExtrinsicSuccess.is(event.event) && event.phase.toPrimitive().applyExtrinsic == extrinsicNumber) {
+                        if (bridgeHub.events.system.ExtrinsicSuccess.is(event.event) && event.phase.toPrimitive().applyExtrinsic == extrinsicNumber) {
                             extrinsicSuccess = true
                         }
                     }
@@ -339,25 +347,25 @@ export async function* trackSendProgress(context: Context, result: SendResult, b
         if (receivedEvents === undefined) {
             throw Error('Timeout while waiting for Bridge Hub delivery.')
         }
-        result.success.bridgeHub.extrinsicSuccess = extrinsicSuccess
-        result.success.bridgeHub.extrinsicNumber = extrinsicNumber
-        result.success.bridgeHub.events = receivedEvents
+        success.bridgeHub.extrinsicSuccess = extrinsicSuccess
+        success.bridgeHub.extrinsicNumber = extrinsicNumber
+        success.bridgeHub.events = receivedEvents
     }
 
-    if(result.success.bridgeHub.extrinsicSuccess) {
-        yield `Message delivered to Bridge Hub block ${result.success.bridgeHub.events?.createdAtHash?.toHex()}. Waiting for message delivery to Asset Hub.`
+    if (success.bridgeHub.extrinsicSuccess) {
+        yield `Message delivered to Bridge Hub block ${success.bridgeHub.events?.createdAtHash?.toHex()}. Waiting for message delivery to Asset Hub.`
     } else {
         throw new Error('Message processing failed on Bridge Hub.')
     }
 
-    if (result.success.assetHub.events === undefined) {
-        let issuedTo = result.success.plan.success.beneficiaryAddress
-        if (result.success.plan.success.assetHub.paraId !== result.success.plan.success.destinationParaId) {
-            issuedTo = paraIdToSovereignAccount("sibl", result.success.plan.success.destinationParaId)
+    if (success.assetHub.events === undefined) {
+        let issuedTo = success.plan?.success.beneficiaryAddress
+        if (success.plan.success.assetHub.paraId !== success.plan.success.destinationParaId) {
+            issuedTo = paraIdToSovereignAccount("sibl", success.plan.success.destinationParaId)
         }
         let extrinsicSuccess = false
         let receivedEvents = await firstValueFrom(
-            context.polkadot.api.assetHub.rx.query.system.events().pipe(
+            assetHub.rx.query.system.events().pipe(
                 take(scanBlocks),
                 tap((events) => console.log(`Waiting for Asset Hub inbound message block ${events.createdAtHash?.toHex()}.`)),
                 filter(events => {
@@ -366,19 +374,19 @@ export async function* trackSendProgress(context: Context, result: SendResult, b
                     let events_iter: any = events
                     for (const event of events_iter) {
                         let eventData = (event.event.toPrimitive() as any).data
-                        if (context.polkadot.api.assetHub.events.messageQueue.Processed.is(event.event)
-                            && eventData[0].toLowerCase() === result.success?.messageId.toLowerCase()
-                            && eventData[1]?.sibling === result.success?.plan.success?.bridgeHub.paraId) {
+                        if (assetHub.events.messageQueue.Processed.is(event.event)
+                            && eventData[0].toLowerCase() === success?.messageId.toLowerCase()
+                            && eventData[1]?.sibling === success?.plan.success?.bridgeHub.paraId) {
 
                             foundMessageQueue = true
                             extrinsicSuccess = eventData[3]
                         }
-                        if (context.polkadot.api.assetHub.events.foreignAssets.Issued.is(event.event)
-                            && eventData[2].toString() === result.success?.plan.success?.amount.toString()
+                        if (assetHub.events.foreignAssets.Issued.is(event.event)
+                            && eventData[2].toString() === success?.plan.success?.amount.toString()
                             && u8aToHex(decodeAddress(eventData[1])).toLowerCase() === issuedTo.toLowerCase()
                             && eventData[0]?.parents === 2
-                            && eventData[0]?.interior?.x2[0]?.globalConsensus?.ethereum?.chainId.toString() === result.success?.plan.success?.ethereumChainId.toString()
-                            && eventData[0]?.interior?.x2[1]?.accountKey20?.key.toLowerCase() === result.success?.plan.success?.token.toLowerCase()) {
+                            && eventData[0]?.interior?.x2[0]?.globalConsensus?.ethereum?.chainId.toString() === success?.plan.success?.ethereumChainId.toString()
+                            && eventData[0]?.interior?.x2[1]?.accountKey20?.key.toLowerCase() === success?.plan.success?.token.toLowerCase()) {
 
                             foundAssetsIssued = true
                         }
@@ -393,11 +401,11 @@ export async function* trackSendProgress(context: Context, result: SendResult, b
         if (receivedEvents === undefined) {
             throw Error('Timeout while waiting for Asset Hub delivery.')
         }
-        result.success.assetHub.events = receivedEvents
-        result.success.assetHub.extrinsicSuccess = extrinsicSuccess
+        success.assetHub.events = receivedEvents
+        success.assetHub.extrinsicSuccess = extrinsicSuccess
     }
-    if(result.success.assetHub.extrinsicSuccess) {
-        yield `Message delivered to Asset Hub block ${result.success.assetHub.events?.createdAtHash?.toHex()}. Transfer complete.`
+    if (success.assetHub.extrinsicSuccess) {
+        yield `Message delivered to Asset Hub block ${success.assetHub.events?.createdAtHash?.toHex()}. Transfer complete.`
     } else {
         throw new Error('Message processing failed on Asset Hub.')
     }
