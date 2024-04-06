16c6f36a77dca2e41edefd2795a8e9d397eff865
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2024-04-06 13:21:04 +0200

    Add runtime types for mainnet Polkadot

--- control/preimage/src/bridge_hub_runtime.rs
+++ control/preimage/src/bridge_hub_runtime.rs
@@ -1,2 +1,5 @@
 #[cfg(feature = "rococo")]
 pub use bridge_hub_rococo_runtime::*;
+
+#[cfg(feature = "polkadot")]
+pub use bridge_hub_polkadot_runtime::*;
--- control/preimage/src/relay_runtime.rs
+++ control/preimage/src/relay_runtime.rs
@@ -1,2 +1,5 @@
 #[cfg(feature = "rococo")]
 pub use rococo_runtime::*;
+
+#[cfg(feature = "polkadot")]
+pub use polkadot_runtime::*;
