af0ae561b13596c6db00f0543219519a365cd04a
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2024-04-06 13:12:19 +0200

    Add launch config for zombienet

--- control/preimage/src/constants.rs
+++ control/preimage/src/constants.rs
@@ -19,9 +19,3 @@ pub const POLKADOT_SYMBOL: &str = "KSM";
 
 #[cfg(feature = "polkadot")]
 pub const POLKADOT_SYMBOL: &str = "DOT";
-
-// Weights copied from https://github.com/paritytech/polkadot-sdk/blob/master/cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/weights/snowbridge_pallet_outbound_queue.rs
-// Added roughly 50%  buffer
-pub const PROCESS_MESSAGE_WEIGHT: (u64, u64) = (100_000_000, 5000);
-pub const COMMIT_SINGLE_MESSAGE_WEIGHT: (u64, u64) = (20_000_000, 2000);
-pub const TOKEN_TRANSFER_GAS_USED_AT_MOST: u64 = 285_000;
