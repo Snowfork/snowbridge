2eef4736945bc6cfe5fd31f933857cb203a9c8d9
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2024-04-28 18:19:49 +0200

    Fix deployment script (#1184)

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -6,12 +6,10 @@ forge script "scripts/Deploy.sol:${1}" \
     --chain-id 1 \
     --rpc-url "${MAINNET_RPC_URL}" \
     --ledger \
-    --mnemonic-derivation-path "${MNEMONIC_DERIVATION_PATH}" \
+    --mnemonic-derivation-paths "${MNEMONIC_DERIVATION_PATH}" \
     --broadcast \
     --verify \
-    --verifier etherscan \
-    --verifier-url "${ETHERSCAN_API_KEY}"
     --optimize \
     --via-ir \
     --optimizer-runs 100000 \
-    -vvv
+    -vvvv
