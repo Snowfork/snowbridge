197ad6720518a6cf32eaa394293bcf05628e6d08
ron <yrong1997@gmail.com>
2024-05-10 22:30:35 +0800

    Format status.ts

--- web/packages/api/src/status.ts
+++ web/packages/api/src/status.ts
@@ -1,58 +1,73 @@
-import { IERC20Metadata__factory, IERC20__factory } from '@snowbridge/contract-types'
-import { Context } from './index'
-import { fetchBeaconSlot } from './utils'
+import { IERC20Metadata__factory, IERC20__factory } from "@snowbridge/contract-types"
+import { Context } from "./index"
+import { fetchBeaconSlot } from "./utils"
 
-export type OperatingMode = 'Normal' | 'Halted'
+export type OperatingMode = "Normal" | "Halted"
 export type BridgeStatusInfo = {
     toEthereum: {
         operatingMode: {
-            outbound: OperatingMode,
-        },
-        latestPolkadotBlockOnEthereum: number,
-        latestPolkaotBlock: number,
-        blockLatency: number,
-        latencySeconds: number,
-    },
+            outbound: OperatingMode
+        }
+        latestPolkadotBlockOnEthereum: number
+        latestPolkaotBlock: number
+        blockLatency: number
+        latencySeconds: number
+    }
     toPolkadot: {
         operatingMode: {
-            beacon: OperatingMode,
-            inbound: OperatingMode,
-            outbound: OperatingMode,
-        },
-        latestEthereumBlockOnPolkadot: number,
-        latestEthereumBlock: number,
-        blockLatency: number,
-        latencySeconds: number,
-    },
+            beacon: OperatingMode
+            inbound: OperatingMode
+            outbound: OperatingMode
+        }
+        latestEthereumBlockOnPolkadot: number
+        latestEthereumBlock: number
+        blockLatency: number
+        latencySeconds: number
+    }
 }
 export type ChannelStatusInfo = {
     toEthereum: {
-        outbound: number,
-        inbound: number,
-    },
+        outbound: number
+        inbound: number
+    }
     toPolkadot: {
         operatingMode: {
             outbound: OperatingMode
-        },
-        outbound: number,
-        inbound: number,
-    },
+        }
+        outbound: number
+        inbound: number
+    }
 }
 
-export const bridgeStatusInfo = async (context: Context, options = {
-    polkadotBlockTimeInSeconds: 6,
-    ethereumBlockTimeInSeconds: 12,
-}): Promise<BridgeStatusInfo> => {
+export const bridgeStatusInfo = async (
+    context: Context,
+    options = {
+        polkadotBlockTimeInSeconds: 6,
+        ethereumBlockTimeInSeconds: 12,
+    }
+): Promise<BridgeStatusInfo> => {
     const latestBeefyBlock = Number(await context.ethereum.contracts.beefyClient.latestBeefyBlock())
-    const latestPolkadotBlock = (await context.polkadot.api.relaychain.query.system.number()).toPrimitive() as number
+    const latestPolkadotBlock = (
+        await context.polkadot.api.relaychain.query.system.number()
+    ).toPrimitive() as number
 
     const latestEthereumBlock = await context.ethereum.api.getBlockNumber()
-    const latestBeaconBlockRoot = (await context.polkadot.api.bridgeHub.query.ethereumBeaconClient.latestFinalizedBlockRoot()).toHex()
-    let latestBeaconSlot = await fetchBeaconSlot(context.config.ethereum.beacon_url, latestBeaconBlockRoot)
-    let latestBeaconExecutionBlock = latestBeaconSlot.data.message.body.execution_payload?.block_number
+    const latestBeaconBlockRoot = (
+        await context.polkadot.api.bridgeHub.query.ethereumBeaconClient.latestFinalizedBlockRoot()
+    ).toHex()
+    let latestBeaconSlot = await fetchBeaconSlot(
+        context.config.ethereum.beacon_url,
+        latestBeaconBlockRoot
+    )
+    let latestBeaconExecutionBlock =
+        latestBeaconSlot.data.message.body.execution_payload?.block_number
     while (latestBeaconExecutionBlock === undefined) {
-        latestBeaconSlot = await fetchBeaconSlot(context.config.ethereum.beacon_url, latestBeaconSlot.data.message.slot - 1)
-        latestBeaconExecutionBlock = latestBeaconSlot.data.message.body.execution_payload?.block_number
+        latestBeaconSlot = await fetchBeaconSlot(
+            context.config.ethereum.beacon_url,
+            latestBeaconSlot.data.message.slot - 1
+        )
+        latestBeaconExecutionBlock =
+            latestBeaconSlot.data.message.body.execution_payload?.block_number
     }
 
     const beefyBlockLatency = latestPolkadotBlock - latestBeefyBlock
@@ -62,9 +77,15 @@ export const bridgeStatusInfo = async (context: Context, options = {
     const beaconLatencySeconds = beaconBlockLatency * options.ethereumBlockTimeInSeconds
 
     const ethereumOperatingMode = await context.ethereum.contracts.gateway.operatingMode()
-    const beaconOperatingMode = (await context.polkadot.api.bridgeHub.query.ethereumBeaconClient.operatingMode()).toPrimitive()
-    const inboundOperatingMode = (await context.polkadot.api.bridgeHub.query.ethereumInboundQueue.operatingMode()).toPrimitive()
-    const outboundOperatingMode = (await context.polkadot.api.bridgeHub.query.ethereumOutboundQueue.operatingMode()).toPrimitive()
+    const beaconOperatingMode = (
+        await context.polkadot.api.bridgeHub.query.ethereumBeaconClient.operatingMode()
+    ).toPrimitive()
+    const inboundOperatingMode = (
+        await context.polkadot.api.bridgeHub.query.ethereumInboundQueue.operatingMode()
+    ).toPrimitive()
+    const outboundOperatingMode = (
+        await context.polkadot.api.bridgeHub.query.ethereumOutboundQueue.operatingMode()
+    ).toPrimitive()
 
     return {
         toEthereum: {
@@ -80,7 +101,7 @@ export const bridgeStatusInfo = async (context: Context, options = {
             operatingMode: {
                 beacon: beaconOperatingMode as OperatingMode,
                 inbound: inboundOperatingMode as OperatingMode,
-                outbound: ethereumOperatingMode === 0n ? 'Normal' : 'Halted' as OperatingMode,
+                outbound: ethereumOperatingMode === 0n ? "Normal" : ("Halted" as OperatingMode),
             },
             latestEthereumBlockOnPolkadot: Number(latestBeaconExecutionBlock),
             latestEthereumBlock: latestEthereumBlock,
@@ -90,11 +111,19 @@ export const bridgeStatusInfo = async (context: Context, options = {
     }
 }
 
-export const channelStatusInfo = async (context: Context, channelId: string): Promise<ChannelStatusInfo> => {
-    const [inbound_nonce_eth, outbound_nonce_eth] = await context.ethereum.contracts.gateway.channelNoncesOf(channelId)
+export const channelStatusInfo = async (
+    context: Context,
+    channelId: string
+): Promise<ChannelStatusInfo> => {
+    const [inbound_nonce_eth, outbound_nonce_eth] =
+        await context.ethereum.contracts.gateway.channelNoncesOf(channelId)
     const operatingMode = await context.ethereum.contracts.gateway.channelOperatingModeOf(channelId)
-    const inbound_nonce_sub = (await context.polkadot.api.bridgeHub.query.ethereumInboundQueue.nonce(channelId)).toPrimitive() as number
-    const outbound_nonce_sub = (await context.polkadot.api.bridgeHub.query.ethereumOutboundQueue.nonce(channelId)).toPrimitive() as number
+    const inbound_nonce_sub = (
+        await context.polkadot.api.bridgeHub.query.ethereumInboundQueue.nonce(channelId)
+    ).toPrimitive() as number
+    const outbound_nonce_sub = (
+        await context.polkadot.api.bridgeHub.query.ethereumOutboundQueue.nonce(channelId)
+    ).toPrimitive() as number
     return {
         toEthereum: {
             outbound: outbound_nonce_sub,
@@ -102,7 +131,7 @@ export const channelStatusInfo = async (context: Context, channelId: string): Pr
         },
         toPolkadot: {
             operatingMode: {
-                outbound: operatingMode === 0n ? 'Normal' : 'Halted' as OperatingMode
+                outbound: operatingMode === 0n ? "Normal" : ("Halted" as OperatingMode),
             },
             outbound: Number(outbound_nonce_eth),
             inbound: inbound_nonce_sub,
@@ -110,24 +139,30 @@ export const channelStatusInfo = async (context: Context, channelId: string): Pr
     }
 }
 
-export const assetStatusInfo = async (context: Context, tokenAddress: string, ownerAddress?: string) => {
+export const assetStatusInfo = async (
+    context: Context,
+    tokenAddress: string,
+    ownerAddress?: string
+) => {
     let [ethereumNetwork, gatewayAddress, isTokenRegistered] = await Promise.all([
         context.ethereum.api.getNetwork(),
         context.ethereum.contracts.gateway.getAddress(),
-        context.ethereum.contracts.gateway.isTokenRegistered(tokenAddress)
+        context.ethereum.contracts.gateway.isTokenRegistered(tokenAddress),
     ])
 
     const ethereumChainId = ethereumNetwork.chainId
-    const multiLocation = context.polkadot.api.assetHub.createType('StagingXcmV3MultiLocation', {
+    const multiLocation = context.polkadot.api.assetHub.createType("StagingXcmV3MultiLocation", {
         parents: 2,
         interior: {
             X2: [
                 { GlobalConsensus: { Ethereum: { chain_id: ethereumChainId } } },
                 { AccountKey20: { key: tokenAddress } },
-            ]
-        }
+            ],
+        },
     })
-    const foreignAsset = (await context.polkadot.api.assetHub.query.foreignAssets.asset(multiLocation)).toPrimitive() as { status: 'Live' }
+    const foreignAsset = (
+        await context.polkadot.api.assetHub.query.foreignAssets.asset(multiLocation)
+    ).toPrimitive() as { status: "Live" }
 
     const tokenContract = IERC20__factory.connect(tokenAddress, context.ethereum.api)
     let ownerBalance = BigInt(0)
@@ -158,7 +193,7 @@ export const assetStatusInfo = async (context: Context, tokenAddress: string, ow
     }
 }
 
-export const assetMetadata = async (context: Context, tokenAddress: string) => { 
+export const assetMetadata = async (context: Context, tokenAddress: string) => {
     const tokenMetadata = IERC20Metadata__factory.connect(tokenAddress, context.ethereum.api)
     const [name, symbol, decimal] = await Promise.all([
         tokenMetadata.name(),
