24a84c42c4a4f7fd2c10928e61ecc22f9ff3ff9d
Ron <yrong1997@gmail.com>
2024-05-16 08:01:06 +0800

    Format ts codes (#1199)
    * Format status.ts
    
    * More metrics for monitoring & Fix for the local setup (#1196)
    
    * More metrics & some minor fix for the local setup
    
    * Alarm to slack as a workaround
    
    * No format
    
    * Log alarm text
    
    * Fix
    
    * Improve alarm
    
    * Server to expose monitor entry point
    
    * Add cron
    
    * Improve alarm
    
    * Cloudwatch integration
    
    * Add mainnet envs for later monitoring
    
    * Add dimensions
    
    * Update Dimension
    
    * Update package.json
    
    * Initialize alarms
    
    * Change period
    
    * Initialize alarm entry point
    
    * Format code
    
    * More format
    
    * Format script

--- web/packages/api/src/query.ts
+++ web/packages/api/src/query.ts
@@ -1,19 +1,21 @@
-import { ApiPromise } from '@polkadot/api'
-import { BlockHash } from '@polkadot/types/interfaces'
-import { Codec } from '@polkadot/types/types'
-import { filter, firstValueFrom, take } from 'rxjs'
+import { ApiPromise } from "@polkadot/api"
+import { BlockHash } from "@polkadot/types/interfaces"
+import { Codec } from "@polkadot/types/types"
+import { filter, firstValueFrom, take } from "rxjs"
 
 export const scanSubstrateEvents = async (
     parachain: ApiPromise,
     start: bigint,
     scanBlocks: bigint,
-    filter: (blockNumber: bigint, blockHash: BlockHash, event: Codec) => Promise<boolean>): Promise<{
-        found: boolean
-        lastScannedBlock: bigint
-        events?: Codec
-    }> => {
-
-    const finalized = (await parachain.rpc.chain.getHeader(await parachain.rpc.chain.getFinalizedHead())).number.toBigInt()
+    filter: (blockNumber: bigint, blockHash: BlockHash, event: Codec) => Promise<boolean>
+): Promise<{
+    found: boolean
+    lastScannedBlock: bigint
+    events?: Codec
+}> => {
+    const finalized = (
+        await parachain.rpc.chain.getHeader(await parachain.rpc.chain.getFinalizedHead())
+    ).number.toBigInt()
     const stopScan = start + scanBlocks
     const end = finalized < stopScan ? finalized : stopScan
 
@@ -38,21 +40,22 @@ export const waitForMessageQueuePallet = async (
     options = {
         scanBlocks: 10,
     }
-): Promise<{ foundEvent?: Codec, allEvents: Codec, extrinsicSuccess: boolean }> => {
+): Promise<{ foundEvent?: Codec; allEvents: Codec; extrinsicSuccess: boolean }> => {
     let extrinsicSuccess = false
     let returnEvent = undefined
     let receivedEvents = await firstValueFrom(
         parachain.rx.query.system.events().pipe(
             take(options.scanBlocks),
-            filter(events => {
+            filter((events) => {
                 let foundMessageQueue = false
                 let foundEvent = false
-                for (const event of (events as any)) {
+                for (const event of events as any) {
                     let eventData = event.event.toPrimitive().data
-                    if (parachain.events.messageQueue.Processed.is(event.event)
-                        && eventData[0].toLowerCase() === messageId.toLowerCase()
-                        && eventData[1]?.sibling === siblingParachain) {
-
+                    if (
+                        parachain.events.messageQueue.Processed.is(event.event) &&
+                        eventData[0].toLowerCase() === messageId.toLowerCase() &&
+                        eventData[1]?.sibling === siblingParachain
+                    ) {
                         foundMessageQueue = true
                         extrinsicSuccess = eventData[3]
                     }
@@ -63,17 +66,17 @@ export const waitForMessageQueuePallet = async (
                     }
                 }
                 return foundMessageQueue && ((extrinsicSuccess && foundEvent) || !extrinsicSuccess)
-            }),
+            })
         ),
         { defaultValue: undefined }
     )
 
     if (receivedEvents === undefined) {
-        throw Error('Timeout while waiting for event.')
+        throw Error("Timeout while waiting for event.")
     }
     return {
         foundEvent: returnEvent,
         allEvents: receivedEvents,
-        extrinsicSuccess: extrinsicSuccess
+        extrinsicSuccess: extrinsicSuccess,
     }
 }
--- web/packages/operations/src/transfer_token.ts
+++ web/packages/operations/src/transfer_token.ts
@@ -1,26 +1,36 @@
-
-import { Keyring } from '@polkadot/keyring'
-import { contextFactory, destroyContext, environment, toEthereum, toPolkadot } from '@snowbridge/api'
-import { Wallet } from 'ethers'
+import { Keyring } from "@polkadot/keyring"
+import {
+    contextFactory,
+    destroyContext,
+    environment,
+    toEthereum,
+    toPolkadot,
+} from "@snowbridge/api"
+import { Wallet } from "ethers"
 
 const monitor = async () => {
-    let env = 'local_e2e'
+    let env = "local_e2e"
     if (process.env.NODE_ENV !== undefined) {
         env = process.env.NODE_ENV
     }
     const snwobridgeEnv = environment.SNOWBRIDGE_ENV[env]
-    if (snwobridgeEnv === undefined) { throw Error(`Unknown environment '${env}'`) }
+    if (snwobridgeEnv === undefined) {
+        throw Error(`Unknown environment '${env}'`)
+    }
 
     const { config } = snwobridgeEnv
 
     const context = await contextFactory({
-        ethereum: { execution_url: config.ETHEREUM_WS_API(process.env.REACT_APP_INFURA_KEY || ''), beacon_url: config.BEACON_HTTP_API },
+        ethereum: {
+            execution_url: config.ETHEREUM_WS_API(process.env.REACT_APP_INFURA_KEY || ""),
+            beacon_url: config.BEACON_HTTP_API,
+        },
         polkadot: {
             url: {
                 bridgeHub: config.BRIDGE_HUB_WS_URL,
                 assetHub: config.ASSET_HUB_WS_URL,
                 relaychain: config.RELAY_CHAIN_WS_URL,
-                parachains: config.PARACHAINS
+                parachains: config.PARACHAINS,
             },
         },
         appContracts: {
@@ -28,11 +38,14 @@ const monitor = async () => {
             beefy: config.BEEFY_CONTRACT,
         },
     })
-    const polkadot_keyring = new Keyring({ type: 'sr25519' })
+    const polkadot_keyring = new Keyring({ type: "sr25519" })
 
-    const ETHEREUM_ACCOUNT = new Wallet('0x5e002a1af63fd31f1c25258f3082dc889762664cb8f218d86da85dff8b07b342', context.ethereum.api)
+    const ETHEREUM_ACCOUNT = new Wallet(
+        "0x5e002a1af63fd31f1c25258f3082dc889762664cb8f218d86da85dff8b07b342",
+        context.ethereum.api
+    )
     const ETHEREUM_ACCOUNT_PUBLIC = await ETHEREUM_ACCOUNT.getAddress()
-    const POLKADOT_ACCOUNT = polkadot_keyring.addFromUri('//Ferdie')
+    const POLKADOT_ACCOUNT = polkadot_keyring.addFromUri("//Ferdie")
     const POLKADOT_ACCOUNT_PUBLIC = POLKADOT_ACCOUNT.address
 
     const amount = 10n
@@ -40,60 +53,98 @@ const monitor = async () => {
     const POLL_INTERVAL_MS = 10_000
     const WETH_CONTRACT = snwobridgeEnv.locations[0].erc20tokensReceivable["WETH"]
 
-    console.log('# Ethereum to Asset Hub')
+    console.log("# Ethereum to Asset Hub")
     {
-        const plan = await toPolkadot.validateSend(context, ETHEREUM_ACCOUNT, POLKADOT_ACCOUNT_PUBLIC, WETH_CONTRACT, 1000, amount, BigInt(0))
-        console.log('Plan:', plan)
+        const plan = await toPolkadot.validateSend(
+            context,
+            ETHEREUM_ACCOUNT,
+            POLKADOT_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            1000,
+            amount,
+            BigInt(0)
+        )
+        console.log("Plan:", plan)
         let result = await toPolkadot.send(context, ETHEREUM_ACCOUNT, plan)
-        console.log('Execute:', result)
+        console.log("Execute:", result)
         while (true) {
-            const { status } = (await toPolkadot.trackSendProgressPolling(context, result))
-            if (status !== "pending") { break }
-            await new Promise(r => setTimeout(r, POLL_INTERVAL_MS));
+            const { status } = await toPolkadot.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
+            }
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
         }
-        console.log('Complete:', result)
+        console.log("Complete:", result)
     }
 
-    console.log('# Asset Hub to Ethereum')
+    console.log("# Asset Hub to Ethereum")
     {
-        const plan = await toEthereum.validateSend(context, POLKADOT_ACCOUNT, 1000, ETHEREUM_ACCOUNT_PUBLIC, WETH_CONTRACT, amount)
-        console.log('Plan:', plan)
+        const plan = await toEthereum.validateSend(
+            context,
+            POLKADOT_ACCOUNT,
+            1000,
+            ETHEREUM_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            amount
+        )
+        console.log("Plan:", plan)
         const result = await toEthereum.send(context, POLKADOT_ACCOUNT, plan)
-        console.log('Execute:', result)
+        console.log("Execute:", result)
         while (true) {
-            const { status } = (await toEthereum.trackSendProgressPolling(context, result))
-            if (status !== "pending") { break }
-            await new Promise(r => setTimeout(r, POLL_INTERVAL_MS));
+            const { status } = await toEthereum.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
+            }
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
         }
-        console.log('Complete:', result)
+        console.log("Complete:", result)
     }
 
-    console.log('# Ethereum to Penpal')
+    console.log("# Ethereum to Penpal")
     {
-        const plan = await toPolkadot.validateSend(context, ETHEREUM_ACCOUNT, POLKADOT_ACCOUNT_PUBLIC, WETH_CONTRACT, 2000, amount, BigInt(4_000_000_000))
-        console.log('Plan:', plan)
+        const plan = await toPolkadot.validateSend(
+            context,
+            ETHEREUM_ACCOUNT,
+            POLKADOT_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            2000,
+            amount,
+            BigInt(4_000_000_000)
+        )
+        console.log("Plan:", plan)
         let result = await toPolkadot.send(context, ETHEREUM_ACCOUNT, plan)
-        console.log('Execute:', result)
+        console.log("Execute:", result)
         while (true) {
-            const { status } = (await toPolkadot.trackSendProgressPolling(context, result))
-            if (status !== "pending") { break }
-            await new Promise(r => setTimeout(r, POLL_INTERVAL_MS));
+            const { status } = await toPolkadot.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
+            }
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
         }
-        console.log('Complete:', result)
+        console.log("Complete:", result)
     }
 
-    console.log('# Penpal to Ethereum')
+    console.log("# Penpal to Ethereum")
     {
-        const plan = await toEthereum.validateSend(context, POLKADOT_ACCOUNT, 2000, ETHEREUM_ACCOUNT_PUBLIC, WETH_CONTRACT, amount)
-        console.log('Plan:', plan)
+        const plan = await toEthereum.validateSend(
+            context,
+            POLKADOT_ACCOUNT,
+            2000,
+            ETHEREUM_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            amount
+        )
+        console.log("Plan:", plan)
         const result = await toEthereum.send(context, POLKADOT_ACCOUNT, plan)
-        console.log('Execute:', result)
+        console.log("Execute:", result)
         while (true) {
-            const { status } = (await toEthereum.trackSendProgressPolling(context, result))
-            if (status !== "pending") { break }
-            await new Promise(r => setTimeout(r, POLL_INTERVAL_MS));
+            const { status } = await toEthereum.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
+            }
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
         }
-        console.log('Complete:', result)
+        console.log("Complete:", result)
     }
 
     await destroyContext(context)
@@ -102,6 +153,6 @@ const monitor = async () => {
 monitor()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error('Error:', error)
+        console.error("Error:", error)
         process.exit(1)
     })
