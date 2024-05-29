20c271c6c7367858ae37ecf1779cfafb3e18e76f
Alistair Singh <alistair.singh7@gmail.com>
2024-05-29 04:03:30 +0200

    Snowbridge/API version 0.1.6 (#1211)
    * improve start up performance
    
    * history to ethereum working
    
    * fmt
    
    * toPolkadot tracking working
    
    * fmt
    
    * run all
    
    * remove cache
    
    * final fixes
    
    * added max consumers check
    
    * more fixes
    
    * remove logging
    
    * check max consumers
    
    * update version
    
    * remove apis not compatible with next
    
    * decode transaction detail
    
    * fmt
    
    * tracking id
    
    * added when field
    
    * fix beneficiary address
    
    * source addresses
    
    * one fetch at a time
    
    * add asset type
    
    * fmt

---
+++ web/packages/api/src/subscan.ts
@@ -0,0 +1,204 @@
+export type SubscanResult = {
+    status: number
+    statusText: string
+    json: any
+    rateLimit: SubscanRateLimit
+}
+
+export type SubscanRateLimit = {
+    limit: number | null
+    reset: number | null
+    remaining: number | null
+    retryAfter: number | null
+}
+
+export type SubscanApiPost = (subUrl: string, body: any) => Promise<SubscanResult>
+export interface SubscanApi {
+    post: SubscanApiPost
+}
+
+const sleepMs = async (ms: number) => {
+    await new Promise<void>((resolve) => {
+        const id = setTimeout(() => {
+            resolve()
+            clearTimeout(id)
+        }, ms)
+    })
+}
+
+export const createApi = (baseUrl: string, apiKey: string, options = { limit: 1 }): SubscanApi => {
+    let url = baseUrl.trim()
+    if (!url.endsWith("/")) {
+        url += "/"
+    }
+
+    const headers = new Headers()
+    headers.append("Content-Type", "application/json")
+    headers.append("x-api-key", apiKey)
+
+    let rateLimit: SubscanRateLimit = {
+        limit: options.limit,
+        reset: 0,
+        remaining: options.limit,
+        retryAfter: 0,
+    }
+    const post: SubscanApiPost = async (subUrl: string, body: any) => {
+        const request: RequestInit = {
+            method: "POST",
+            headers,
+            body: JSON.stringify(body),
+            redirect: "follow",
+        }
+
+        if (rateLimit.retryAfter !== null && rateLimit.retryAfter > 0) {
+            console.log("Being rate limited", rateLimit)
+            await sleepMs(rateLimit.retryAfter * 1000)
+        }
+        if (rateLimit.remaining === 0 && rateLimit.reset !== null && rateLimit.reset > 0) {
+            console.log("Being rate limited", rateLimit)
+            await sleepMs(rateLimit.reset * 1000)
+        }
+
+        const response = await fetch(`${url}${subUrl}`, request)
+
+        rateLimit.limit = Number(response.headers.get("ratelimit-limit"))
+        rateLimit.reset = Number(response.headers.get("ratelimit-reset"))
+        rateLimit.remaining = Number(response.headers.get("ratelimit-remaining"))
+        rateLimit.retryAfter = Number(response.headers.get("retry-after"))
+
+        if (response.status !== 200) {
+            throw new Error(
+                `Failed to fetch from Subscan: ${response.status} ${response.statusText}`
+            )
+        }
+
+        const json = await response.json()
+        return {
+            status: response.status,
+            statusText: response.statusText,
+            json,
+            rateLimit: { ...rateLimit },
+        }
+    }
+
+    return {
+        post,
+    }
+}
+
+export const fetchEvents = async <T>(
+    api: SubscanApi,
+    module: string,
+    eventIds: string[],
+    fromBlock: number,
+    toBlock: number,
+    page: number,
+    rows: number,
+    filterMap: (events: any, params: any) => Promise<T | null>
+) => {
+    const eventsBody = {
+        module,
+        block_range: `${fromBlock}-${toBlock}`,
+        event_id: eventIds.length === 1 ? eventIds[0] : undefined,
+        row: rows,
+        page,
+    }
+
+    const eventResponse = await api.post("api/v2/scan/events", eventsBody)
+
+    let endOfPages = false
+    if (eventResponse.json.data.events === null) {
+        eventResponse.json.data.events = []
+        endOfPages = true
+    }
+
+    const map = new Map<string, any>()
+    eventResponse.json.data.events
+        .filter((e: any) => eventIds.includes(e.event_id))
+        .forEach((e: any) => {
+            map.set(e.event_index, e)
+        })
+
+    const events = []
+
+    if (map.size > 0) {
+        const paramsBody = { event_index: Array.from(map.keys()) }
+        const paramsResponse = await api.post("api/scan/event/params", paramsBody)
+
+        if (paramsResponse.json.data === null) {
+            paramsResponse.json.data = []
+        }
+
+        for (const { event_index, params } of paramsResponse.json.data) {
+            const event = map.get(event_index)
+            const transform = await filterMap(event, params)
+            if (transform === null) {
+                continue
+            }
+            events.push({ ...event, params, data: transform })
+        }
+    }
+    return {
+        status: eventResponse.status,
+        statusText: eventResponse.statusText,
+        events,
+        endOfPages,
+    }
+}
+
+export const fetchExtrinsics = async <T>(
+    api: SubscanApi,
+    module: string,
+    call: string,
+    fromBlock: number,
+    toBlock: number,
+    page: number,
+    rows: number,
+    filterMap: (extrinsic: any, params: any) => Promise<T | null>
+) => {
+    const extBody = {
+        module,
+        call,
+        block_range: `${fromBlock}-${toBlock}`,
+        row: rows,
+        page,
+    }
+    const extResponse = await api.post("api/v2/scan/extrinsics", extBody)
+
+    let endOfPages = false
+    if (extResponse.json.data.extrinsics === null) {
+        extResponse.json.data.extrinsics = []
+        endOfPages = true
+    }
+    const map = new Map<string, any>()
+    extResponse.json.data.extrinsics.forEach((e: any) => {
+        map.set(e.extrinsic_index, e)
+    })
+
+    const extrinsics = []
+
+    if (map.size > 0) {
+        const paramsBody = { extrinsic_index: Array.from(map.keys()) }
+        const extParams = await api.post("api/scan/extrinsic/params", paramsBody)
+
+        if (extParams.json.data === null) {
+            extParams.json.data = []
+        }
+
+        for (const { extrinsic_index, params } of extParams.json.data) {
+            const event = map.get(extrinsic_index)
+            const transform = await filterMap(event, params)
+            if (transform === null) {
+                continue
+            }
+
+            extrinsics.push({ ...event, params, data: transform })
+        }
+    }
+    return {
+        status: extResponse.status,
+        statusText: extResponse.statusText,
+        extrinsics,
+        endOfPages,
+    }
+}
