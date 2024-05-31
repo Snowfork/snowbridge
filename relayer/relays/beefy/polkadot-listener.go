80b4dbd0629c59ddd005c37ccb56518c27be21fa
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2024-05-31 16:06:34 +0200

    unused code

--- relayer/relays/beefy/polkadot-listener.go
+++ relayer/relays/beefy/polkadot-listener.go
@@ -9,7 +9,6 @@ import (
 	"golang.org/x/sync/errgroup"
 
 	"github.com/snowfork/snowbridge/relayer/chain/relaychain"
-	"github.com/snowfork/snowbridge/relayer/substrate"
 )
 
 type PolkadotListener struct {
@@ -108,45 +107,4 @@ func (li *PolkadotListener) scanCommitments(
 			}
 		}
 	}
-}
-
-func (li *PolkadotListener) queryBeefyAuthorities(blockHash types.Hash) ([]substrate.Authority, error) {
-	var authorities []substrate.Authority
-	ok, err := li.conn.API().RPC.State.GetStorage(li.beefyAuthoritiesKey, &authorities, blockHash)
-	if err != nil {
-		return nil, err
-	}
-	if !ok {
-		return nil, fmt.Errorf("beefy authorities not found")
-	}
-
-	return authorities, nil
-}
-
-func (li *PolkadotListener) queryBeefyAuthoritySet(blockHash types.Hash) (BeefyAuthoritySet, error) {
-	var authoritySet BeefyAuthoritySet
-	storageKey, err := types.CreateStorageKey(li.conn.Metadata(), "MmrLeaf", "BeefyAuthorities", nil, nil)
-	ok, err := li.conn.API().RPC.State.GetStorage(storageKey, &authoritySet, blockHash)
-	if err != nil {
-		return authoritySet, err
-	}
-	if !ok {
-		return authoritySet, fmt.Errorf("beefy authoritySet not found")
-	}
-
-	return authoritySet, nil
-}
-
-func (li *PolkadotListener) queryBeefyNextAuthoritySet(blockHash types.Hash) (BeefyAuthoritySet, error) {
-	var nextAuthoritySet BeefyAuthoritySet
-	storageKey, err := types.CreateStorageKey(li.conn.Metadata(), "MmrLeaf", "BeefyNextAuthorities", nil, nil)
-	ok, err := li.conn.API().RPC.State.GetStorage(storageKey, &nextAuthoritySet, blockHash)
-	if err != nil {
-		return nextAuthoritySet, err
-	}
-	if !ok {
-		return nextAuthoritySet, fmt.Errorf("beefy nextAuthoritySet not found")
-	}
-
-	return nextAuthoritySet, nil
 }
