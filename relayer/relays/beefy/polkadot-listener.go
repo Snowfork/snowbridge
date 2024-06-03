715ac5a1c8030dac8a6ce4d3a899d48083d4eaa3
ron <yrong1997@gmail.com>
2024-06-03 13:53:51 +0800

    Find for next beefy block

--- relayer/relays/beefy/polkadot-listener.go
+++ relayer/relays/beefy/polkadot-listener.go
@@ -151,65 +151,66 @@ func (li *PolkadotListener) generateBeefyUpdate(ctx context.Context, relayBlockN
 	return request, nil
 }
 
-func (li *PolkadotListener) findLatestBeefyBlock(relayBlockNumber uint64) (types.Hash, error) {
+func (li *PolkadotListener) findNextBeefyBlock(blockNumber uint64) (types.Hash, error) {
 	api := li.conn.API()
-	var latestBeefyBlockNumber uint64
-	var latestBeefyBlockHash types.Hash
+	var nextBeefyBlockHash, finalizedBeefyBlockHash types.Hash
+	var err error
+	nextBeefyBlockNumber := blockNumber
 	for {
-		finalizedBeefyBlockHash, err := api.RPC.Beefy.GetFinalizedHead()
+		finalizedBeefyBlockHash, err = api.RPC.Beefy.GetFinalizedHead()
 		if err != nil {
-			return latestBeefyBlockHash, fmt.Errorf("fetch beefy finalized head: %w", err)
+			return nextBeefyBlockHash, fmt.Errorf("fetch beefy finalized head: %w", err)
 		}
-
 		finalizedBeefyBlockHeader, err := api.RPC.Chain.GetHeader(finalizedBeefyBlockHash)
 		if err != nil {
-			return latestBeefyBlockHash, fmt.Errorf("fetch block header: %w", err)
+			return nextBeefyBlockHash, fmt.Errorf("fetch block header: %w", err)
 		}
-
-		latestBeefyBlockNumber = uint64(finalizedBeefyBlockHeader.Number)
-		if latestBeefyBlockNumber < relayBlockNumber {
+		latestBeefyBlockNumber := uint64(finalizedBeefyBlockHeader.Number)
+		if latestBeefyBlockNumber <= nextBeefyBlockNumber {
+			// The relay block not finalized yet, just wait and retry
 			time.Sleep(6 * time.Second)
 			continue
-		}
-		latestBeefyBlockHash = finalizedBeefyBlockHash
-		break
-	}
-	return latestBeefyBlockHash, nil
-}
-
-func (li *PolkadotListener) findNextBeefyBlock(relayBlockNumber uint64) (types.Hash, error) {
-	api := li.conn.API()
-	nextBeefyBlockNumber := relayBlockNumber
-	var nextBeefyBlockHash types.Hash
-	var err error
-	for {
-		nextBeefyBlockHash, err = api.RPC.Chain.GetBlockHash(nextBeefyBlockNumber)
-		if err != nil {
-			return nextBeefyBlockHash, fmt.Errorf("fetch block hash: %w", err)
-		}
-		block, err := api.RPC.Chain.GetBlock(nextBeefyBlockHash)
-		if err != nil {
-			return nextBeefyBlockHash, fmt.Errorf("fetch block: %w", err)
-		}
-
-		var commitment *types.SignedCommitment
-		for j := range block.Justifications {
-			sc := types.OptionalSignedCommitment{}
-			if block.Justifications[j].EngineID() == "BEEF" {
-				err := types.DecodeFromBytes(block.Justifications[j].Payload(), &sc)
+		} else if latestBeefyBlockNumber <= nextBeefyBlockNumber+600 {
+			// The relay block has been finalized not long ago(1 hour), just return the finalized block
+			nextBeefyBlockHash = finalizedBeefyBlockHash
+			break
+		} else {
+			// The relay block has been finalized for a long time, in this case return the next block
+			// which contains a beefy justification
+			for {
+				if nextBeefyBlockNumber == latestBeefyBlockNumber {
+					nextBeefyBlockHash = finalizedBeefyBlockHash
+					break
+				}
+				nextBeefyBlockHash, err = api.RPC.Chain.GetBlockHash(nextBeefyBlockNumber)
 				if err != nil {
-					return nextBeefyBlockHash, fmt.Errorf("decode BEEFY signed commitment: %w", err)
+					return nextBeefyBlockHash, fmt.Errorf("fetch block hash: %w", err)
 				}
-				ok, value := sc.Unwrap()
-				if ok {
-					commitment = &value
+				block, err := api.RPC.Chain.GetBlock(nextBeefyBlockHash)
+				if err != nil {
+					return nextBeefyBlockHash, fmt.Errorf("fetch block: %w", err)
 				}
+
+				var commitment *types.SignedCommitment
+				for j := range block.Justifications {
+					sc := types.OptionalSignedCommitment{}
+					if block.Justifications[j].EngineID() == "BEEF" {
+						err := types.DecodeFromBytes(block.Justifications[j].Payload(), &sc)
+						if err != nil {
+							return nextBeefyBlockHash, fmt.Errorf("decode BEEFY signed commitment: %w", err)
+						}
+						ok, value := sc.Unwrap()
+						if ok {
+							commitment = &value
+						}
+					}
+				}
+				if commitment != nil {
+					return nextBeefyBlockHash, nil
+				}
+				nextBeefyBlockNumber++
 			}
 		}
-		if commitment != nil {
-			return nextBeefyBlockHash, nil
-		}
-		nextBeefyBlockNumber++
 	}
-
+	return nextBeefyBlockHash, nil
 }
