2b302029f9aac245a2f9d2797a67351bc78d0083
ron <yrong1997@gmail.com>
2024-06-05 08:59:29 +0800

    Revert changes

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -6,7 +6,7 @@ forge script "scripts/DeployBeefyClient.sol:DeployBeefyClient" \
     --chain-id 1 \
     --rpc-url "${MAINNET_RPC_URL}" \
     --ledger \
-    --mnemonic-derivation-paths "m/44'/60'/1'/0/0" \
+    --mnemonic-derivation-paths "${MNEMONIC_DERIVATION_PATH}" \
     --broadcast \
     --verify \
     --optimize \
