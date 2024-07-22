bd3d74983fd795b5e93dcb0ee44ccd9488a1a128
ron <yrong1997@gmail.com>
2024-07-22 08:54:38 +0800

    Fix latency config

--- web/packages/api/src/status.ts
+++ web/packages/api/src/status.ts
@@ -91,8 +91,8 @@ export const bridgeStatusInfo = async (
     options = {
         polkadotBlockTimeInSeconds: 6,
         ethereumBlockTimeInSeconds: 12,
-        toPolkadotCheckIntervalInBlock: BlockLatencyThreshold.ToEthereum,
-        toEthereumCheckIntervalInBlock: BlockLatencyThreshold.ToPolkadot,
+        toPolkadotCheckIntervalInBlock: BlockLatencyThreshold.ToPolkadot,
+        toEthereumCheckIntervalInBlock: BlockLatencyThreshold.ToEthereum,
     }
 ): Promise<BridgeStatusInfo> => {
     // Beefy status
