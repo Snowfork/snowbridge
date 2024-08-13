9554d8e84bb3b8be0d88c6c9120aed857de33f6f
ron <yrong1997@gmail.com>
2024-08-13 18:38:40 +0800

    Set TreatMissingData explict to BREACHING

--- web/packages/operations/src/alarm.ts
+++ web/packages/operations/src/alarm.ts
@@ -227,6 +227,7 @@ export const initializeAlarms = async () => {
     let alarmCommandSharedInput = {
         Namespace: CLOUD_WATCH_NAME_SPACE + "-" + name,
         Threshold: 0,
+        TreatMissingData: "BREACHING",
     }
 
     // Alarm for stale bridge
