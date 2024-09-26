583bc9094f2c9bd640420f632d05dd1f2f84b70b
ron <yrong1997@gmail.com>
2024-09-26 06:50:25 +0000

    Revert change

--- web/packages/operations/src/transfer_token.ts
+++ web/packages/operations/src/transfer_token.ts
@@ -1,4 +1,3 @@
-import "dotenv/config"
 import { Keyring } from "@polkadot/keyring"
 import {
     contextFactory,
@@ -9,23 +8,22 @@ import {
 } from "@snowbridge/api"
 import { WETH9__factory } from "@snowbridge/contract-types"
 import { Wallet } from "ethers"
-import cron from "node-cron"
 
 const monitor = async () => {
     let env = "local_e2e"
     if (process.env.NODE_ENV !== undefined) {
         env = process.env.NODE_ENV
     }
-    const snowbridgeEnv = environment.SNOWBRIDGE_ENV[env]
-    if (snowbridgeEnv === undefined) {
+    const snwobridgeEnv = environment.SNOWBRIDGE_ENV[env]
+    if (snwobridgeEnv === undefined) {
         throw Error(`Unknown environment '${env}'`)
     }
 
-    const { config } = snowbridgeEnv
+    const { config } = snwobridgeEnv
 
     const context = await contextFactory({
         ethereum: {
-            execution_url: process.env["EXECUTION_NODE_URL"] || config.ETHEREUM_API(process.env.REACT_APP_INFURA_KEY || ""),
+            execution_url: config.ETHEREUM_API(process.env.REACT_APP_INFURA_KEY || ""),
             beacon_url: config.BEACON_HTTP_API,
         },
         polkadot: {
@@ -44,17 +42,17 @@ const monitor = async () => {
     const polkadot_keyring = new Keyring({ type: "sr25519" })
 
     const ETHEREUM_ACCOUNT = new Wallet(
-        process.env["ETHEREUM_KEY"] || "0x5e002a1af63fd31f1c25258f3082dc889762664cb8f218d86da85dff8b07b342",
+        "0x5e002a1af63fd31f1c25258f3082dc889762664cb8f218d86da85dff8b07b342",
         context.ethereum.api
     )
     const ETHEREUM_ACCOUNT_PUBLIC = await ETHEREUM_ACCOUNT.getAddress()
-    const POLKADOT_ACCOUNT = process.env["SUBSTRATE_KEY"]?polkadot_keyring.addFromUri(process.env["SUBSTRATE_KEY"]):polkadot_keyring.addFromUri("//Ferdie")
+    const POLKADOT_ACCOUNT = polkadot_keyring.addFromUri("//Ferdie")
     const POLKADOT_ACCOUNT_PUBLIC = POLKADOT_ACCOUNT.address
 
-    const amount = 2_000_000_000_000n
+    const amount = 10n
 
     const POLL_INTERVAL_MS = 10_000
-    const WETH_CONTRACT = snowbridgeEnv.locations[0].erc20tokensReceivable.find(
+    const WETH_CONTRACT = snwobridgeEnv.locations[0].erc20tokensReceivable.find(
         (t) => t.id === "WETH"
     )!.address
 
@@ -116,71 +114,60 @@ const monitor = async () => {
         }
         console.log("Complete:", result)
     }
-    // Disable penpal transfers
-    if (process.env["PENPAL_TRANSFER"] == 'true') {
-        console.log("# Ethereum to Penpal")
-        {
-            const plan = await toPolkadot.validateSend(
-                context,
-                ETHEREUM_ACCOUNT,
-                POLKADOT_ACCOUNT_PUBLIC,
-                WETH_CONTRACT,
-                2000,
-                amount,
-                BigInt(4_000_000_000)
-            )
-            console.log("Plan:", plan, plan.failure?.errors)
-            let result = await toPolkadot.send(context, ETHEREUM_ACCOUNT, plan)
-            console.log("Execute:", result)
-            while (true) {
-                const { status } = await toPolkadot.trackSendProgressPolling(context, result)
-                if (status !== "pending") {
-                    break
-                }
-                await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
-            }
-            console.log("Complete:", result)
-        }
 
-        console.log("# Penpal to Ethereum")
-        {
-            const plan = await toEthereum.validateSend(
-                context,
-                POLKADOT_ACCOUNT,
-                2000,
-                ETHEREUM_ACCOUNT_PUBLIC,
-                WETH_CONTRACT,
-                amount
-            )
-            console.log("Plan:", plan, plan.failure?.errors)
-            const result = await toEthereum.send(context, POLKADOT_ACCOUNT, plan)
-            console.log("Execute:", result)
-            while (true) {
-                const { status } = await toEthereum.trackSendProgressPolling(context, result)
-                if (status !== "pending") {
-                    break
-                }
-                await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
+    console.log("# Ethereum to Penpal")
+    {
+        const plan = await toPolkadot.validateSend(
+            context,
+            ETHEREUM_ACCOUNT,
+            POLKADOT_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            2000,
+            amount,
+            BigInt(4_000_000_000)
+        )
+        console.log("Plan:", plan, plan.failure?.errors)
+        let result = await toPolkadot.send(context, ETHEREUM_ACCOUNT, plan)
+        console.log("Execute:", result)
+        while (true) {
+            const { status } = await toPolkadot.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
             }
-            console.log("Complete:", result)
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
         }
+        console.log("Complete:", result)
     }
+
+    console.log("# Penpal to Ethereum")
+    {
+        const plan = await toEthereum.validateSend(
+            context,
+            POLKADOT_ACCOUNT,
+            2000,
+            ETHEREUM_ACCOUNT_PUBLIC,
+            WETH_CONTRACT,
+            amount
+        )
+        console.log("Plan:", plan, plan.failure?.errors)
+        const result = await toEthereum.send(context, POLKADOT_ACCOUNT, plan)
+        console.log("Execute:", result)
+        while (true) {
+            const { status } = await toEthereum.trackSendProgressPolling(context, result)
+            if (status !== "pending") {
+                break
+            }
+            await new Promise((r) => setTimeout(r, POLL_INTERVAL_MS))
+        }
+        console.log("Complete:", result)
+    }
+
     await destroyContext(context)
 }
 
-if (process.argv.length != 3) {
-    console.error("Expected one argument with Enum from `start|cron|init`")
-    process.exit(1)
-}
-
-if (process.argv[2] == "start") {
-    monitor()
-        .then(() => process.exit(0))
-        .catch((error) => {
-            console.error("Error:", error)
-            process.exit(1)
-        })
-} else if (process.argv[2] == "cron") {
-    console.log("running cronjob")
-    cron.schedule(process.env["CRON_EXPRESSION"] || "0 0 * * *", monitor)
-}
+monitor()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error("Error:", error)
+        process.exit(1)
+    })
