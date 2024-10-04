cdf2b0a7b1c9fadf2447ece5f3435b6aceb6fe1e
Clara van Staden <claravanstaden64@gmail.com>
2024-10-04 10:13:50 +0200

    Adds Paseo to control tool (#1295)
    * add testnet deploy script
    
    * fix script
    
    * fix BH agent
    
    * fixes
    
    * remove pricing parameters
    
    * adds paseo as env
    
    * update metadata
    
    * fixes
    
    * adds sudo and updates paseo runtime code
    
    * edits readme, fix chain id
    
    * fixes
    
    * fixes
    
    * conditionals
    
    * cleanup and update readme

--- control/preimage/src/relay_runtime.rs
+++ control/preimage/src/relay_runtime.rs
@@ -12,3 +12,8 @@ pub use polkadot_runtime::*;
 pub use westend_runtime::runtime_types::westend_runtime::RuntimeCall;
 #[cfg(feature = "westend")]
 pub use westend_runtime::*;
+
+#[cfg(feature = "paseo")]
+pub use paseo_runtime::runtime_types::paseo_runtime::RuntimeCall;
+#[cfg(feature = "paseo")]
+pub use paseo_runtime::*;
