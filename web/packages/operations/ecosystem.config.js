f6946cfcd82db32d8dfa892bfddf8d6d47aa6822
ron <yrong1997@gmail.com>
2024-10-12 06:26:10 +0000

    Seperate westend services

--- web/packages/operations/ecosystem.config.js
+++ web/packages/operations/ecosystem.config.js
@@ -7,13 +7,19 @@ module.exports = {
       args: "cron"
     },
     {
-      name: "transferToPolkadot",
+      name: "westend-monitor",
+      node_args: "--require=dotenv/config",
+      script: "./dist/src/main.js",
+      args: "cron"
+    },
+    {
+      name: "westend-transferToPolkadot",
       node_args: "--require=dotenv/config",
       script: "./dist/src/transfer_to_polkadot.js",
       args: "cron"
     },
     {
-      name: "transferToEthereum",
+      name: "westend-transferToEthereum",
       node_args: "--require=dotenv/config",
       script: "./dist/src/transfer_to_ethereum.js",
       args: "cron"
