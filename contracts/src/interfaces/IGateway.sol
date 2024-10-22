e0b4e701753dde034bc449f018b4a55a1e03da92
Alistair Singh <alistair.singh7@gmail.com>
2024-10-22 14:39:20 +0200

    remove unused Token.sol

--- contracts/src/interfaces/IGateway.sol
+++ contracts/src/interfaces/IGateway.sol
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
 pragma solidity 0.8.25;
 
-import {OperatingMode, InboundMessage, ParaID, ChannelID, MultiAddress, TokenInfo} from "../Types.sol";
+import {OperatingMode, InboundMessage, ParaID, ChannelID, MultiAddress} from "../Types.sol";
 import {Verification} from "../Verification.sol";
 import {UD60x18} from "prb/math/src/UD60x18.sol";
 
