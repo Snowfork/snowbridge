f47a25820d30da6b9d31b1dd57767d31674720c2
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2024-10-29 23:13:37 +0200

    view function

--- contracts/src/upgrades/Gateway202410.sol
+++ contracts/src/upgrades/Gateway202410.sol
@@ -25,7 +25,7 @@ contract Gateway202410 is Gateway {
     {}
 
     // Override parent initializer to prevent re-initialization of storage.
-    function initialize(bytes memory) external override {
+    function initialize(bytes memory) external view override {
         // Ensure that arbitrary users cannot initialize storage in this logic contract.
         if (ERC1967.load() == address(0)) {
             revert Unauthorized();
