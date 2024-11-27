49c7199dacd376f3f280766f681ab173d2b6790b
NindoK <b.daniele@protonmail.ch>
2024-11-27 18:21:58 +0100

    fix history

--- contracts/.history/src/Validators_20241127120439.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
-pragma solidity 0.8.25;
-
-import {BeefyClient} from "./BeefyClient.sol";
-import {ScaleCodec} from "./utils/ScaleCodec.sol";
-import {SubstrateTypes} from "./SubstrateTypes.sol";
-
-library Validators {}
