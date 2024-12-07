49aac41873fa5c2a1956e90f2221cb4a53c40ce9
Alistair Singh <alistair.singh7@gmail.com>
2024-12-07 05:02:17 +0200

    fix tests and add all tests to run-tests

--- smoketest/run-tests.sh
+++ smoketest/run-tests.sh
@@ -5,19 +5,23 @@ set -xe
 cargo test --no-run
 
 tests=(
+  # ERC20 Tests
   register_token
-
   send_token
   send_token_to_penpal
   transfer_token
 
+  # PNA Tests
+  register_polkadot_token 
+  transfer_polkadot_token
+  send_polkadot_token
+
+  # System Pallet Tests
   set_pricing_params
   set_token_transfer_fees
-
   create_agent
   create_channel
   transfer_native_from_agent
-
   upgrade_gateway
 )
 
