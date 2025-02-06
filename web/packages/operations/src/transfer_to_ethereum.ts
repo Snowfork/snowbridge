53fb98bdd40e24371aa27c4babf706dad2688e15
ron <yrong1997@gmail.com>
2025-02-06 13:47:01 +0000

    Remove private key in the code

--- web/packages/operations/src/transfer_to_ethereum.ts
+++ web/packages/operations/src/transfer_to_ethereum.ts
@@ -44,8 +44,7 @@ const transfer = async () => {
     const polkadot_keyring = new Keyring({ type: "sr25519" })
 
     const ETHEREUM_ACCOUNT = new Wallet(
-        process.env["ETHEREUM_KEY"] ||
-            "0x5e002a1af63fd31f1c25258f3082dc889762664cb8f218d86da85dff8b07b342",
+        process.env["ETHEREUM_KEY"] || "your_key_here",
         context.ethereum()
     )
     const ETHEREUM_ACCOUNT_PUBLIC = await ETHEREUM_ACCOUNT.getAddress()
