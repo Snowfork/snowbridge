cf261f273c9a346e69ed45bb4e56c6c4ebdbd271
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2025-02-27 09:46:25 +0200

    Improve ERC20 implementation (#1384)

--- contracts/src/interfaces/IERC20.sol
+++ contracts/src/interfaces/IERC20.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023 Axelar Network
-// SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
+// SPDX-FileCopyrightText: 2025 Snowfork <hello@snowfork.com>
 
 pragma solidity 0.8.28;
 
@@ -8,7 +8,10 @@ pragma solidity 0.8.28;
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
 interface IERC20 {
-    error InvalidAccount();
+    error InvalidSender(address);
+    error InvalidReceiver(address);
+    error InvalidSpender(address);
+    error InvalidApprover(address);
     error InsufficientBalance(address sender, uint256 balance, uint256 needed);
     error InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
 
