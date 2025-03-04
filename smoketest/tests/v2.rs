53db204ab94a8eb43a0d034013088ce34f47926e
Ron <yrong1997@gmail.com>
2025-03-04 09:48:43 +0800

    Improve smoke tests (#1396)
    * Rather build register token on BH
    
    * Add comments
    
    * adds metadata fields to register token
    
    * Revert "adds metadata fields to register token"
    
    This reverts commit 90cbff5167e447d0bdcb3532b1ff677a5bdaef8a.
    
    * update tests
    
    * Cleanup
    
    * Update bindings
    
    * Fix smoke test send token to destination chain
    
    * Rename path
    
    * Add send pna
    
    * Check amount
    
    ---------
    
    Co-authored-by: Vincent Geddes <117534+vgeddes@users.noreply.github.com>
    Co-authored-by: claravanstaden <claravanstaden64@gmail.com>

--- smoketest/tests/v2.rs
+++ smoketest/tests/v2.rs
@@ -1,10 +1,20 @@
-#[path = "v2/outbound/agent_transact.rs"]
-mod agent_transact;
-#[path = "v2/outbound/register_agent.rs"]
+#[path = "v2/register_agent.rs"]
 mod register_agent;
-#[path = "v2/outbound/register_pna.rs"]
+#[path = "v2/register_ena.rs"]
+mod register_ena;
+#[path = "v2/register_pna.rs"]
 mod register_pna;
-#[path = "v2/outbound/transfer_ena.rs"]
+#[path = "v2/send_ena_to_ah.rs"]
+mod send_ena_to_ah;
+#[path = "v2/send_ena_to_penpal.rs"]
+mod send_ena_to_penpal;
+#[path = "v2/send_pna.rs"]
+mod send_pna;
+#[path = "v2/transact_e2p.rs"]
+mod transact_e2p;
+#[path = "v2/transact_p2e.rs"]
+mod transact_p2e;
+#[path = "v2/transfer_ena.rs"]
 mod transfer_ena;
-#[path = "v2/outbound/transfer_pna.rs"]
+#[path = "v2/transfer_pna.rs"]
 mod transfer_pna;
