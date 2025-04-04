415b108acb3b7e76adcea14e3ac83d193f8cd648
claravanstaden <claravanstaden64@gmail.com>
2025-04-04 16:55:31 +0200

    fix lodestar

--- scripts/init-smoketests.sh
+++ scripts/init-smoketests.sh
@@ -3,16 +3,9 @@
 set -eux
 
 echo "Checkout lodestar"
-pushd ..
-  if [ ! -d "lodestar" ]; then
-    git clone https://github.com/ChainSafe/lodestar
-  fi
-  if [ ! -L "snowbridge/lodestar" ]; then
-    (cd snowbridge && ln -sf ../lodestar lodestar)
-  fi
-  pushd lodestar
-    git fetch && git checkout $LODESTAR_VERSION
-  popd
+git clone https://github.com/ChainSafe/lodestar
+pushd lodestar
+  git fetch && git checkout $LODESTAR_VERSION
 popd
 
 echo "Setting up git hooks"
