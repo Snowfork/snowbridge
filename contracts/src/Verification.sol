737673a7ad82a84fa7e2ea97b13edc2717ea789a
claravanstaden <claravanstaden64@gmail.com>
2025-04-10 15:58:30 +0200

    simplify check

--- contracts/src/Verification.sol
+++ contracts/src/Verification.sol
@@ -141,25 +141,17 @@ library Verification {
         ParachainHeader calldata header,
         bool isV2
     ) internal pure returns (bool) {
+        bytes1 digestItemOtherKind = isV2 ? DIGEST_ITEM_OTHER_SNOWBRIDGE_V2 : DIGEST_ITEM_OTHER_SNOWBRIDGE;
+        
         for (uint256 i = 0; i < header.digestItems.length; i++) {
             // First check if the digest item is of the correct kind (DIGEST_ITEM_OTHER)
             // and has the correct length (33 bytes)
-            if (header.digestItems[i].kind == DIGEST_ITEM_OTHER && header.digestItems[i].data.length == 33) {
-                // For V1 protocol (isV2 = false), only accept DIGEST_ITEM_OTHER_SNOWBRIDGE
-                if (!isV2 && 
-                    header.digestItems[i].data[0] == DIGEST_ITEM_OTHER_SNOWBRIDGE &&
-                    commitment == bytes32(header.digestItems[i].data[1:])
-                ) {
-                    return true;
-                }
-                
-                // For V2 protocol (isV2 = true), only accept DIGEST_ITEM_OTHER_SNOWBRIDGE_V2
-                if (isV2 && 
-                    header.digestItems[i].data[0] == DIGEST_ITEM_OTHER_SNOWBRIDGE_V2 &&
-                    commitment == bytes32(header.digestItems[i].data[1:])
-                ) {
-                    return true;
-                }
+            if (header.digestItems[i].kind == DIGEST_ITEM_OTHER && 
+                header.digestItems[i].data.length == 33 &&
+                header.digestItems[i].data[0] == digestItemOtherKind &&
+                commitment == bytes32(header.digestItems[i].data[1:])
+            ) {
+                return true;
             }
         }
         return false;
