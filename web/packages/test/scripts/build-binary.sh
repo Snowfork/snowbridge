b2aa0f05ea9882c30ddb14c60f15ad163af83764
claravanstaden <claravanstaden64@gmail.com>
2025-04-11 15:09:03 +0200

    revert contracts v1 build

--- web/packages/test/scripts/build-binary.sh
+++ web/packages/test/scripts/build-binary.sh
@@ -65,13 +65,9 @@ build_relayers_v1_v2() {
     pushd "$root_dir"
 
     # Backup relayer directory
-    RELAYER_BACKUP_DIR=$(mktemp -d -t relayer-backup-XXXXXX)
-    echo "Backing up relayer directory to $RELAYER_BACKUP_DIR"
-    cp -r "$relay_dir" "$RELAYER_BACKUP_DIR/"
-    # Backup contracts directory
-    CONTRACTS_BACKUP_DIR=$(mktemp -d -t contracts-backup-XXXXXX)
-    echo "Backing up contracts directory to $CONTRACTS_BACKUP_DIR"
-    cp -r "$root_dir/contracts" "$CONTRACTS_BACKUP_DIR/"
+    BACKUP_DIR=$(mktemp -d -t relayer-backup-XXXXXX)
+    echo "Backing up relayer directory to $BACKUP_DIR"
+    cp -r "$relay_dir" "$BACKUP_DIR/"
 
     CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
@@ -83,35 +79,35 @@ build_relayers_v1_v2() {
 
     # Build snowbridge-v1 branch version
     echo "Building relayer v1"
-    rm -rf "$root_dir/contracts"
-    rm -rf "$relay_dir"
+    checkout_build_and_copy "snowbridge-v1" "snowbridge-relay-v1"
 
-    BRANCH="snowbridge-v1"
+    # Restore original relayer directory
+    echo "Restoring original relayer directory from backup"
+    rm -rf "$relay_dir"
+    mv "$BACKUP_DIR/$(basename "$relay_dir")" "$relay_dir"
+    rm -rf "$BACKUP_DIR"
+
+    popd
+}
+
+# Function to checkout, build relayer, and copy binary
+checkout_build_and_copy() {
+    BRANCH=$1
+    BINARY_NAME=$2
+
+    pushd $root_dir
+
+    rm -rf "$relay_dir"
 
     echo "Checking out relayer directory from branch: $BRANCH"
     git fetch origin $BRANCH
     git checkout FETCH_HEAD -- relayer
-    git checkout FETCH_HEAD -- contracts
-
-    echo "Building v1 contracts: $BRANCH"
-    build_contracts
 
     echo "Building relayer from branch: $BRANCH"
     mage -d "$relay_dir" build
 
     echo "Copying binary to output directory"
-    cp $relay_bin "$output_bin_dir/snowbridge-relay-v1"
-
-    # Restore original relayer and contracts directory
-    echo "Restoring original relayer and contracts directory from backup"
-    rm -rf "$relay_dir"
-    mv "$RELAYER_BACKUP_DIR/$(basename "$relay_dir")" "$relay_dir"
-    rm -rf "$RELAYER_BACKUP_DIR"
-
-    rm -rf "$root_dir/contracts"
-    mv "$CONTRACTS_BACKUP_DIR/contracts" "$root_dir/contracts"
-    rm -rf "$CONTRACTS_BACKUP_DIR"
-
+    cp $relay_bin "$output_bin_dir/$BINARY_NAME"
     popd
 }
 
