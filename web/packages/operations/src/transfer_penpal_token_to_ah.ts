50e15e2577511ba9cee9862df4d8a60d673eb3cb
Ron <yrong1997@gmail.com>
2025-04-11 22:46:55 +0800

    Support pna (#1448)
    * Support PNA
    
    * Revamp for V2
    
    * Update packages
    
    * Fix turbo build
    
    * Release packages

---
+++ web/packages/operations/src/transfer_penpal_token_to_ah.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"
+import { transferToPolkadot } from "./transfer_to_polkadot"
+
+const transfer = async () => {
+    await transferToPolkadot(1000, "pal", 100_000_000n)
+}
+
+transfer()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error("Error:", error)
+        process.exit(1)
+    })
