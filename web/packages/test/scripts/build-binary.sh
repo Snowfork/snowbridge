ba3abce26f1d1421325980c88bf767bc1774b275
claravanstaden <claravanstaden64@gmail.com>
2025-04-14 11:12:38 +0200

    testing

--- web/packages/test/scripts/build-binary.sh
+++ web/packages/test/scripts/build-binary.sh
@@ -149,10 +149,12 @@ install_binary() {
     echo "Building and installing binaries."
     mkdir -p $output_bin_dir
     build_lodestar
-    build_binaries
+    #build_binaries
     if [ "$snowbridge_v1_v2" = true ]; then
+        echo "Building relayers v1 and v2"
         build_relayers_v1_v2
     else
+        echo "Building relayers v1"
         build_contracts
         build_latest_relayer
     fi
