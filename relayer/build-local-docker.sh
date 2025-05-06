8adaeea765933c59fb1c3dfe105109aa0a0bb76f
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-05-06 15:35:59 +0100

    init

---
+++ relayer/build-local-docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+source ../web/packages/test/scripts/set-env.sh
+
+build_image()
+{
+    docker build -f Dockerfile -t snowbridge-relayer:local .
+}
+
+if [ -z "${from_start_services:-}" ]; then
+    echo "Building local docker image"
+    build_image
+fi
