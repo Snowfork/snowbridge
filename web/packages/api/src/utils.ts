25c17946c73a1fe97ed4064996413a88aa5a419f
Alistair Singh <alistair.singh7@gmail.com>
2025-07-03 02:10:07 +0200

    fix padding

--- web/packages/api/src/utils.ts
+++ web/packages/api/src/utils.ts
@@ -135,5 +135,5 @@ export function padFeeByPercentage(fee: bigint, padPercent: bigint) {
     if (padPercent < 0 || padPercent > 100) {
         throw Error(`padPercent ${padPercent} not in range of 0 to 100.`)
     }
-    return fee * ((100n + padPercent) / 100n)
+    return (fee * (100n + padPercent)) / 100n
 }
