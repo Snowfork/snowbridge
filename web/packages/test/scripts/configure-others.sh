b557c1e72faa00063eb64240d4f3d5474c6482bd
ron <yrong1997@gmail.com>
2025-07-26 11:43:42 +0800

    Configure WND registration under others

--- web/packages/test/scripts/configure-others.sh
+++ web/packages/test/scripts/configure-others.sh
@@ -4,12 +4,6 @@ set -eu
 source scripts/set-env.sh
 source scripts/xcm-helper.sh
 
-
-register_weth() {
-    register_weth_on_ah
-    register_weth_on_penpal
-}
-
 register_weth_on_ah() {
     # register weth
     local call='0x3501020209079edaa8020300b8ea8cb425d85536b158d661da1ef0895bb92f1d00ce796ae65569a670d0c1cc1ac12515a3ce21b5fbf729d63d7b289baad070139d0104'
@@ -137,10 +131,8 @@ function transfer_local_balance() {
             "${amount}"
 }
 
-add_liquidity_on_ah() {
-    # register Wnd on BH
-    local call="0x24010501000c776e640c776e640c020109079edaa8020002286bee"
-    send_governance_transact_from_relaychain $ASSET_HUB_PARAID "$call"
+
+configure_on_ah() {
     # Mint Ether to Alice
     local call="0x3506020109079edaa80200d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d1300002cf61a24a229"
     send_transact_through_bridge_from_relaychain $ASSET_HUB_PARAID "$call"
@@ -152,21 +144,26 @@ add_liquidity_on_ah() {
     send_transact_through_user_origin_from_relaychain $ASSET_HUB_PARAID "$sudo_pubkey" "$call"
     local call="0x38010100020109079edaa8020080c6a47e8d0300000000000000000000008d49fd1a0700000000000000000001000000000000000000000000000000010000000000000000000000000000001cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c"
     send_transact_through_user_origin_from_relaychain $ASSET_HUB_PARAID "$sudo_pubkey" "$call"
-}
-
-configure_others_on_ah() {
-    add_liquidity_on_ah
+    # Register WETH
+    register_weth_on_ah
+     # register Wnd on Ethereum
+    local call="0x24010501000c776e640c776e640c020109079edaa8020002286bee"
+    send_governance_transact_from_relaychain $ASSET_HUB_PARAID "$call"
+    # register Roc on AH and Ethereum
     register_roc_on_ah
 }
 
-function configure_all() {
-    configure_others_on_ah
+configure_on_penpal() {
     configure_bridge
     register_ether
-    register_weth
+    register_weth_on_penpal
     register_pal
     mint_wnd_as_fee
-    add_liquidity
+}
+
+function configure_all() {
+    configure_on_ah
+    configure_on_penpal
 }
 
 
