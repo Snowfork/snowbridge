3a6b96e8cb092487361fe5d0cf37b7b4d6ddab45
ron <yrong1997@gmail.com>
2025-07-28 16:17:05 +0800

    Fix setup scripts

--- web/packages/test-helpers/src/configureE2E.ts
+++ web/packages/test-helpers/src/configureE2E.ts
@@ -36,7 +36,9 @@ const sendBatchTransactions = async (
     noSending?: boolean
 ) => {
     // Connect to node
-    let api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:11144") })
+    let api = await ApiPromise.create({
+        provider: new WsProvider("ws://127.0.0.1:" + wsPort.toString()),
+    })
     api = await api.isReady
 
     // Initialize Keyring and add an account (Replace with your private key or use mnemonic)
@@ -76,148 +78,7 @@ const sendBatchTransactions = async (
         return Promise.resolve({ sender, tx: finalTx })
     }
     // Sign and send the batch transaction
-    batchTx.signAndSend(sender, ({ status }) => {
-        if (status.isInBlock) {
-            console.log(`✅ Transaction included in block: ${status.asInBlock}`)
-        }
-    })
-}
-
-const sendBatchTransactionsOnAssethub = async () => {
-    // Connect to node
-    const api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:12144") })
-
-    // Initialize Keyring and add an account (Replace with your private key or use mnemonic)
-    const keyring = new Keyring({ type: "sr25519" })
-    const sender = keyring.addFromUri(sudoAccount)
-
-    const versionedLocation = api.createType("XcmVersionedLocation", {
-        v4: {
-            parents: 1,
-            interior: {
-                x1: [
-                    {
-                        accountId32: {
-                            network: null,
-                            id: SudoPubKey,
-                        },
-                    },
-                ],
-            },
-        },
-    })
-
-    // Define recipient addresses and amounts (replace with real addresses)
-    const transactions = [
-        //Account for penpal (Sibling parachain 2000)
-        api.tx.balances.transferAllowDeath(
-            "5Eg2fntJ27qsari4FGrGhrMqKFDRnkNSR6UshkZYBGXmSuC8",
-            InitialFund
-        ),
-        //Account for snowbridge sovereign
-        api.tx.balances.transferAllowDeath(
-            "5GjRnmh5o3usSYzVmsxBWzHEpvJyHK4tKNPhjpUR3ASrruBy",
-            InitialFund
-        ),
-        //Account for checking account
-        api.tx.balances.transferAllowDeath(
-            "5EYCAe5ijiYgWYWi1fs8Xz1td1djEtJVVnNfzvDRP4VtLL7Y",
-            InitialFund
-        ),
-        api.tx.polkadotXcm.addAuthorizedAlias(versionedLocation, null),
-    ]
-
-    // Create a batch transaction
-    const batchTx = api.tx.utility.batchAll(transactions)
-
-    console.log("Sending batch transaction...")
-
-    // Sign and send the batch transaction
-    batchTx.signAndSend(sender, ({ status }) => {
-        if (status.isInBlock) {
-            console.log(`✅ Transaction included in block: ${status.asInBlock}`)
-        }
-    })
-}
-
-const buildHrmpChannels = async () => {
-    // Connect to node
-    let api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:9944") })
-    api = await api.isReady
-
-    // Initialize Keyring and add an account (Replace with your private key or use mnemonic)
-    const keyring = new Keyring({ type: "sr25519" })
-    const sender = keyring.addFromUri(sudoAccount)
-    await cryptoWaitReady()
-
-    const transactions = [
-        api.tx.hrmp.forceOpenHrmpChannel(1000, 1002, 8, 512),
-        api.tx.hrmp.forceOpenHrmpChannel(1002, 1000, 8, 512),
-        api.tx.hrmp.forceOpenHrmpChannel(1000, 2000, 8, 512),
-        api.tx.hrmp.forceOpenHrmpChannel(2000, 1000, 8, 512),
-    ]
-
-    // Create a batch transaction
-    const batchTx = api.tx.utility.batchAll(transactions)
-    const sudoTx = api.tx.sudo.sudo(batchTx)
-
-    console.log("Sending sudo transaction...")
-
-    // Sign and send the batch transaction
-    const unsub = await sudoTx.signAndSend(sender, ({ status }) => {
-        if (status.isInBlock) {
-            console.log(`✅ Transaction included in block: ${status.asInBlock}`)
-            unsub()
-        }
-    })
-}
-
-const sendBatchTransactionsOnPenpal = async () => {
-    // Connect to node
-    const api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:13144") })
-
-    // Initialize Keyring and add an account (Replace with your private key or use mnemonic)
-    const keyring = new Keyring({ type: "sr25519" })
-    const sender = keyring.addFromUri(sudoAccount)
-
-    const versionedLocation = api.createType("XcmVersionedLocation", {
-        v4: {
-            parents: 1,
-            interior: {
-                x1: [
-                    {
-                        accountId32: {
-                            network: null,
-                            id: SudoPubKey,
-                        },
-                    },
-                ],
-            },
-        },
-    })
-
-    // Define recipient addresses and amounts (replace with real addresses)
-    const transactions = [
-        //Account for AH sovereign
-        api.tx.balances.transferAllowDeath(
-            "5Eg2fntNprdN3FgH4sfEaaZhYtddZQSQUqvYJ1f2mLtinVhV",
-            InitialFund
-        ),
-        //Checking account
-        api.tx.balances.transferAllowDeath(
-            "5EYCAe5ijiYgWYWi1fs8Xz1td1djEtJVVnNfzvDRP4VtLL7Y",
-            InitialFund
-        ),
-        api.tx.polkadotXcm.addAuthorizedAlias(versionedLocation, null),
-    ]
-
-    // Create a batch transaction
-    const batchTx = api.tx.utility.batchAll(transactions)
-
-    console.log("Sending batch transaction...")
-
-    // Sign and send the batch transaction
-    batchTx.signAndSend(sender, ({ status }) => {
+    finalTx.signAndSend(sender, ({ status }) => {
         if (status.isInBlock) {
             console.log(`✅ Transaction included in block: ${status.asInBlock}`)
         }
