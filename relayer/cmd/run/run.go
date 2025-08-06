c6d37cef05a8bd47efbc14a9661e0b9dc329669e
Parth <desaiparth08@gmail.com>
2025-08-06 18:15:04 +0400

    Remove parachain module and related commands (#12)
    * remove parachain module and related commands
    
    * fix compilation error

--- relayer/cmd/run/run.go
+++ relayer/cmd/run/run.go
@@ -4,7 +4,6 @@ import (
 	"github.com/snowfork/snowbridge/relayer/cmd/run/beacon"
 	"github.com/snowfork/snowbridge/relayer/cmd/run/beefy"
 	"github.com/snowfork/snowbridge/relayer/cmd/run/execution"
-	"github.com/snowfork/snowbridge/relayer/cmd/run/parachain"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +15,6 @@ func Command() *cobra.Command {
 	}
 
 	cmd.AddCommand(beefy.Command())
-	cmd.AddCommand(parachain.Command())
 	cmd.AddCommand(beacon.Command())
 	cmd.AddCommand(execution.Command())
 
