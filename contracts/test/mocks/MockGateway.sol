06f8d2b38e45a54e89c0eb638240585d8e6d8b27
Evgeny Svirsky <e.svirsky@tut.by>
2025-08-07 13:12:41 +0300

    - Reverted solidity version to 0.8.25.0

--- contracts/test/mocks/MockGateway.sol
+++ contracts/test/mocks/MockGateway.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.28;
+pragma solidity 0.8.25;
 
 import {Gateway} from "../../src/Gateway.sol";
 import {ChannelID, ParaID, OperatingMode} from "../../src/Types.sol";
