afb5644ffee7d1c117ce33accb9fb928b46d08c2
claravanstaden <claravanstaden64@gmail.com>
2025-09-01 14:06:10 +0200

    revert log

--- web/packages/api/src/toEthereum_v2.ts
+++ web/packages/api/src/toEthereum_v2.ts
@@ -968,10 +968,6 @@ export async function dryRunOnSourceParachain(
             tx.toHuman(),
             result.toHuman()
         )
-        console.dir(
-            result.toHuman(),
-            {depth: 100}
-        )
     } else {
         bridgeHubForwarded =
             result.asOk.forwardedXcms.find((x) => {
