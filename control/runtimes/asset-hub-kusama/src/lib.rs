f5e59603660fd3fa2cc71939def22e0f43dc1fa0
claravanstaden <claravanstaden64@gmail.com>
2025-09-16 10:46:54 +0200

    update control tool

--- control/runtimes/asset-hub-kusama/src/lib.rs
+++ control/runtimes/asset-hub-kusama/src/lib.rs
@@ -1,6 +1,7 @@
 #[subxt::subxt(
     runtime_metadata_path = "asset-hub-metadata.bin",
-    derive_for_all_types = "Clone"
+    derive_for_all_types = "Clone, WrapperTypeEncode",
+    derive_for_type(path = "asset_hub_kusama_runtime::RuntimeCall", derive = "codec::Encode"),
 )]
 mod runtime {}
 
