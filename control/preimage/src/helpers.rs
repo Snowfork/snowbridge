b7120c27f3bda0ee56ac3c3de4f02ff98742196f
claravanstaden <claravanstaden64@gmail.com>
2025-09-23 11:34:37 +0200

    remove unnecessary cfg and deps

--- control/preimage/src/helpers.rs
+++ control/preimage/src/helpers.rs
@@ -180,7 +180,6 @@ pub fn sudo(call: Box<RelayRuntimeCall>) -> RelayRuntimeCall {
 }
 
 pub fn force_xcm_version() -> AssetHubRuntimeCall {
-    #[cfg(any(feature = "paseo", feature = "westend", feature = "polkadot"))]
     use crate::asset_hub_runtime::runtime_types::staging_xcm::v5::{
         junction::Junction::GlobalConsensus, junction::NetworkId, junctions::Junctions::X1,
         location::Location,
