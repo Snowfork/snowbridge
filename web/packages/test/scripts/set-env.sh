b9d1cfdf9af073aa0ea892593162a7f1056758b9
claravanstaden <claravanstaden64@gmail.com>
2025-10-09 11:16:54 +0200

    update version

--- web/packages/test/scripts/set-env.sh
+++ web/packages/test/scripts/set-env.sh
@@ -15,7 +15,7 @@ export PATH="$output_bin_dir:$PATH"
 polkadot_sdk_dir="${POLKADOT_SDK_DIR:-../polkadot-sdk}"
 
 export GETH_VERSION=v1.16.4
-export LODESTAR_VERSION=v1.35.0-rc.1
+export LODESTAR_VERSION=v1.35.0
 export snowbridge_v1="${BUILD_V1:-false}"
 v1_root_dir="$root_dir/../snowbridge-v1"
 export v1_contract_dir="$v1_root_dir/contracts"
