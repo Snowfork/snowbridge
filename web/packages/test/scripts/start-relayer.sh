a67b3e908115ba56dcd9ef021ac8a532172caea9
claravanstaden <claravanstaden64@gmail.com>
2025-10-11 16:21:23 +0200

    merge damage

--- web/packages/test/scripts/start-relayer.sh
+++ web/packages/test/scripts/start-relayer.sh
@@ -159,7 +159,6 @@ config_relayer() {
         --argjson electra_forked_epoch $electra_forked_epoch \
         --argjson fulu_forked_epoch $fulu_forked_epoch \
         --arg channelID $ASSET_HUB_CHANNEL_ID \
-        --argjson electra_forked_epoch $electra_forked_epoch \
         '
       .source.ethereum.endpoint = $eth_endpoint_ws
     | .source.contracts.Gateway = $k1
@@ -169,7 +168,6 @@ config_relayer() {
     | .source.beacon.spec.forkVersions.fulu = $fulu_forked_epoch
     | .source."channel-id" = $channelID
     | .schedule.id = 2
-    | .source.beacon.spec.forkVersions.electra = $electra_forked_epoch
     ' \
         config/execution-relay.json >$output_dir/execution-relay-asset-hub-2.json
 
@@ -177,12 +175,14 @@ config_relayer() {
       jq \
           --arg eth_endpoint_ws $eth_endpoint_ws \
           --arg k1 "$(address_for GatewayProxy)" \
+          --argjson fulu_forked_epoch $fulu_forked_epoch \
           --argjson electra_forked_epoch $electra_forked_epoch \
           '
         .source.ethereum.endpoint = $eth_endpoint_ws
       | .source.contracts.Gateway = $k1
       | .schedule.id = 0
       | .source.beacon.spec.forkVersions.electra = $electra_forked_epoch
+      | .source.beacon.spec.forkVersions.fulu = $fulu_forked_epoch
 
       ' \
           config/execution-relay.json >$output_dir/execution-relay.json
