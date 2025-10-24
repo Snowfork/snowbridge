d9b5fcfd808c4681b9b751f79ade9395741b9af4
ron <yrong1997@gmail.com>
2025-10-24 07:58:37 +0000

    Fix execution relay

--- relayer/relays/execution/main.go
+++ relayer/relays/execution/main.go
@@ -178,8 +178,7 @@ func (r *Relay) Start(ctx context.Context, eg *errgroup.Group) error {
 						log.WithField("nonce", ev.Nonce).Info("beacon header not finalized yet")
 						continue
 					} else if err != nil {
-						log.WithFields(log.Fields{"nonce": ev.Nonce, "error": err}).Warn("submit event: message was not processed")
-						continue
+						return fmt.Errorf("submit event: %w", err)
 					}
 				}
 			}
