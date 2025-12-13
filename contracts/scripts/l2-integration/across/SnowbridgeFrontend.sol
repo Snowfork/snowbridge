3c2bb983eed24bc10921aea98576ec72bcac31b4
ron <yrong1997@gmail.com>
2025-12-13 15:27:23 +0000

    Reorder deposit logic

--- contracts/scripts/l2-integration/across/SnowbridgeFrontend.sol
+++ contracts/scripts/l2-integration/across/SnowbridgeFrontend.sol
@@ -38,49 +38,14 @@ contract SnowbridgeFrontend is Ownable {
         uint256 nativeFeeAmount,
         address recipient
     ) public {
-        // Fund the native fee to the remote HANDLER which will pay the relayer and execution fees and call
-        // Snowbridge Gateway to execute the message
         require(
             nativeFeeAmount > sendParams.relayerFee + sendParams.executionFee,
             "Native fee must be greater than send fees"
         );
         IERC20(L2WETH9).approve(address(SPOKE_POOL), nativeFeeAmount);
-        SPOKE_POOL.deposit(
-            bytes32(uint256(uint160(recipient))),
-            bytes32(uint256(uint160(HANDLER))),
-            bytes32(uint256(uint160(L2WETH9))),
-            bytes32(uint256(uint160(L1WETH9))),
-            nativeFeeAmount,
-            sendParams.relayerFee + sendParams.executionFee,
-            params.destinationChainId,
-            bytes32(0), // exclusiveRelayer, zero means any relayer can fill
-            uint32(block.timestamp - 600), // quoteTimestamp set to 10 minutes before now
-            uint32(block.timestamp + 600), // fillDeadline set to 10 minutes after now
-            0, // exclusivityDeadline, zero means no exclusivity
-            bytes("")
-        );
-
-        // Approve input token to SpokePool and prepare message for cross-chain call
         IERC20(params.inputToken).approve(address(SPOKE_POOL), params.inputAmount);
-        Call[] memory calls = new Call[](1);
-        bytes memory sendMessageCalldata = abi.encodeCall(
-            IGatewayV2.v2_sendMessage,
-            (
-                sendParams.xcm,
-                sendParams.assets,
-                sendParams.claimer,
-                sendParams.executionFee,
-                sendParams.relayerFee
-            )
-        );
-        calls[0] = Call({
-            target: address(GATEWAY),
-            callData: sendMessageCalldata,
-            value: sendParams.executionFee + sendParams.relayerFee
-        });
-        Instructions memory instructions =
-            Instructions({calls: calls, fallbackRecipient: recipient});
-        bytes memory message = abi.encode(instructions);
+
+        // A first deposit for the token swap on the destination chain
         SPOKE_POOL.deposit(
             bytes32(uint256(uint160(recipient))),
             bytes32(uint256(uint160(HANDLER))),
@@ -93,6 +58,42 @@ contract SnowbridgeFrontend is Ownable {
             uint32(block.timestamp - 600),
             uint32(block.timestamp + 600),
             0,
+            bytes("")
+        );
+
+        // A second deposit to cover the cross-chain v2_sendMessage call along with fees
+        Call[] memory calls = new Call[](1);
+        calls[0] = Call({
+            target: address(GATEWAY),
+            callData: abi.encodeCall(
+                IGatewayV2.v2_sendMessage,
+                (
+                    sendParams.xcm,
+                    sendParams.assets,
+                    sendParams.claimer,
+                    sendParams.executionFee,
+                    sendParams.relayerFee
+                )
+            ),
+            value: sendParams.executionFee + sendParams.relayerFee
+        });
+        Instructions memory instructions =
+            Instructions({calls: calls, fallbackRecipient: recipient});
+        bytes memory message = abi.encode(instructions);
+        SPOKE_POOL.deposit{
+            value: nativeFeeAmount
+        }(
+            bytes32(uint256(uint160(recipient))),
+            bytes32(uint256(uint160(HANDLER))),
+            bytes32(uint256(uint160(L2WETH9))),
+            bytes32(uint256(uint160(L1WETH9))),
+            nativeFeeAmount,
+            sendParams.relayerFee + sendParams.executionFee,
+            params.destinationChainId,
+            bytes32(0), // exclusiveRelayer, zero means any relayer can fill
+            uint32(block.timestamp - 600), // quoteTimestamp set to 10 minutes before now
+            uint32(block.timestamp + 600), // fillDeadline set to 10 minutes after now
+            0, // exclusivityDeadline, zero means no exclusivity
             message
         );
     }
