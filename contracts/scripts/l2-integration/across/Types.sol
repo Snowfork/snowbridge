2b17a2811a36e4efe0504c3b677a19b4cfecaa48
ron <yrong1997@gmail.com>
2026-01-12 15:16:48 +0000

    Add docs

--- contracts/scripts/l2-integration/across/Types.sol
+++ contracts/scripts/l2-integration/across/Types.sol
@@ -15,21 +15,30 @@ struct Instructions {
     address fallbackRecipient;
 }
 
+// Parameters for performing a swap
 struct SwapParams {
+    //input token address on source chain
     address inputToken;
+    //output token address on destination chain
     address outputToken;
+    // amount for input token to swap
     uint256 inputAmount;
+    // amount for output token to receive
     uint256 outputAmount;
+    // destination chain id
     uint256 destinationChainId;
+    // fill deadline buffer in seconds
     uint32 fillDeadlineBuffer;
 }
 
+// The first five parameters correspond to the same parameters in Gateway.sol's v2_sendMessage function. See Gateway.sol for more details.
+// The final parameter is specific to the seperated call used to prefund both the execution fee and the relayer fee.
 struct SendParams {
     bytes xcm;
     bytes[] assets;
     bytes claimer;
     uint128 executionFee;
     uint128 relayerFee;
-    // Fee to be paid on L2 in native token
+    // Fees are paid on L2 in the native token
     uint128 l2Fee;
 }
