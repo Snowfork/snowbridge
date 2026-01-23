3ee7ebe2072d7d155999e6927989b30470f2c25f
claravanstaden <claravanstaden64@gmail.com>
2026-01-23 09:44:56 +0200

    update solc versions

--- contracts/src/l2-integration/SnowbridgeL1Adaptor.sol
+++ contracts/src/l2-integration/SnowbridgeL1Adaptor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.28;
+pragma solidity 0.8.33;
 
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
