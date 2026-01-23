46170db0611aecad3c487ac1fe54ec8cbc39ac31
ron <yrong1997@gmail.com>
2026-01-23 13:46:03 +0800

    Update l2 contracts  to 0.8.33

--- contracts/scripts/l2-integration/across/test/TestXcm.s.sol
+++ contracts/scripts/l2-integration/across/test/TestXcm.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.28;
+pragma solidity 0.8.33;
 
 import {Script} from "forge-std/Script.sol";
 import {XCM_PRECOMPILE} from "../constants/Sepolia.sol";
