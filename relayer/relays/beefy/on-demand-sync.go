f086bcf732cb8e05e7604ea1572f1a0a38a9a59f
claravanstaden <claravanstaden64@gmail.com>
2026-02-05 12:07:33 +0200

    add removed fiat shamir back

--- relayer/relays/beefy/on-demand-sync.go
+++ relayer/relays/beefy/on-demand-sync.go
@@ -260,7 +260,11 @@ func (relay *OnDemandRelay) syncBeefyUpdate(ctx context.Context, task *Request) 
 	} else {
 		task.ValidatorsRoot = state.NextValidatorSetRoot
 	}
-	err = relay.ethereumWriter.submit(ctx, task)
+	if relay.ethereumWriter.config.EnableFiatShamir {
+		err = relay.ethereumWriter.submitFiatShamir(ctx, task)
+	} else {
+		err = relay.ethereumWriter.submit(ctx, task)
+	}
 	if err != nil {
 		return fmt.Errorf("fail to submit beefy update: %w", err)
 	}
