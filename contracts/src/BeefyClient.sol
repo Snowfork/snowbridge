577e298ef80deb60a7d6575f15b08505d0fa8b84
ron <yrong1997@gmail.com>
2026-02-10 15:02:08 +0800

    Revert "Audit Issue 9: Enforce single payload (#1654)"
    This reverts commit 3c26e64aed3c7c4e508cb7bb9786140cbdebac8e.

--- contracts/src/BeefyClient.sol
+++ contracts/src/BeefyClient.sol
@@ -766,14 +766,16 @@ contract BeefyClient {
         pure
         returns (bytes32)
     {
-        if (commitment.payload.length != 1) {
-            revert CommitmentNotRelevant();
+        for (uint256 i = 0; i < commitment.payload.length; i++) {
+            if (commitment.payload[i].payloadID == MMR_ROOT_ID) {
+                if (commitment.payload[i].data.length != 32) {
+                    revert InvalidMMRRootLength();
+                } else {
+                    return bytes32(commitment.payload[i].data);
+                }
+            }
         }
-        PayloadItem memory payload = commitment.payload[0];
-        if (payload.payloadID != MMR_ROOT_ID || payload.data.length != 32) {
-            revert CommitmentNotRelevant();
-        }
-        return bytes32(payload.data);
+        revert CommitmentNotRelevant();
     }
 
     function encodeCommitment(Commitment calldata commitment)
