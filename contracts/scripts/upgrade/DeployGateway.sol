49b0b3c789870c8c7a95b60b4189bb95e1c4d0d4
ron <yrong1997@gmail.com>
2026-02-10 11:22:33 +0000

    Upgrade Gateway contract on Sepolia

--- contracts/scripts/upgrade/DeployGateway.sol
+++ contracts/scripts/upgrade/DeployGateway.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.33;
 
 import {AgentExecutor} from "../../src/AgentExecutor.sol";
 import {Gateway202509} from "../../src/upgrade/Gateway202509.sol";
-import {GatewaySepolia202601} from "../../src/upgrade/Gateway202601.sepolia.sol";
+import {GatewaySepolia202602} from "../../src/upgrade/Gateway202602.sepolia.sol";
 import {Gateway} from "../../src/Gateway.sol";
 import {ParaID} from "../../src/Types.sol";
 import {Script} from "forge-std/Script.sol";
@@ -13,8 +13,6 @@ import {console} from "forge-std/console.sol";
 
 contract DeployGateway is Script {
     using stdJson for string;
-
-    address beefyClient = 0x1817874feAb3ce053d0F40AbC23870DB35C2AFfc;
 
     function run() public {
         vm.startBroadcast();
@@ -25,12 +23,14 @@ contract DeployGateway is Script {
             keccak256(abi.encodePacked(vm.envString("NODE_ENV")))
                 == keccak256(abi.encodePacked("polkadot_mainnet"))
         ) {
+            address beefyClient = 0x1817874feAb3ce053d0F40AbC23870DB35C2AFfc;
             gatewayLogic = new Gateway202509(address(beefyClient), address(executor));
         } else if (
             keccak256(abi.encodePacked(vm.envString("NODE_ENV")))
                 == keccak256(abi.encodePacked("westend_sepolia"))
         ) {
-            gatewayLogic = new GatewaySepolia202601(address(beefyClient), address(executor));
+            address beefyClient = 0xA04460B1D8bBef33F54edB2C3115e3E4D41237A6;
+            gatewayLogic = new GatewaySepolia202602(address(beefyClient), address(executor));
         }
 
         console.log("Gateway contract address: %s", address(gatewayLogic));
