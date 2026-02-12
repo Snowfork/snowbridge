fe6bb7831dcec8a4142023da70ba387d3034214c
ron <yrong1997@gmail.com>
2026-02-12 10:11:51 +0000

    Fix output token

--- web/packages/api/src/toEthereumSnowbridgeV2.ts
+++ web/packages/api/src/toEthereumSnowbridgeV2.ts
@@ -1033,7 +1033,7 @@ export async function buildL2Call(
         let calldata = l1Adapter.interface.encodeFunctionData("depositNativeEther", [
             {
                 inputToken: tokenAddress,
-                outputToken: l2TokenAddress,
+                outputToken: l2FeeTokenAddress,
                 inputAmount: tokenAmount,
                 outputAmount: tokenAmount - l2BridgeFeeInL1Token,
                 destinationChainId: l2ChainId,
