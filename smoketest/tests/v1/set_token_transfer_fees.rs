6b258dd0946eb02f2254a3e7b513a503dccbb691
ron <yrong1997@gmail.com>
2026-02-27 17:56:09 +0800

    Furthur cleanup

--- smoketest/tests/v1/set_token_transfer_fees.rs
+++ smoketest/tests/v1/set_token_transfer_fees.rs
@@ -23,8 +23,6 @@ async fn set_token_transfer_fees() {
 	let gateway_addr: Address = (*GATEWAY_PROXY_CONTRACT).into();
 	let ethereum_client = test_clients.ethereum_client;
 	let gateway = IGateway::new(gateway_addr, ethereum_client.clone());
-	let fees = gateway.quoteRegisterTokenFee().call().await.expect("get fees");
-	println!("register fees {:?}", fees);
 
 	let ethereum_system_api = bridgehub::api::ethereum_system::calls::TransactionApi;
 
@@ -46,7 +44,4 @@ async fn set_token_transfer_fees() {
 
 	wait_for_ethereum_event::<IGateway::TokenTransferFeesChanged>(ethereum_client, gateway_addr)
 		.await;
-
-	let fees = gateway.quoteRegisterTokenFee().call().await.expect("get fees");
-	println!("asset fees {:?}", fees);
 }
