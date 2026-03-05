2ba4c3e589659f6f1523c86026468ff11f975f91
Alistair Singh <alistair.singh7@gmail.com>
2026-03-06 00:21:31 +0200

    to EthereumEVM done

--- web/packages/api/src/transfers/toEthereumEvm/transferInterface.ts
+++ web/packages/api/src/transfers/toEthereumEvm/transferInterface.ts
@@ -1,7 +1,67 @@
-import { AssetRegistry, ContractCall } from "@snowbridge/base-types"
-import { DeliveryFee } from "../../toEthereum_v2"
-import { TransferEvm, ValidationResultEvm } from "../../toEthereumFromEVM_v2"
+import {
+    Asset,
+    AssetRegistry,
+    ContractCall,
+    ERC20Metadata,
+    EthereumChain,
+    Parachain,
+} from "@snowbridge/base-types"
+import { EventRecord } from "@polkadot/types/interfaces"
+import { ContractTransaction, TransactionReceipt } from "ethers"
+import { OperationStatus } from "../../status"
+import { DeliveryFee, FeeInfo, ValidationLog } from "../../toEthereum_v2"
 import { EthersContext } from "../../index"
+
+export type TransferEvm = {
+    input: {
+        registry: AssetRegistry
+        sourceAccount: string
+        beneficiaryAccount: any
+        tokenAddress: string
+        amount: bigint
+        fee: DeliveryFee
+    }
+    computed: {
+        sourceParaId: number
+        sourceAccountHex: string
+        tokenErcMetadata: ERC20Metadata
+        ahAssetMetadata: Asset
+        sourceAssetMetadata: Asset
+        sourceParachain: Parachain
+        messageId: string
+        ethChain?: EthereumChain
+        customXcmHex?: string
+        xcTokenAddress?: string
+    }
+    tx: ContractTransaction
+}
+
+export type ValidationResultEvm = {
+    logs: ValidationLog[]
+    success: boolean
+    data: {
+        bridgeStatus: OperationStatus
+        nativeBalance: bigint
+        dotBalance?: bigint
+        tokenBalance: bigint
+        feeInfo?: FeeInfo
+        sourceDryRunError: any
+        assetHubDryRunError: any
+    }
+    transfer: TransferEvm
+}
+
+export type MessageReceiptEvm = {
+    blockNumber: number
+    blockHash: string
+    substrateBlockHash: string
+    txIndex: number
+    txHash: string
+    success: boolean
+    events: EventRecord[]
+    dispatchError?: any
+    messageId?: string
+}
 
 export interface TransferInterface {
     getDeliveryFee(
@@ -33,4 +93,9 @@ export interface TransferInterface {
     ): Promise<TransferEvm>
 
     validateTransfer(context: EthersContext, transfer: TransferEvm): Promise<ValidationResultEvm>
+
+    getMessageReceipt(
+        source: { sourceParaId: number; context: EthersContext },
+        receipt: TransactionReceipt,
+    ): Promise<MessageReceiptEvm>
 }
