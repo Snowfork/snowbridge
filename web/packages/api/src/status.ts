6363b822bc095f50d24bdb424fdea40e8ded31aa
Alistair Singh <alistair.singh7@gmail.com>
2026-03-11 03:38:17 +0200

    move contracts to base types

--- web/packages/api/src/status.ts
+++ web/packages/api/src/status.ts
@@ -7,8 +7,7 @@ import {
 import { Context, subsquidV2 } from "./index"
 import { fetchBeaconSlot, fetchFinalityUpdate } from "./utils"
 import { ApiPromise } from "@polkadot/api"
-import { IGatewayV1, IGatewayV2 } from "./contracts"
-import { EthereumProviderTypes } from "@snowbridge/base-types"
+import { EthereumProviderTypes, IGatewayV1, IGatewayV2 } from "@snowbridge/base-types"
 
 export type OperatingMode = "Normal" | "Halted"
 export type BridgeStatusInfo = {
