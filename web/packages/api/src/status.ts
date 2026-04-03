493bf9e0fefe57630afb8a675420990d85bf0694
Ron <yrong1997@gmail.com>
2026-04-03 14:28:04 +0800

    Limit scope of monitoring (#1762)
    * Limit monitor scope to consensus for testnet
    
    * Cleanup

--- web/packages/api/src/status.ts
+++ web/packages/api/src/status.ts
@@ -219,45 +219,47 @@ export const channelStatusInfo = async (
         await bridgeHub.query.ethereumOutboundQueue.nonce(channelId)
     ).toPrimitive() as number
 
-    // V2 nonces
-    const v2_outbound_nonce_eth = Number(await gatewayV2.v2_outboundNonce())
-    const v2_outbound_nonce_sub = (
-        await bridgeHub.query.ethereumOutboundQueueV2.nonce()
-    ).toPrimitive() as number
-
-    const v2_max_delivered_nonce_to_polkadot = await subsquidV2.fetchMaxDeliveredNonceToPolkadot(
-        context.graphqlApiUrl(),
-        v2_outbound_nonce_eth,
-    )
-    const v2_max_delivered_nonce_to_ethereum = await subsquidV2.fetchMaxDeliveredNonceToEthereum(
-        context.graphqlApiUrl(),
-        v2_outbound_nonce_sub,
-    )
-
     let estimatedDeliveryTime: any,
         toEthereumUndeliveredTimeout: number | undefined,
         toPolkadotUndeliveredTimeout: number | undefined = undefined,
         toEthereumPendings: ToEthereumTransferResult[] = [],
-        toPolkadotPendings: ToPolkadotTransferResult[] = []
+        toPolkadotPendings: ToPolkadotTransferResult[] = [],
+        v2_max_delivered_nonce_to_polkadot: number | undefined = undefined,
+        v2_max_delivered_nonce_to_ethereum: number | undefined = undefined,
+        v2_outbound_nonce_eth: number | undefined = undefined,
+        v2_outbound_nonce_sub: number | undefined = undefined
 
-    if (channelId.toLowerCase() == ASSET_HUB_CHANNEL_ID.toLowerCase()) {
+    if (
+        context.environment.name == "polkadot_mainnet" &&
+        channelId.toLowerCase() == ASSET_HUB_CHANNEL_ID.toLowerCase()
+    ) {
+        v2_outbound_nonce_eth = Number(await gatewayV2.v2_outboundNonce())
+        v2_outbound_nonce_sub = (
+            await bridgeHub.query.ethereumOutboundQueueV2.nonce()
+        ).toPrimitive() as number
+        v2_max_delivered_nonce_to_polkadot = await subsquidV2.fetchMaxDeliveredNonceToPolkadot(
+            context.graphqlApiUrl(),
+            v2_outbound_nonce_eth,
+        )
+        v2_max_delivered_nonce_to_ethereum = await subsquidV2.fetchMaxDeliveredNonceToEthereum(
+            context.graphqlApiUrl(),
+            v2_outbound_nonce_sub,
+        )
         estimatedDeliveryTime = await subsquidV2.fetchEstimatedDeliveryTime(context.graphqlApiUrl())
 
-        let latency = await subsquidV2.fetchToEthereumUndeliveredLatency(context.graphqlApiUrl())
-        if (latency && latency.elapse) {
-            toEthereumUndeliveredTimeout = latency.elapse
-        }
-        latency = await subsquidV2.fetchToPolkadotUndeliveredLatency(context.graphqlApiUrl())
-        if (latency && latency.elapse) {
-            toPolkadotUndeliveredTimeout = latency.elapse
-        }
-
-        try {
-            // Pending transfers
+        let toEthereumUndelivered = await subsquidV2.fetchToEthereumUndeliveredLatency(
+            context.graphqlApiUrl(),
+        )
+        if (toEthereumUndelivered && toEthereumUndelivered.elapse) {
+            toEthereumUndeliveredTimeout = toEthereumUndelivered.elapse
             toEthereumPendings = await toEthereumPendingTransfers(context.graphqlApiUrl(), 10)
+        }
+        let toPolkadotUndelivered = await subsquidV2.fetchToPolkadotUndeliveredLatency(
+            context.graphqlApiUrl(),
+        )
+        if (toPolkadotUndelivered && toPolkadotUndelivered.elapse) {
+            toPolkadotUndeliveredTimeout = toPolkadotUndelivered.elapse
             toPolkadotPendings = await toPolkadotPendingTransfers(context.graphqlApiUrl(), 10)
-        } catch (error) {
-            console.error("Error fetching pending transfers:", error)
         }
     }
 
